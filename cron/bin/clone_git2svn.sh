4c278e2e326249560d84a3f01038c23ccb786892
id774 <idnanashi@gmail.com>
2008-08-25 18:24:54 +0900

    #8 Enhance clone git2svn script for adding repository of github

--- cron/bin/clone_git2svn.sh
+++ cron/bin/clone_git2svn.sh
@@ -3,8 +3,13 @@
 cd $HOME/local/github/scripts && git pull && cd -
 rsync -av --delete $HOME/local/github/scripts $HOME/svnwork/id774
 rm -rf $HOME/svnwork/id774/scripts/.git
+
+cd $HOME/local/github/scratch && git pull && cd -
+rsync -av --delete $HOME/local/github/scratch $HOME/svnwork/id774
+rm -rf $HOME/svnwork/id774/scratch/.git
+
 cd $HOME/svnwork/id774
-svk add .
+svk add -v .
 svk ci -m "clone from github"
 svk sm -m "clone from github" //id774 //mirror/id774
 
