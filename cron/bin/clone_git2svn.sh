3ed994ad97facce48d0600f21d68935fea71983e
id774 <idnanashi@gmail.com>
2008-08-25 23:51:43 +0900

    #9 Fix a minor bug of svk options

--- cron/bin/clone_git2svn.sh
+++ cron/bin/clone_git2svn.sh
@@ -9,7 +9,7 @@ rsync -av --delete $HOME/local/github/scratch $HOME/svnwork/id774
 rm -rf $HOME/svnwork/id774/scratch/.git
 
 cd $HOME/svnwork/id774
-svk add -v .
+svk add .
 svk ci -m "clone from github"
 svk sm -m "clone from github" //id774 //mirror/id774
 
