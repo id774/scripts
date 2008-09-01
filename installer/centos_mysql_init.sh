ee5dcac9e55808b7249abdc553045ddcadf9835d
id774 <idnanashi@gmail.com>
2008-09-01 16:22:23 +0900

    #16 MySQL setup script for CentOS supposing run with sudo

--- installer/centos_mysql_init.sh
+++ installer/centos_mysql_init.sh
@@ -5,27 +5,30 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v1.1 9/1,2008
+#       Improvement permission of mysql, run with sudo.
 #  v1.0 8/16,2008
 #       Stable.
 ########################################################################
 
-/etc/init.d/mysqld stop
-rm -rf /var/log/mysql
-rm -rf /var/log/mysql_innodb
-rm -rf /var/lib/mysql
-mkdir /var/log/mysql
-mkdir /var/log/mysql_innodb
-mkdir /var/lib/mysql
-mkdir /var/lib/mysql/data
-chown mysql:mysql /var/log/mysql
-chown mysql:mysql /var/log/mysql_innodb
-chown mysql:mysql /var/lib/mysql
-chown mysql:mysql /var/lib/mysql/data
-chmod 700 /var/log/mysql
-chmod 701 /var/lib/mysql
-chmod 700 /var/lib/mysql/data
-usermod -s /sbin/nologin mysql
-mv /var/log/mysqld.log /var/log/mysql/
-cp $SCRIPTS/etc/my-centos.cnf /etc/my.cnf
-mysql_install_db --defaults-file=/etc/my.cnf
-/etc/init.d/mysqld start
+sudo /etc/init.d/mysqld stop
+sudo rm -rf /var/log/mysql
+sudo rm -rf /var/log/mysql_innodb
+sudo rm -rf /var/lib/mysql
+sudo mkdir /var/log/mysql
+sudo mkdir /var/log/mysql_innodb
+sudo mkdir /var/lib/mysql
+sudo mkdir /var/lib/mysql/data
+sudo chown mysql:mysql /var/log/mysql
+sudo chown mysql:mysql /var/log/mysql_innodb
+sudo chown mysql:mysql /var/lib/mysql
+sudo chown mysql:mysql /var/lib/mysql/data
+sudo chmod 700 /var/log/mysql
+sudo chmod 755 /var/lib/mysql
+sudo touch /var/lib/mysql/mysql.sock
+sudo chmod 700 /var/lib/mysql/data
+sudo usermod -s /sbin/nologin mysql
+sudo mv /var/log/mysqld.log /var/log/mysql/
+sudo cp $SCRIPTS/etc/my-centos.cnf /etc/my.cnf
+sudo mysql_install_db --defaults-file=/etc/my.cnf
+sudo /etc/init.d/mysqld start
