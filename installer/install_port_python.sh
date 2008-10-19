3aa0518acccab840cb1745531324f8478ef28ba3
id774 <idnanashi@gmail.com>
2008-10-19 11:33:25 +0900

    #56 Add MacPorts Python Installer

---
+++ installer/install_port_python.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+########################################################################
+# MacPorts Python installer
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v1.0 10/19,2008
+#       Stable.
+########################################################################
+
+sudo port -d selfupdate
+
+sudo port -d install python25
+sudo ln -s /opt/local/bin/python2.5 /usr/local/bin/python
+sudo port -d install py25-hashlib
+sudo port -d install py25-zlib
+sudo port -d install py25-setuptools
+sudo ln -s /opt/local/bin/easy_install-2.5 /usr/local/bin/easy_install
+
+sudo port -d install py25-readline
+sudo port -d install py25-mysql
+sudo port -d install py25-twisted
+sudo port -d install py25-pgsql
+sudo port -d install py25-mechanize
+sudo port -d install py25-openssl
+sudo port -d install py25-paste
+sudo port -d install py25-simplejson
+sudo port -d install py25-nose
+sudo port -d install py25-sqlalchemy
+sudo port -d install py25-sqlalchemy-migrate
+sudo port -d install py25-turbogears
+sudo port -d install py25-memcached
+
+port installed
