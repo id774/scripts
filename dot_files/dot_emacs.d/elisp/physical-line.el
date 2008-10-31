750183b80eb63e19c6c4fac224e2383761ee575b
id774 <idnanashi@gmail.com>
2008-10-31 20:36:22 +0900

    #74 Implement emacs physical line mode

--- dot_files/dot_emacs.d/elisp/physical-line.el
+++ dot_files/dot_emacs.d/elisp/physical-line.el
@@ -1,4 +1,6 @@
 ;; 物理行移動
+(global-set-key "\C-p" 'previous-window-line)
+(global-set-key "\C-n" 'next-window-line)
 (global-set-key "\M-p" 'previous-window-line)
 (global-set-key "\M-n" 'next-window-line)
 (global-set-key [up] 'previous-window-line)
