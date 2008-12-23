e6f5e417e071caf7ab4d3cf23c07b4d15df4fcbf
id774 <idnanashi@gmail.com>
2008-12-24 03:40:05 +0900

    #110 Change auto-complete key

--- dot_files/dot_emacs.d/elisp/global-set-key.el
+++ dot_files/dot_emacs.d/elisp/global-set-key.el
@@ -16,7 +16,7 @@
       (t (keyboard-translate ?\C-h ?\C-?)))
 
 ;; auto-complete-modeの有効/無効を切り替える
-(define-key global-map "\C-c\C-c\ a" 'auto-complete-mode)
+(define-key global-map "\C-c\C-c\ c" 'auto-complete-mode)
 
 ;; Twitter
 (define-key global-map "\C-c\C-c\ 1" 'twitter1-mode)
