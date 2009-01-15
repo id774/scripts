94f84ee13abbe42445ca12e37e40cfbc125e5e86
id774 <idnanashi@gmail.com>
2009-01-15 11:25:24 +0900

    #152 Add madwifi installer

---
+++ installer/install_madwifi.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+########################################################################
+# Install MadWifi
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v1.0 1/15,2009
+#       Stable.
+########################################################################
+
+set_network_permission() {
+    case $OSTYPE in
+      *darwin*)
+        sudo chown -R root:wheel /usr/local/src/network
+        ;;
+      *)
+        sudo chown -R root:root /usr/local/src/network
+        ;;
+    esac
+}
+
+install_trunk() {
+    test -d /usr/local/src/network/madwifi || sudo mkdir -p /usr/local/src/network/madwifi
+    cd /usr/local/src/network/madwifi
+    sudo svn co http://svn.madwifi-project.org/madwifi/trunk
+    cd trunk
+    sudo make
+    sudo make install
+    sudo modinfo ath_pci
+}
+
+install_trunk
+set_network_permission
+
