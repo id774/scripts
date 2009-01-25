84128bd026d0c9959136884fd7433b15c43ce88b
id774 <idnanashi@gmail.com>
2009-01-25 17:08:46 +0900

    #176 Hide log of tar

--- cron/bin/clone_coderepos.sh
+++ cron/bin/clone_coderepos.sh
@@ -4,5 +4,9 @@ test -f $HOME/local/git/coderepos.tar.gz && rm $HOME/local/git/coderepos.tar.gz
 cd $HOME/local/git/share
 git svn rebase
 cd ..
-tar czvf $HOME/local/git/coderepos.tar.gz share/
+tar czvf $HOME/local/git/coderepos.tar.gz share/ >/dev/null
+
+cp $HOME/local/git/coderepos.tar.gz /home/plagger/local/git/coderepos.tar.gz
+chmod 640 /home/plagger/local/git/coderepos.tar.gz
+chown plagger:plagger /home/plagger/local/git/coderepos.tar.gz
 
