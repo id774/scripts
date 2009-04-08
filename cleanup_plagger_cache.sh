3d34c8d6b63b77a5730c467a4cecf5ded941ed59
id774 <idnanashi@gmail.com>
2009-04-08 10:36:03 +0900

    #214 Add while_demo.sh

--- cleanup_plagger_cache.sh
+++ cleanup_plagger_cache.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
 plagger_cleanup_tmpdir() {
-    sudo rm -rf /home/plagger/.plagger_tmp/$1
-    sudo mkdir /home/plagger/.plagger_tmp/$1
-    sudo chown -R plagger:plagger /home/plagger/.plagger_tmp/$1
-    sudo chmod 750 /home/plagger/.plagger_tmp/$1
+  while [ $# -gt 0 ]
+  do
+    sudo rm -rf /home/plagger/.plagger_tmp/tmp$1
+    sudo mkdir /home/plagger/.plagger_tmp/tmp$1
+    sudo chown -R plagger:plagger /home/plagger/.plagger_tmp/tmp$1
+    sudo chmod 750 /home/plagger/.plagger_tmp/tmp$1
+    shift
+  done 
 }
 
-plagger_cleanup_tmpdir tmp0
-plagger_cleanup_tmpdir tmp1
-plagger_cleanup_tmpdir tmp2
-plagger_cleanup_tmpdir tmp3
-plagger_cleanup_tmpdir tmp4
-plagger_cleanup_tmpdir tmp5
-plagger_cleanup_tmpdir tmp6
-plagger_cleanup_tmpdir tmp7
-plagger_cleanup_tmpdir tmp8
-plagger_cleanup_tmpdir tmp9
-plagger_cleanup_tmpdir tmp10
-plagger_cleanup_tmpdir tmp11
+plagger_cleanup_tmpdir 0 1 2 3 4 5 6 7 8 9 10 11 12
 
---
+++ while_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while_demo() {
+  while [ $# -gt 0 ]
+  do
+    echo $1
+    shift
+  done 
+}
+
+while_demo $*
