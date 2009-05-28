2d50dbb1fd04cc4a9665641797683779c2ea2e6e
id774 <idnanashi@gmail.com>
2009-05-28 23:26:05 +0900

    #276 Correspond to skype backup tool for darwin

--- skype-pack.sh
+++ skype-pack.sh
@@ -3,5 +3,13 @@
 test -d ~/arc || exit 1
 test -f ~/arc/skype.tar.gz && rm ~/arc/skype.tar.gz
 cd
-tar czvf ~/arc/skype.tar.gz .Skype
+
+case $OSTYPE in
+  *darwin*)
+    tar czvf ~/arc/skype.tar.gz "Library/Application Support/Skype"
+    ;;
+  *)
+    tar czvf ~/arc/skype.tar.gz .mozilla
+    ;;
+esac
 
--- skype-unpack.sh
+++ skype-unpack.sh
@@ -2,6 +2,15 @@
 
 test -f ~/arc/skype.tar.gz || exit 1
 cd
-rm -rf ~/.Skype
+
+case $OSTYPE in
+  *darwin*)
+    rm -rf  "~/Library/Application Support/Skype"
+    ;;
+  *)
+    rm -rf ~/.Skype
+    ;;
+esac
+
 tar xzvf ~/arc/skype.tar.gz
 
