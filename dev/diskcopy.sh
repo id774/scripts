f08d849296be6548a16a8ab027bbaf12ca01ce29
id774 <idnanashi@gmail.com>
2009-06-16 13:44:54 +0900

    #279 Show return code of diskcopy tool

--- dev/diskcopy.sh
+++ dev/diskcopy.sh
@@ -1,43 +1,61 @@
 #!/bin/sh
 
+JOBLOG=~/diskcopy.log
 test -n "$1" && SOURCE_DRV=$1
 test -n "$1" || SOURCE_DRV=~/mnt/sdc
 test -n "$2" && TARGET_DRV=$2
 test -n "$2" || TARGET_DRV=~/mnt/sdb
-test -n "$3" && ADMIN_MAIL_ADDRESS=$3
-test -n "$3" || ADMIN_MAIL_ADDRESS=xxxxxx@gmail.com
+# test -n "$3" && ADMIN_MAIL_ADDRESS=$3
+# test -n "$3" || ADMIN_MAIL_ADDRESS=xxxxxx@gmail.com
 
-echo "diskcopy start.">~/diskcopy.log
+echo "diskcopy start.">$JOBLOG
 
-cp -av $SOURCE_DRV/user1 $TARGET_DRV
-echo "user1 completed.">>~/diskcopy.log
+echo -n "Diskcopy job started at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    cat ~/diskcopy.log | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat $JOBLOG | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    ;;
+esac
+
+cp -av $SOURCE_DRV/user1 $TARGET_DRV
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcopy user1 completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+case "$ADMIN_MAIL_ADDRESS" in
+  *@*)
+    cat $JOBLOG | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
 cp -av $SOURCE_DRV/user2 $TARGET_DRV
-echo "user2 completed.">>~/diskcopy.log
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcopy user2 completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    cat ~/diskcopy.log | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat $JOBLOG | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
 cp -av $SOURCE_DRV/user3 $TARGET_DRV
-echo "user3 completed.">>~/diskcopy.log
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcopy user3 completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    cat ~/diskcopy.log | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat $JOBLOG | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
 cp -av $SOURCE_DRV/largefiles $TARGET_DRV
-echo "largefiles completed.">>~/diskcopy.log
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcopy largefiles completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+df -T>>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    cat ~/diskcopy.log | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat $JOBLOG | mail -s "[cron-log][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
