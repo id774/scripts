7904304f1881d4c9b4fc93f333a6c35a2dd5f3c4
id774 <idnanashi@gmail.com>
2009-07-27 07:01:25 +0900

    #300 Upgrade clamav in auto apt upgrade job

--- dev/update_setup_v0.2.sh
+++ dev/update_setup_v0.2.sh
@@ -84,6 +84,9 @@ sudo chown -R root:root /root/bin
 # ClamAV Auto Upgrade Job
 sudo cp $SCRIPTS/cron/bin/clamav_upgrade.sh /root/bin/clamav_upgrade.sh
 sudo chmod 700 /root/bin/clamav_upgrade.sh
+sudo cp $SCRIPTS/cron/bin/auto-upgrade /etc/cron.daily/auto-upgrade
+sudo vim /etc/cron.daily/auto-upgrade
+sudo chmod 700 /etc/cron.daily/auto-upgrade
 sudo cp $SCRIPTS/cron/etc/clamscan_exclude /root/bin/clamscan_exclude
 sudo vim /root/bin/clamscan_exclude
 sudo chmod 600 /root/bin/clamscan_exclude
