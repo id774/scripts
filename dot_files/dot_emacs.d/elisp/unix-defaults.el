67b5a14348e0e3e0020ea3fffc38c88573db2088
id774 <idnanashi@gmail.com>
2009-12-01 15:57:16 +0900

    #337 Convert tabs into spaces elisp file

--- dot_files/dot_emacs.d/elisp/unix-defaults.el
+++ dot_files/dot_emacs.d/elisp/unix-defaults.el
@@ -45,13 +45,13 @@
                 'japanese-jisx0208
                 '("Hiragino Maru Gothic Pro" . "iso10646-1"))
               (setq face-font-rescale-alist
-	            '(("^-apple-hiragino.*" . 1.2)
-	              (".*osaka-bold.*" . 1.2)
-	              (".*osaka-medium.*" . 1.2)
-	              (".*courier-bold-.*-mac-roman" . 1.0)
-	              (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-	              (".*monaco-bold-.*-mac-roman" . 0.9)
-	              ("-cdac$" . 1.3)))))))
+                '(("^-apple-hiragino.*" . 1.2)
+                (".*osaka-bold.*" . 1.2)
+                (".*osaka-medium.*" . 1.2)
+                (".*courier-bold-.*-mac-roman" . 1.0)
+                (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+                (".*monaco-bold-.*-mac-roman" . 0.9)
+                ("-cdac$" . 1.3)))))))
       )
 ;; フレーム設定
     (setq default-frame-alist
