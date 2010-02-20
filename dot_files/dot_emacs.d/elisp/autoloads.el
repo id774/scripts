edfe7b7a7f879e9bdaeba2b6c91f8d8c20acda02
id774 <idnanashi@gmail.com>
2010-02-20 14:52:56 +0900

    #386 Add Anything.el

--- dot_files/dot_emacs.d/elisp/autoloads.el
+++ dot_files/dot_emacs.d/elisp/autoloads.el
@@ -291,6 +291,19 @@
 (setq key-chord-two-keys-delay 0.04)
 (key-chord-mode 1)
 
+;; Anything.el
+(require 'anything-config)
+(setq anything-sources (list anything-c-source-buffers
+                             anything-c-source-bookmarks
+                             anything-c-source-recentf
+                             anything-c-source-file-name-history
+                             anything-c-source-locate))
+(define-key anything-map (kbd "C-p") 'anything-previous-line)
+(define-key anything-map (kbd "C-n") 'anything-next-line)
+(define-key anything-map (kbd "C-v") 'anything-next-source)
+(define-key anything-map (kbd "M-v") 'anything-previous-source)
+(global-set-key (kbd "C-;") 'anything)
+
 ;; UNIX·ÏÀßÄê
 (load-p "unix-defaults")
 
