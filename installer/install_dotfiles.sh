f8aefd5a016c7bda26e34c77b26c36af00239cc3
solaris <solaris@vmsolaris1.(none)>
2010-03-02 00:58:45 +0900

    #408 Correspond to zsh on solaris

--- installer/install_dotfiles.sh
+++ installer/install_dotfiles.sh
@@ -20,12 +20,12 @@ do
   test -d /Users/demo         && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /Users/demo/.$DOT_FILES
   test -d /Users/work         && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /Users/work/.$DOT_FILES
   test -d /root               && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /root/.$DOT_FILES
-  test -d /home/ubuntu        && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/ubuntu/.$DOT_FILES
-  test -d /home/debian        && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/debian/.$DOT_FILES
-  test -d /home/centos        && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/centos/.$DOT_FILES
+  test -d /export/home/solaris        && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /export/home/solaris/.$DOT_FILES
+  test -d /export/home/debian        && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /export/home/debian/.$DOT_FILES
+  test -d /export/home/centos        && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /export/home/centos/.$DOT_FILES
   test -d /etc/skel           && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /etc/skel/.$DOT_FILES
-  test -d /home/plagger       && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/plagger/.$DOT_FILES
-  test -d /home/testuser      && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/testuser/.$DOT_FILES
+  test -d /export/home/plagger       && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /export/home/plagger/.$DOT_FILES
+  test -d /export/home/testuser      && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /export/home/testuser/.$DOT_FILES
   test -d /var/lib/postgresql && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /var/lib/postgresql/.$DOT_FILES
 done
 
@@ -37,12 +37,12 @@ do
   test -f /Users/demo/$DOT_FILES         && sudo rm -vf /Users/demo/.$DOT_FILES
   test -f /Users/work/$DOT_FILES         && sudo rm -vf /Users/work/.$DOT_FILES
   test -f /root/$DOT_FILES               && sudo rm -vf /root/.$DOT_FILES
-  test -f /home/ubuntu/$DOT_FILES        && sudo rm -vf /home/ubuntu/.$DOT_FILES
-  test -f /home/debian/$DOT_FILES        && sudo rm -vf /home/debian/.$DOT_FILES
-  test -f /home/centos/$DOT_FILES        && sudo rm -vf /home/centos/.$DOT_FILES
+  test -f /export/home/solaris/$DOT_FILES        && sudo rm -vf /export/home/solaris/.$DOT_FILES
+  test -f /export/home/debian/$DOT_FILES        && sudo rm -vf /export/home/debian/.$DOT_FILES
+  test -f /export/home/centos/$DOT_FILES        && sudo rm -vf /export/home/centos/.$DOT_FILES
   test -f /etc/skel/$DOT_FILES           && sudo rm -vf /etc/skel/.$DOT_FILES
-  test -f /home/plagger/$DOT_FILES       && sudo rm -vf /home/plagger/.$DOT_FILES
-  test -f /home/testuser/$DOT_FILES      && sudo rm -vf /home/testuser/.$DOT_FILES
+  test -f /export/home/plagger/$DOT_FILES       && sudo rm -vf /export/home/plagger/.$DOT_FILES
+  test -f /export/home/testuser/$DOT_FILES      && sudo rm -vf /export/home/testuser/.$DOT_FILES
   test -f /var/lib/postgresql/$DOT_FILES && sudo rm -vf /var/lib/postgresql/.$DOT_FILES
 done
 
