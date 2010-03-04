5133b6490918d402b88640a18d9921d5ac25e40a
id774 <idnanashi@gmail.com>
2010-03-04 12:54:48 +0900

    #418 Refactoring

--- gitpull-all.sh
+++ gitpull-all.sh
@@ -2,38 +2,38 @@
 
 gitpull() {
     if [ -d $HOME/local/$1/$3 ]; then
-        cd ~/local/$1/$3
+        cd $HOME/local/$1/$3
         git pull
     else
-        cd ~/local/$1
+        cd $HOME/local/$1
         git clone git://github.com/$2/$3.git
     fi
 }
 
 assembla_git() {
     if [ -d $HOME/local/$1/$2 ]; then
-        cd ~/local/$1/$2
+        cd $HOME/local/$1/$2
         git pull
     else
-        cd ~/local/$1
+        cd $HOME/local/$1
         git clone git://git.assembla.com/$2.git
     fi
 }
 
 emacswiki_get() {
     test -d $HOME/local/$1 || mkdir -p $HOME/local/$1
-    cd ~/local/$1
+    cd $HOME/local/$1
     test -f $1.el && rm $1.el
     wget http://www.emacswiki.org/emacs/download/$1.el
-    diff ~/local/$1/$1.el ~/.emacs.d/elisp/3rd-party/$1.el
+    diff $HOME/local/$1/$1.el $HOME/.emacs.d/elisp/3rd-party/$1.el
 }
 
 repoorcz_pull() {
     if [ -d $HOME/local/$1/$2 ]; then
-        cd ~/local/$1/$2
+        cd $HOME/local/$1/$2
         $1 pull
     else
-        cd ~/local/$1
+        cd $HOME/local/$1
         git clone git://repo.or.cz/$2.git
     fi
 }
