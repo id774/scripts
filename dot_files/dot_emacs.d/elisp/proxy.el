72eea8c3060e8d312c745292fa3a64bf140834f8
id774 <idnanashi@gmail.com>
2010-03-10 00:45:26 +0900

    #437 Implement function global-proxy-use-toggle

--- dot_files/dot_emacs.d/elisp/proxy.el
+++ dot_files/dot_emacs.d/elisp/proxy.el
@@ -8,6 +8,16 @@
 (defvar global-proxy-user nil)
 (defvar global-proxy-password nil)
 
+;; Proxyのオンオフを切り替えるする関数
+(defun global-proxy-use-toggle () ""
+  (interactive)
+  (setq global-proxy-use
+        (not global-proxy-use))
+  (message "%s %s"
+           "Use Proxy:"
+           (if global-proxy-use
+               "on" "off")))
+
 ;; Local Variables:
 ;; mode : emacs-lisp
 ;; coding : euc-jp-unix
