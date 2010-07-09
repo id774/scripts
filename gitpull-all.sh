75a7256f728cce8a25369a84e1fa1b07449c3018
id774 <idnanashi@gmail.com>
2010-07-09 19:13:29 +0900

    #497 Reset working directory option before git pull

--- gitpull-all.sh
+++ gitpull-all.sh
@@ -3,6 +3,7 @@
 gitpull() {
     if [ -d $HOME/local/$1/$3 ]; then
         cd $HOME/local/$1/$3
+        test -n "$1" && git reset --hard
         git pull
     else
         cd $HOME/local/$1
@@ -13,6 +14,7 @@ gitpull() {
 assembla_git() {
     if [ -d $HOME/local/$1/$2 ]; then
         cd $HOME/local/$1/$2
+        test -n "$1" && git reset --hard
         git pull
     else
         cd $HOME/local/$1
@@ -23,6 +25,7 @@ assembla_git() {
 debian_monthly_report() {
     if [ -d $HOME/local/$1/$2 ]; then
         cd $HOME/local/$1/$2
+        test -n "$1" && git reset --hard
         $1 pull
     else
         cd $HOME/local/$1
@@ -43,6 +46,7 @@ emacswiki_get() {
 repoorcz_pull() {
     if [ -d $HOME/local/$1/$2 ]; then
         cd $HOME/local/$1/$2
+        test -n "$1" && git reset --hard
         $1 pull
     else
         cd $HOME/local/$1
@@ -51,30 +55,30 @@ repoorcz_pull() {
 }
 
 anything_get_all() {
-    repoorcz_pull git anything-config
+    repoorcz_pull git anything-config $*
     #emacswiki_get anything
 }
 
 assembla_git_all() {
-    assembla_git git 774-gthumb
+    assembla_git git 774-gthumb $*
 }
 
 gitpull_all() {
-    gitpull github id774 id774.github.com
-    gitpull github id774 termtter-plugins
-    gitpull github jugyo termtter
-    gitpull github hayamiz twittering-mode
-    gitpull github miyagawa plagger
-    gitpull github m2ym auto-complete
-    gitpull github mooz js2-mode
+    gitpull github id774 id774.github.com $*
+    gitpull github id774 termtter-plugins $*
+    gitpull github jugyo termtter $*
+    gitpull github hayamiz twittering-mode $*
+    gitpull github miyagawa plagger $*
+    gitpull github m2ym auto-complete $*
+    gitpull github mooz js2-mode $*
 }
 
 main() {
-    debian_monthly_report git monthly-report
-    assembla_git_all
-    anything_get_all
-    gitpull_all
+    debian_monthly_report git monthly-report $*
+    assembla_git_all $*
+    anything_get_all $*
+    gitpull_all $*
 }
 
 ping -c 1 -i 3 google.com > /dev/null 2>&1 || exit 1
-main
+main $*
