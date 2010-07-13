077fbcd41051de8e57030ed7cdde8be2f386335f
id774 <idnanashi@gmail.com>
2010-07-13 09:31:36 +0900

    #499 Add the user to deploy dotfiles

--- installer/install_dotfiles.sh
+++ installer/install_dotfiles.sh
@@ -29,11 +29,12 @@ do
   test -d /Users/demo          && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /Users/demo/.$DOT_FILES
   test -d /Users/work          && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /Users/work/.$DOT_FILES
   test -d /root                && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /root/.$DOT_FILES
-  test -d /home/ubuntu         && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/ubuntu/.$DOT_FILES
   test -d /home/debian         && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/debian/.$DOT_FILES
+  test -d /home/ubuntu         && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/ubuntu/.$DOT_FILES
   test -d /home/centos         && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/centos/.$DOT_FILES
   test -d /etc/skel            && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /etc/skel/.$DOT_FILES
   test -d /home/plagger        && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/plagger/.$DOT_FILES
+  test -d /home/twitter        && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/twitter/.$DOT_FILES
   test -d /home/testuser       && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /home/testuser/.$DOT_FILES
   test -d /var/lib/postgresql  && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /var/lib/postgresql/.$DOT_FILES
   test -d /export/home/solaris && cp      $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES /export/home/solaris/.$DOT_FILES
