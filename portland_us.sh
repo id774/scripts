ba9db5ff48edf4a714566aac49a4576f6955035a
id774 <idnanashi@gmail.com>
2010-07-27 03:42:50 +0900

    #510 Require package of xdg-user-dirs-gtk

--- portland_us.sh
+++ portland_us.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [ `aptitude search xdg-user-dirs-gtk | awk '/^i/' | wc -l` = 0 ]; then
+    sudo aptitude install xdg-user-dirs-gtk
+fi
+
 LANG=C xdg-user-dirs-gtk-update
 vim $HOME/.config/user-dirs.dirs
