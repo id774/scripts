8a122a95214392f91881a255e084296caca14110
id774 <idnanashi@gmail.com>
2010-08-08 02:14:17 +0900

    #524 Use DejaVu font for emacs on Ubuntu Lucid

--- dot_files/dot_emacs.d/elisp/unix-defaults.el
+++ dot_files/dot_emacs.d/elisp/unix-defaults.el
@@ -68,6 +68,7 @@
                             '(height . 40) ; 起動時のサイズ（縦）
                             )))
         (set-default-font "Bitstream Vera Sans Mono-8")
+        ;;(set-default-font "DejaVu Sans Mono-8") ;; Ubuntu Lucid
         (set-fontset-font (frame-parameter nil 'font)
                           'japanese-jisx0208
                           '("VL ゴシック" . "unicode-bmp"))
