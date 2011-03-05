20e8ad325aa7f2ea180bfb0ec0dd2f4ed5c5781a
id774 <idnanashi@gmail.com>
2011-03-06 00:12:53 +0900

    #624 Add private plagger plugins

--- installer/install_plagger_plugins.sh
+++ installer/install_plagger_plugins.sh
@@ -5,18 +5,25 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.2 3/6,2011
+#       Add private plagger plugins.
 #  v0.1 8/1,2010
 #       First.
 ########################################################################
 
 # Install plagger plugin
 export plagger_dir=$HOME/tmp
+
 test -d /usr/local/share/perl/5.8.8/Plagger && export plagger_dir=/usr/local/share/perl/5.8.8/Plagger # Ubuntu 8.04
 test -d /usr/local/share/perl/5.10.0/Plagger && export plagger_dir=/usr/local/share/perl/5.10.0/Plagger # Debian 5.0
 test -d /usr/local/share/perl/5.10.1/Plagger && export plagger_dir=/usr/local/share/perl/5.10.1/Plagger # Ubuntu 10.04
+test -d /usr/local/share/perl/Plagger && export plagger_dir=/usr/local/share/perl/Plagger
+
 sudo cp -Rv $SCRIPTS/cron/plagger/assets/plugins/* $plagger_dir/assets/plugins/
 sudo cp -Rv $SCRIPTS/cron/plagger/plugins/publish/* $plagger_dir/Plugin/Publish/
 sudo cp -Rv $SCRIPTS/cron/plagger/plugins/filter/* $plagger_dir/Plugin/Filter/
 sudo cp -Rv $SCRIPTS/cron/plagger/plugins/customfeed/* $plagger_dir/Plugin/CustomFeed/
 sudo cp -Rv $SCRIPTS/cron/plagger/plugins/subscription/* $plagger_dir/Plugin/Subscription/
 
+sudo cp -Rv $PRIVATE/cron/plagger/plugins/filter/* $plagger_dir/Plugin/Filter/
+sudo cp -Rv $PRIVATE/cron/plagger/assets/plugins/* $plagger_dir/assets/plugins/
