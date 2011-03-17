bd35dfacb51ecb0ac81e63d2af08f7ce56be9dcc
id774 <idnanashi@gmail.com>
2011-03-17 16:03:02 +0900

    #639 Emacs 23.3

--- installer/install_emacs.sh
+++ installer/install_emacs.sh
@@ -4,10 +4,13 @@
 # Install emacs
 #  $1 = version
 #  $2 = prefix
-#  $3 = not save to src
+#  $3 = build options
+#  $4 = not save to src
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.3 3/17,2011
+#       Emacs 23.3.
 #  v0.2 9/16,2010
 #       Refactoring.
 #  v0.1 9/14,2010
@@ -16,9 +19,11 @@
 
 setup_environment() {
     test -n "$1" && EMACS_VERSION=$1
-    test -n "$1" || EMACS_VERSION=23.2
-    test -n "$3" || SUDO=sudo
-    test -n "$3" && SUDO=
+    test -n "$1" || EMACS_VERSION=23.3
+    test -n "$3" && BUILD_OPTIONS=$3
+    test -n "$3" || BUILD_OPTIONS=--with-ns
+    test -n "$4" || SUDO=sudo
+    test -n "$4" && SUDO=
     case $OSTYPE in
       *darwin*)
         OPTIONS=-pR
@@ -38,10 +43,18 @@ save_sources() {
     sudo chown -R $OWNER /usr/local/src/emacs/emacs-$EMACS_VERSION
 }
 
+adding_patches() {
+    wget http://cloud.github.com/downloads/typester/emacs/feature-fullscreen.patch
+    patch -p < feature-fullscreen.patch
+    wget http://cloud.github.com/downloads/typester/emacs/fix-shiftmodifier-with-ime.patch
+    patch -p1 < fix-shiftmodifier-with-ime.patch
+}
+
 make_and_install() {
     cd emacs-$EMACS_VERSION
-    test -n "$2" || ./configure --without-x --prefix=$HOME/local/emacs/$EMACS_VERSION
-    test -n "$2" && ./configure --without-x --prefix=$2
+    adding_patches $*
+    test -n "$2" || ./configure --without-x $3 --prefix=$HOME/local/emacs/$EMACS_VERSION
+    test -n "$2" && ./configure --without-x $3 --prefix=$2
     make
     $SUDO make install
     cd ..
@@ -54,7 +67,7 @@ get_emacs() {
     test -f emacs-$EMACS_VERSION.tar.bz2 || exit 1
     tar xjvf emacs-$EMACS_VERSION.tar.bz2
     test "$2" = "sourceonly" || make_and_install $*
-    test -n "$3" || save_sources
+    test -n "$4" || save_sources
     cd ..
     rm -rf install_emacs
 }
