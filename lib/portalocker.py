4c93d8273ca4405ff4bf1bfa3a16166316986439
id774 <idnanashi@gmail.com>
2011-04-19 13:53:08 +0900

    #664 Add PortaLocker

---
+++ lib/portalocker.py
@@ -0,0 +1,29 @@
+import os, fcntl
+
+if os.name == 'nt':
+    # Require 'win32all'
+    import win32con, win32file, pywintypes
+    LOCK_EX = win32con.LOCKFILE_EXCLUSIVE_LOCK
+    LOCK_SH = 0
+    LOCK_NB = win32con.LOCKFILE_FAIL_IMMEDIATELY
+    __overlapped = pywintypes.OVERLAPPED( )
+
+    def lock(file, flags):
+        hfile = win32file._get_osfhandle(file.fileno( ))
+        win32file.LockFileEx(hfile, flags, 0, 0xffff0000, __overlapped)
+
+    def unlock(file):
+        hfile = win32file._get_osfhandle(file.fileno( ))
+        win32file.UnlockFileEx(hfile, 0, 0xffff0000, __overlapped)
+
+elif os.name == 'posix':
+    from fcntl import LOCK_EX, LOCK_SH, LOCK_NB
+    def lock(file, flags):
+        fcntl.flock(file.fileno( ), flags)
+
+    def unlock(file):
+        fcntl.flock(file.fileno( ), fcntl.LOCK_UN)
+
+else:
+    raise RuntimeError("Only defined for nt and posix platforms")
+
---
+++ portalocker_sample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys, os
+p = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib')
+if not p in sys.path:
+    sys.path.append(p)
+try:
+    p = os.path.join(os.environ['SCRIPTS'], 'lib')
+    if not p in sys.path:
+        sys.path.append(p)
+except KeyError:
+    pass
+
+import portalocker
+
+afile = open("portalocker_sample.py")
+portalocker.lock(afile, portalocker.LOCK_EX)
