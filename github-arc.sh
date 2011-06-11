0fd93e5703f7253fa221fbb59b76975c4d0816ea
id774 <idnanashi@gmail.com>
2011-06-12 03:28:15 +0900

    #725 Add dot_zsh repository to github archive

--- github-arc.sh
+++ github-arc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 test -d $HOME/local/github/scripts && cd $HOME/local/github/scripts && git pull && cd -
+test -d $HOME/local/github/dot_zsh && cd $HOME/local/github/dot_zsh && git pull && cd -
 test -d $HOME/local/github/dot_emacs && cd $HOME/local/github/dot_emacs && git pull && cd -
 test -d $HOME/local/github/termtter-plugins && cd $HOME/local/github/termtter-plugins && git pull && cd -
 test -f $HOME/local/github/github.tar.gz && rm -f $HOME/local/github/github.tar.gz
