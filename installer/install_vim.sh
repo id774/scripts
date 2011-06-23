9ab67666e8f4148abe23a87729a914bfd8295cf8
id774 <idnanashi@gmail.com>
2011-06-23 14:13:51 +0900

    #741 Downgrade to vim 7.2

--- installer/install_vim.sh
+++ installer/install_vim.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 #
 ########################################################################
-# Install Vim
+# Install Vim 7.2
 #  $1 = version
 #  $2 = prefix
 #  $3 = not save to src
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
-#  v1.0 9/14,2010
-#       Renew to update to vim 7.3.
+#  v1.7 6/23,2011
+#       Fix prefix bug.
+#  v1.6 9/14,2010
+#       Install to local.
+#  v1.5 8/29,2010
+#       Update to 7.2.446.
+#  v1.4 3/7,2010
+#       Refactoring and update to 7.2.385.
+#  v1.3 2/20,2010
+#       Refactoring.
+#  v1.2 3/10,2009
+#       Switch wget to curl.
+#  v1.1 10/3,2008
+#       Implement local build from source.
+#  v1.0 8/15,2008
+#       Stable.
 ########################################################################
 
 setup_environment() {
@@ -28,18 +42,18 @@ setup_environment() {
 save_sources() {
     test -d /usr/local/src/vim && sudo rm -rf /usr/local/src/vim
     test -d /usr/local/src/vim || sudo mkdir -p /usr/local/src/vim
-    sudo cp vim-7.3.tar.bz2 /usr/local/src/vim
-    sudo cp vim-7.3-extra.tar.gz /usr/local/src/vim
-    sudo cp vim-7.3-lang.tar.gz /usr/local/src/vim
-    sudo cp -a vim73 /usr/local/src/vim
+    sudo cp vim-7.2.tar.bz2 /usr/local/src/vim
+    sudo cp vim-7.2-extra.tar.gz /usr/local/src/vim
+    sudo cp vim-7.2-lang.tar.gz /usr/local/src/vim
+    sudo cp -a vim72 /usr/local/src/vim
     sudo cp -a patches /usr/local/src/vim
     sudo chown -R $OWNER /usr/local/src/vim
 }
 
 build_and_install() {
-    cd vim73
-    cat ../patches/7.3.* | patch -p0
-    test -n "$1" || ./configure --enable-multibyte --enable-xim --enable-fontset --with-features=big --enable-perlinterp --enable-rubyinterp --enable-pythoninterp --prefix=$HOME/local/vim/7.3
+    cd vim72
+    cat ../patches/7.2.* | patch -p0
+    test -n "$1" || ./configure --enable-multibyte --enable-xim --enable-fontset --with-features=big --enable-perlinterp --enable-rubyinterp --enable-pythoninterp --prefix=$HOME/local/vim/7.2
     test -n "$1" && (test -n "$2" || ./configure --enable-multibyte --enable-xim --enable-fontset --with-features=big --enable-perlinterp --enable-rubyinterp --enable-pythoninterp --prefix=$1)
     test -n "$1" && test -n "$2" && ./configure --enable-multibyte --enable-xim --enable-fontset --with-features=big --enable-perlinterp --enable-rubyinterp --enable-pythoninterp --prefix=$1 --with-local-dir=$2
     make
@@ -47,18 +61,26 @@ build_and_install() {
 }
 
 get_source_and_install() {
-    curl -O ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
-    curl -O ftp://ftp.vim.org/pub/vim/extra/vim-7.3-extra.tar.gz
-    curl -O ftp://ftp.vim.org/pub/vim/extra/vim-7.3-lang.tar.gz
-    tar xjvf vim-7.3.tar.bz2
-    tar xzvf vim-7.3-extra.tar.gz
-    tar xzvf vim-7.3-lang.tar.gz
+    curl -O ftp://ftp.vim.org/pub/vim/unix/vim-7.2.tar.bz2
+    curl -O ftp://ftp.vim.org/pub/vim/extra/vim-7.2-extra.tar.gz
+    curl -O ftp://ftp.vim.org/pub/vim/extra/vim-7.2-lang.tar.gz
+    tar xjvf vim-7.2.tar.bz2
+    tar xzvf vim-7.2-extra.tar.gz
+    tar xzvf vim-7.2-lang.tar.gz
     mkdir patches
     cd patches
-    curl -O 'ftp://ftp.vim.org/pub/vim/patches/7.3/7.3.[001-004]'
+    curl -O ftp://ftp.vim.org/pub/vim/patches/7.2/7.2.001-100.gz
+    gunzip 7.2.001-100.gz
+    curl -O ftp://ftp.vim.org/pub/vim/patches/7.2/7.2.101-200.gz
+    gunzip 7.2.101-200.gz
+    curl -O ftp://ftp.vim.org/pub/vim/patches/7.2/7.2.201-300.gz
+    gunzip 7.2.201-300.gz
+    curl -O ftp://ftp.vim.org/pub/vim/patches/7.2/7.2.301-400.gz
+    gunzip 7.2.301-400.gz
+    curl -O 'ftp://ftp.vim.org/pub/vim/patches/7.2/7.2.[401-446]'
     cd ../
     test -n "$3" || save_sources
-    build_and_install $1
+    build_and_install $*
     cd ../../
 }
 
