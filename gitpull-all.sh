6a837c02ed5f17d011fff1b55b24b7f9704bb492
id774 <idnanashi@gmail.com>
2011-11-09 15:46:57 +0900

    836 Remove old repository

--- gitpull-all.sh
+++ gitpull-all.sh
@@ -88,7 +88,6 @@ assembla_git_all() {
 }
 
 gitpull_all() {
-    gitpull github id774 id774.github.com $*
     gitpull github id774 dot_zsh $*
     gitpull github id774 dot_emacs $*
     gitpull github id774 termtter-plugins $*
