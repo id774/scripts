8b90bb0dbb46563c017c27401390a89898a06fd7
id774 <idnanashi@gmail.com>
2011-11-17 10:00:49 +0900

    841 Purge ubuntu old kernels

---
+++ purge_old_kernel_of_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
+LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
+METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
+OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
+
+sudo aptitude purge $OLDKERNELS
