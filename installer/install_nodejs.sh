aa66b1829165f92de8a597e77a9a2d13c3f65068
id774 <idnanashi@gmail.com>
2011-11-22 14:51:59 +0900

    843 Add node.js installer

---
+++ installer/install_nodejs.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+########################################################################
+# Install Node.js
+#  $1 = version
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v0.1 11/22,2011
+#       First.
+########################################################################
+
+setup_environment() {
+    test -n "$1" || VERSION=0.6.2
+    test -n "$1" && VERSION=$1
+    NODE=node-v$VERSION
+}
+
+save_sources() {
+    sudo mkdir -p /usr/local/src/node.js
+    sudo cp -av $NODE /usr/local/src/node.js
+    sudo chown -R root:root /usr/local/src/node.js
+}
+
+install_node() {
+    setup_environment $*
+
+    mkdir install_node
+    cd install_node
+
+    wget http://nodejs.org/dist/$NODE.tar.gz
+    tar xzvf $NODE.tar.gz
+    cd $NODE
+    ./configure
+    make
+    sudo make install
+    cd ..
+    test -n "$2" || save_sources
+    cd ..
+    rm -rf install_node
+}
+
+ping -c 1 id774.net > /dev/null 2>&1 || exit 1
+install_node $*
