9cb84ad963c4af3f79d1e2e9d489a0a5d0de6649
id774 <idnanashi@gmail.com>
2012-04-16 14:33:19 +0900

    934 Shape up unnecessary funtion for debian setup script

--- installer/debian_batch_installers.sh
+++ installer/debian_batch_installers.sh
@@ -5,15 +5,13 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.3 4/16,2012
+#       Shape up unnecessary functions.
 #  v0.2 1/23,2012
 #       Reconstruction CoffeeScript Environment.
 #  v0.1 9/28,2011
 #       First version.
 ########################################################################
-
-install_pppconfig() {
-    $SCRIPTS/installer/install_pppconfig.sh
-}
 
 set_zsh_to_default() {
     chsh -s /bin/zsh
@@ -26,48 +24,6 @@ fix_apt_listbugs() {
 
 configure_exim4() {
     sudo dpkg-reconfigure exim4-config
-}
-
-install_paco() {
-    $SCRIPTS/installer/install_paco.sh
-}
-
-build_emacs() {
-    $SCRIPTS/installer/install_emacs.sh 23.3 /opt/emacs/23.3
-    $SCRIPTS/installer/install_emacs_w3m.sh 23.3 /opt/emacs/23.3
-    sudo ln -fs /opt/emacs/23.3/bin/emacs /opt/bin/emacs
-}
-
-install_navi2ch() {
-    $SCRIPTS/installer/install_navi2ch.sh
-}
-
-build_vim() {
-    $SCRIPTS/installer/install_ncurses.sh
-    $SCRIPTS/installer/install_vim.sh
-}
-
-install_iptables() {
-    $SCRIPTS/installer/install_iptables.sh debian
-}
-
-install_ruby_and_rails() {
-    $SCRIPTS/installer/install_ruby_and_rails.sh
-}
-
-install_nodejs_and_coffeescript() {
-    $SCRIPTS/installer/install_nodejs.sh
-    $SCRIPTS/installer/install_coffeescript.sh
-    $SCRIPTS/installer/install_npm.sh
-}
-
-install_python() {
-    $SCRIPTS/installer/install_python.sh $1 /opt/python/$1
-    #$SCRIPTS/config/update-alternatives-python.sh
-}
-
-install_python_framework() {
-    $SCRIPTS/installer/install_python_framework.sh
 }
 
 install_dot_vim() {
@@ -92,10 +48,6 @@ install_dot_emacs() {
     $HOME/local/github/dot_emacs/install_dotemacs.sh
 }
 
-install_mew() {
-    $SCRIPTS/installer/install_mew.sh /opt/emacs/23.3/bin/emacs
-}
-
 install_dot_files() {
     $SCRIPTS/installer/install_dotfiles.sh
 }
@@ -108,37 +60,8 @@ install_truecrypt() {
     $SCRIPTS/installer/install_truecrypt.sh $1
 }
 
-install_sqlite() {
-    $SCRIPTS/installer/install_sqlite.sh
-}
-
-install_svn() {
-    $SCRIPTS/installer/install_debian_svn.sh
-}
-
 sshfs_modules() {
     sudo vi /etc/modules
-}
-
-configure_samba() {
-    sudo update-rc.d -f samba remove
-    sudo cp $SCRIPTS/etc/smb.conf /etc/samba/smb.conf
-    sudo smbpasswd -a $USER
-}
-
-install_postgres() {
-    $SCRIPTS/installer/install_postgres.py install
-}
-
-install_mysql() {
-    $SCRIPTS/installer/install_mysql.py install
-}
-
-install_kvm() {
-    if [ `egrep '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l` != 0 ]; then
-        sudo addgroup $USER libvirtd
-        sudo addgroup $USER kvm
-    fi
 }
 
 configure_sysstat() {
@@ -171,44 +94,12 @@ setup_rc_local() {
     $SCRIPTS/installer/install_rclocal.sh
 }
 
-add_blacklist() {
-    test -r /etc/modprobe.d/blacklist && \
-      sudo vi /etc/modprobe.d/blacklist
-    test -r /etc/modprobe.d/blacklist.conf && \
-      sudo vi /etc/modprobe.d/blacklist.conf
-}
-
-install_termtter_plugins() {
-    cd $HOME/local/github
-    git clone git://github.com/id774/termtter-plugins.git
-    cd
-    ln -s $HOME/local/github/termtter-plugins
-    $PRIVATE/installer/install_dottermtter.sh
-    $SCRIPTS/installer/install_termtter_plugins.sh
-}
-
-install_plagger_plugins() {
-    $SCRIPTS/installer/install_plagger_plugins.sh
-}
-
 setup_ntp() {
     sudo vi /etc/ntp.conf
 }
 
 setup_fail2ban() {
     sudo vi /etc/fail2ban/jail.conf
-}
-
-install_munin() {
-    $SCRIPTS/installer/install_munin.sh
-}
-
-get_share_documents() {
-    wget http://id774.net/archive/share-documents.tar.gz
-    sudo tar xzvf share-documents.tar.gz -C /usr/local/share
-    rm share-documents.tar.gz
-    sudo chmod -R 755 /usr/local/share/share-documents
-    ln -s /usr/local/share/share-documents $HOME/share
 }
 
 change_default() {
@@ -219,6 +110,7 @@ change_default() {
     sudo vi /etc/ssh/sshd_config
     sudo vi /etc/pam.d/sshd
     sudo vi /etc/pam.d/login
+    sudo vi /etc/motd
     sudo vi /etc/fstab
     sudo vi /etc/hosts
 }
@@ -251,45 +143,23 @@ erase_history() {
 }
 
 operation() {
-    install_pppconfig
     set_zsh_to_default
     fix_apt_listbugs
     configure_exim4
-    #install_paco
-    #build_emacs
-    #install_navi2ch
-    #build_vim
-    install_iptables
-    #install_ruby_and_rails
-    #install_nodejs_and_coffeescript
-    #install_python
-    #install_python_framework
     install_dot_vim
     install_dot_zsh
     install_dot_emacs
-    #install_mew
     install_dotfiles
     install_truecrypt linux-`uname -m`
-    #install_sqlite
-    #install_svn
     sshfs_modules
-    #configure_samba
-    #install_postgres
-    #install_mysql
-    #install_kvm
     configure_sysstat
     configure_hddtemp
     configure_smartmontools
     setup_sysadmin_scripts
     setup_web
     setup_rc_local
-    add_blacklist
-    #install_termtter_plugins
-    #install_plagger_plugins
     setup_ntp
     setup_fail2ban
-    #install_munin
-    #get_share_documents
     change_default
     setup_grub
     setup_group_and_passwd
