3847b954c14188dcf99f89eb824d825c5a4ae539
id774 <idnanashi@gmail.com>
2012-04-16 14:38:47 +0900

    934 Shape up unnecessary funtion for debian setup script

--- installer/debian_env.sh
+++ installer/debian_env.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.2 4/16,2012
+#       Shape up unnecessary functions.
 #  v0.1 9/28,2011
 #       First version.
 ########################################################################
@@ -19,12 +21,6 @@ create_admin_group() {
     sudo groupadd wheel
 }
 
-flash_udev_net_rules() {
-    test -f /etc/udev/rules.d/70-persistent-net.rules && \
-      sudo mv -f /etc/udev/rules.d/70-persistent-net.rules \
-      /etc/udev/rules.d/70-persistent-net.rules.old
-}
-
 setup_apt_source() {
     SOURCESLIST=sources-$DISTRIB_CODENAME.list
     sudo cp $SCRIPTS/etc/$SOURCESLIST /etc/apt/sources.list
@@ -32,24 +28,9 @@ setup_apt_source() {
     sudo apt-get update
 }
 
-ubuntu_ja_gpg() {
-    eval `cat /etc/lsb-release`
-    wget -q http://www.ubuntulinux.jp/ubuntu-ja-archive-keyring.gpg -O- \
-      | sudo apt-key add -
-    sudo wget http://www.ubuntulinux.jp/sources.list.d/$DISTRIB_CODENAME.list -O \
-      /etc/apt/sources.list.d/ubuntu-ja.list
-    sudo apt-get update
-}
-
 chromium_daily_gpg() {
     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com \
       0xfbef0d696de1c72ba5a835fe5a9bf3bb4e5e17b5
-}
-
-stop_services() {
-    sudo update-rc.d -f cupsys remove
-    sudo update-rc.d -f hplip remove
-    sudo apt-get remove apt-index-watcher
 }
 
 opt_bin_and_sbin() {
@@ -64,7 +45,6 @@ home_permission() {
 operation() {
     show_hw_info
     create_admin_group
-    flash_udev_net_rules
 
     # tune2fs
     $SCRIPTS/installer/setup_tune2fs.sh
@@ -72,10 +52,8 @@ operation() {
     test -f /etc/lsb-release && DISTRIB_CODENAME=lucid
     test -f /etc/lsb-release || DISTRIB_CODENAME=squeeze
     setup_apt_source
-    #ubuntu_ja_gpg
     #chromium_daily_gpg
 
-    stop_services
     opt_bin_and_sbin
     home_permission
 }
