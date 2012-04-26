9ab98da2f3752369319041df3bc9e3a40ce740c2
id774 <idnanashi@gmail.com>
2012-04-26 10:38:07 +0900

    943 Add jade

--- installer/install_npm.sh
+++ installer/install_npm.sh
@@ -16,6 +16,7 @@ install_npm() {
     sudo $NPM install js2coffee -g
     sudo $NPM install mysql -g
     sudo $NPM install express -g
+    sudo $NPM install jade -g
     sudo $NPM install ejs -g
     sudo $NPM install less -g
     sudo $NPM install uglify-js -g
