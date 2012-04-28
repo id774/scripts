e6bcfb0afb014241c325a91343d1f7e51794e855
id774 <idnanashi@gmail.com>
2012-04-28 19:04:58 +0900

    949 Correspond to alias of hostname

--- installer/setup_tune2fs.sh
+++ installer/setup_tune2fs.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.2 4/28,2012
+#       Update for alias of hostname.
 #  v0.1 9/26,2011
 #       First version.
 ########################################################################
@@ -22,63 +24,63 @@ setup_tune2fs() {
     test -b /dev/sda9  && sudo tune2fs -i 0 -c 0 -m 1 /dev/sda9
     test -b /dev/sda10 && sudo tune2fs -i 0 -c 0 -m 1 /dev/sda10
 
-    test -b /dev/mapper/`/bin/hostname`-root && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname`-root
-    test -b /dev/mapper/`/bin/hostname`-tmp  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname`-tmp
-    test -b /dev/mapper/`/bin/hostname`-var  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname`-var
-    test -b /dev/mapper/`/bin/hostname`-opt  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname`-opt
-    test -b /dev/mapper/`/bin/hostname`-usr  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname`-usr
-    test -b /dev/mapper/`/bin/hostname`-home && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname`-home
+    test -b /dev/mapper/`/bin/hostname -a`-root && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname -a`-root
+    test -b /dev/mapper/`/bin/hostname -a`-tmp  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname -a`-tmp
+    test -b /dev/mapper/`/bin/hostname -a`-var  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname -a`-var
+    test -b /dev/mapper/`/bin/hostname -a`-opt  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname -a`-opt
+    test -b /dev/mapper/`/bin/hostname -a`-usr  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname -a`-usr
+    test -b /dev/mapper/`/bin/hostname -a`-home && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/`/bin/hostname -a`-home
 
-    test -b /dev/mapper/lv_`/bin/hostname`-root && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-root
-    test -b /dev/mapper/lv_`/bin/hostname`-tmp  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-tmp
-    test -b /dev/mapper/lv_`/bin/hostname`-var  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-var
-    test -b /dev/mapper/lv_`/bin/hostname`-opt  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-opt
-    test -b /dev/mapper/lv_`/bin/hostname`-usr  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-usr
-    test -b /dev/mapper/lv_`/bin/hostname`-home && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-home
+    test -b /dev/mapper/lv_`/bin/hostname -a`-root && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname -a`-root
+    test -b /dev/mapper/lv_`/bin/hostname -a`-tmp  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname -a`-tmp
+    test -b /dev/mapper/lv_`/bin/hostname -a`-var  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname -a`-var
+    test -b /dev/mapper/lv_`/bin/hostname -a`-opt  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname -a`-opt
+    test -b /dev/mapper/lv_`/bin/hostname -a`-usr  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname -a`-usr
+    test -b /dev/mapper/lv_`/bin/hostname -a`-home && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname -a`-home
 
-    test -b /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-root && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-root
-    test -b /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-tmp  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-tmp
-    test -b /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-var  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-var
-    test -b /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-opt  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-opt
-    test -b /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-usr  && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-usr
-    test -b /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-home && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname`-lv_`/bin/hostname`-home
+    test -b /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-root && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-root
+    test -b /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-tmp  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-tmp
+    test -b /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-var  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-var
+    test -b /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-opt  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-opt
+    test -b /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-usr  && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-usr
+    test -b /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-home && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`-home
 
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol01 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol01
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol02 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol02
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol03 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol03
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol04 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol04
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol05 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol05
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol06 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol06
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol07 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol07
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol08 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol08
-    test -b /dev/mapper/lv_`/bin/hostname`-LogVol09 && \
-      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/lv_`/bin/hostname`-LogVol09
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol01 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol01
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol02 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol02
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol03 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol03
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol04 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol04
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol05 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol05
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol06 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol06
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol07 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol07
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol08 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol08
+    test -b /dev/mapper/vg_`/bin/hostname -a`-LogVol09 && \
+      sudo tune2fs -i 0 -c 0 -m 1 /dev/mapper/vg_`/bin/hostname -a`-LogVol09
 }
 
 setup_tune2fs
