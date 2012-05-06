101d7e16163056f41bece9cdc7342e8ce28af7b1
id774 <idnanashi@gmail.com>
2012-05-07 01:48:14 +0900

    962 Renew desktop setup scripts for Ubuntu Precise

--- installer/debian_desktop_installers.sh
+++ installer/debian_desktop_installers.sh
@@ -9,27 +9,9 @@
 #       First version.
 ########################################################################
 
-im-switch -c
-sudo rmmod pcspkr
-test -r /etc/modprobe.d/blacklist && sudo vi /etc/modprobe.d/blacklist
-test -r /etc/modprobe.d/blacklist.conf && sudo vi /etc/modprobe.d/blacklist.conf
+sudo vim /etc/lightdm/lightdm.conf
+# allow-guest=false
 
-$SCRIPTS/installer/install_dotfiles.sh dot_xmodmaprc_lucid
+sudo vim /etc/resolvconf/resolv.conf.d/base /etc/resolv.conf
 
-which s2ram > /dev/null && which xflock4 > /dev/null && \
-  sudo cp $SCRIPTS/xfsuspend.sh /usr/local/sbin/xfsuspend && \
-  sudo chown root:root /usr/local/sbin/xfsuspend && \
-  sudo chmod 755 /usr/local/sbin/xfsuspend && \
-  sudo vi /usr/local/sbin/xfsuspend
-
-test -f /usr/share/themes/Xfce-dusk/gtk-2.0/gtkrc && \
-  sudo cp $HOME/scripts/etc/themes/xfce-dusk/gtkrc \
-  /usr/share/themes/Xfce-dusk/gtk-2.0/gtkrc
-
-# GDM Themes
-$SCRIPTS/installer/install_gdmthemes.sh
-$SCRIPTS/installer/install_gdmthemes2.sh
-
-# Iceweasel and Icedove (Debian)
-test -f /etc/lsb-release || $SCRIPTS/installer/install_iceweasel.sh
-
+sudo ln -fs /var/log/syslog /var/log/messages
