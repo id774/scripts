faeac5e2d7e2649f4e661ce529544ba36506b0a6
id774 <idnanashi@gmail.com>
2012-05-07 01:49:46 +0900

    962 Renew desktop setup scripts for Ubuntu Precise

--- installer/debian_desktop_installers.sh
+++ installer/debian_desktop_installers.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.2 5/7,2012
+#       Renew for Ubuntu Precise.
 #  v0.1 9/28,2011
 #       First version.
 ########################################################################
