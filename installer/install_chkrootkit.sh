6824b68d67aa75ac17574e16e4d824993da04309
id774 <idnanashi@gmail.com>
2012-05-15 14:43:11 +0900

    977 Add chkrootkit setup script

---
+++ installer/install_chkrootkit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+########################################################################
+# chkrootkit setup script
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v1.0 5/15,2012
+#       First.
+########################################################################
+
+# job
+sudo cp $SCRIPTS/cron/bin/chkrootkit /etc/cron.daily/chkrootkit
+sudo chmod 750 /etc/cron.daily/chkrootkit
+sudo chown root:adm /etc/cron.daily/chkrootkit
+# logdir
+test -d /var/log/chkrootkit || sudo mkdir /var/log/chkrootkit
+sudo chmod 750 /var/log/chkrootkit
+sudo chown root:adm /var/log/chkrootkit
+# log
+sudo touch /var/log/chkrootkit/chkrootkit.log
+sudo chmod 640 /var/log/chkrootkit/chkrootkit.log
+sudo chown root:adm /var/log/chkrootkit/chkrootkit.log
+# logrotate
+sudo cp $SCRIPTS/cron/etc/logrotate.d/chkrootkit /etc/logrotate.d/chkrootkit
+sudo chmod 644 /etc/logrotate.d/chkrootkit
+sudo chown root:root /etc/logrotate.d/chkrootkit
