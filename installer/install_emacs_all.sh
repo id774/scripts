8cd442ebac0f22ba849e2b2d25d5f6a85853b2bf
id774 <idnanashi@gmail.com>
2012-06-17 18:07:17 +0900

    1001 Downgrade to emacs 23.4

--- installer/install_emacs_all.sh
+++ installer/install_emacs_all.sh
@@ -13,7 +13,7 @@ test -n "$SCRIPTS" || export SCRIPTS=$HOME/scripts
 test -n "$PRIVATE" || export PRIVATE=$HOME/private/scripts
 
 install_emacs() {
-    VERSION=24.1
+    VERSION=23.4
     $SCRIPTS/installer/install_emacs.sh $VERSION /opt/emacs/$VERSION
     $SCRIPTS/installer/install_emacs_w3m.sh /opt/emacs/$VERSION
     $SCRIPTS/installer/install_mew.sh /opt/emacs/$VERSION/bin/emacs
