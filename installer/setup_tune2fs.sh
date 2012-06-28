602ab1a23dd3fefa5f3e00610ee23fa5e173959b
id774 <idnanashi@gmail.com>
2012-06-28 10:00:15 +0900

    1005 Refactoring tune2fs script

--- installer/setup_tune2fs.sh
+++ installer/setup_tune2fs.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.4 6/28,2012
+#       Refactoring.
 #  v0.3 4/28,2012
 #       Refactoring.
 #  v0.2 4/28,2012
@@ -36,17 +38,17 @@ set_for_mapper() {
 }
 
 set_lvm_debian() {
-    mapper=/dev/mapper/`/bin/hostname -a`
+    mapper=/dev/mapper/$HOSTNAME_S
     set_for_mapper
 }
 
 set_lvm_rhel() {
-    mapper=/dev/mapper/vg_`/bin/hostname -a`-lv_`/bin/hostname -a`
+    mapper=/dev/mapper/vg_$HOSTNAME_S-lv_$HOSTNAME_S
     set_for_mapper
 }
 
 set_lvm_custom() {
-    mapper=/dev/mapper/lv_`/bin/hostname -a`
+    mapper=/dev/mapper/lv_$HOSTNAME_S
     set_for_mapper
 }
 
@@ -54,12 +56,13 @@ set_lvm_logvol() {
     i=0
     while [ $i -lt 10 ]
     do
-        exec_tune2fs /dev/mapper/vg_`/bin/hostname -a`-LogVol0$i
+        exec_tune2fs /dev/mapper/vg_$HOSTNAME_S-LogVol0$i
         i=`expr $i + 1`
     done
 }
 
 setup_tune2fs() {
+    HOSTNAME_S=`/bin/hostname -s`
     set_sda
     set_lvm_debian
     set_lvm_rhel
