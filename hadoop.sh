32b01c44a72a5a38a34fa004931571b105ae601e
id774 <idnanashi@gmail.com>
2012-07-24 17:00:32 +0900

    1010 Added hadoop start script

---
+++ hadoop.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+test -n "$1" || exit 1
+export HADOOP_VER=0.20
+sudo /etc/init.d/hadoop-$HADOOP_VER-namenode $1
+sudo /etc/init.d/hadoop-$HADOOP_VER-jobtracker $1
+sudo /etc/init.d/hadoop-$HADOOP_VER-datanode $1
+sudo /etc/init.d/hadoop-$HADOOP_VER-tasktracker $1
+
