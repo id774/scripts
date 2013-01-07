8014fb7b2bad42c0d1e04cc4152e4c474c6fb2b1
id774 <idnanashi@gmail.com>
2013-01-07 09:48:05 +0900

    1058 Allow to specify the length of the argument for the simple password

--- simple_passwd.py
+++ simple_passwd.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import sys
 import string
 from random import choice
 
-print("".join([choice(string.ascii_lowercase + string.digits) for i in range(8)]))
+def generate_passwd(length):
+    print("".join([choice(string.ascii_lowercase + string.digits) for i in range(int(length))]))
+
+if __name__=='__main__':
+    if len(sys.argv) != 2:
+        generate_passwd(8)
+    else:
+        generate_passwd(sys.argv[1])
+
--- simple_passwd.rb
+++ simple_passwd.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
 
-p [*0..9, *'a'..'z', *'A'..'Z', '_', '-', '!', '#', '&'].sample(8).join
+def generate_passwd(length)
+  puts [*0..9, *'a'..'z', *'A'..'Z', '_', '-', '!', '#', '&'].sample(length).join
+end
+
+if __FILE__ == $0
+  len=ARGV.shift || 8; length=len.to_i
+  generate_passwd(length)
+end
