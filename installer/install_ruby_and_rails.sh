6e79a8afdf138a19de30ea46e188507aeb40ab9f
id774 <idnanashi@gmail.com>
2013-02-08 11:46:18 +0900

    1078 Update to ruby 1.9.3-p385

--- installer/install_ruby_and_rails.sh
+++ installer/install_ruby_and_rails.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.8 2/8,2013
+#       Update to ruby 1.9.3-p385.
 #  v0.7 1/17,2013
 #       Update to ruby 1.9.3-p374.
 #  v0.6 1/7,2013
@@ -29,6 +31,8 @@ purge_old_modules() {
       sudo rm -rf /opt/ruby/1.9.2
     test -d /opt/ruby/1.9.3 && \
       sudo rm -rf /opt/ruby/1.9.3
+    test -d /usr/local/src/ruby/ruby-1.9.3-p374 && \
+      sudo rm -rf /usr/local/src/ruby/ruby-1.9.3-p374
     test -d /usr/local/src/ruby/ruby-1.9.3-p362 && \
       sudo rm -rf /usr/local/src/ruby/ruby-1.9.3-p362
     test -d /usr/local/src/ruby/ruby-1.9.3-p327 && \
@@ -50,7 +54,7 @@ purge_old_modules() {
 }
 
 install_ruby() {
-    $SCRIPTS/installer/install_ruby.sh 193-374 /opt/ruby/1.9.3
+    $SCRIPTS/installer/install_ruby.sh 193-385 /opt/ruby/1.9.3
     $SCRIPTS/installer/install_gems.sh /opt/ruby/1.9.3
     $SCRIPTS/installer/install_termtter_plugins.sh
 }
