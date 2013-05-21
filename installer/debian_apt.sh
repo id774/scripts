aeab429d2ad9a2614cde4fd0e125a94e10aeccb9
id774 <idnanashi@gmail.com>
2013-05-21 22:05:04 +0900

    1124 Add libcurl4-openssl-dev

--- installer/debian_apt.sh
+++ installer/debian_apt.sh
@@ -191,7 +191,8 @@ ruby_lang() {
       autoconf byacc bison automake \
       libopenssl-ruby libreadline-dev zlib1g-dev ruby \
       ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 \
-      libreadline-ruby1.8 libruby1.8 libopenssl-ruby
+      libreadline-ruby1.8 libruby1.8 libopenssl-ruby \
+      libcurl4-openssl-dev
       # autoconf-doc
 }
 
