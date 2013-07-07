1bab684f5881d0f019068e14aa2e3904f9448cfa
id774 <idnanashi@gmail.com>
2013-07-07 18:19:11 +0900

    1166 Remove obsolete ruby sources

--- installer/install_ruby_and_rails.sh
+++ installer/install_ruby_and_rails.sh
@@ -31,6 +31,16 @@ test -n "$PRIVATE" || export PRIVATE=$HOME/private/scripts
 purge_old_modules() {
     test -d /opt/ruby/1.9.2 && \
       sudo rm -rf /opt/ruby/1.9.2
+    test -d /usr/local/src/ruby/ruby-2.0.0-p195 && \
+      sudo rm -rf /usr/local/src/ruby/ruby-2.0.0-p195
+    test -d /usr/local/src/ruby/ruby-2.0.0-p0 && \
+      sudo rm -rf /usr/local/src/ruby/ruby-2.0.0-p0
+    test -d /usr/local/src/ruby/ruby-1.9.3-p429 && \
+      sudo rm -rf /usr/local/src/ruby/ruby-1.9.3-p429
+    test -d /usr/local/src/ruby/ruby-1.9.3-p392 && \
+      sudo rm -rf /usr/local/src/ruby/ruby-1.9.3-p392
+    test -d /usr/local/src/ruby/ruby-1.9.3-p385 && \
+      sudo rm -rf /usr/local/src/ruby/ruby-1.9.3-p385
     test -d /usr/local/src/ruby/ruby-1.9.3-p374 && \
       sudo rm -rf /usr/local/src/ruby/ruby-1.9.3-p374
     test -d /usr/local/src/ruby/ruby-1.9.3-p362 && \
