5c1358f1a7d3c8b76dfa6c6536de83432b209286
id774 <idnanashi@gmail.com>
2013-07-08 10:25:56 +0900

    1168 Add gem naivebayes

--- installer/install_gems.sh
+++ installer/install_gems.sh
@@ -119,6 +119,7 @@ install_gem() {
     sudo $GEM install prawn --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install gnuplot --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install ai4r --no-ri --no-rdoc $2 $3 $4
+    sudo $GEM install naivebayes --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install kmeans --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install sysadmin --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install automatic --no-ri --no-rdoc $2 $3 $4
