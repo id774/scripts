63dc8a886fd2ac6865f07a716a553afc089d724d
id774 <idnanashi@gmail.com>
2013-07-08 14:16:33 +0900

    1169 Add MongoDB installer

---
+++ installer/install_mongodb.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+#
+########################################################################
+# Install MongoDB
+#  $1 = version
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v0.1 7/8,2013
+#       First.
+########################################################################
+
+setup_environment() {
+    test -n "$1" && VERSION=$1
+    test -n "$1" || VERSION=2.4.5
+    SYSTEM_ID=linux-`uname -m`
+    case $OSTYPE in
+      *darwin*)
+        OWNER=root:wheel
+        ;;
+      *)
+        OWNER=root:root
+        ;;
+    esac
+}
+
+mkdir_if_not_exist(){
+    test -d $1 || sudo mkdir -p $1
+}
+
+rmdir_if_exist(){
+    test -d $1 && sudo rm -rf $1
+}
+
+set_permission() {
+    sudo chown -R $1:$1 /data/db
+    sudo chown -R $1:$1 /opt/mongo/$VERSION
+    sudo chmod -R g+w,o-rwx /data/db/
+}
+
+create_user() {
+    sudo useradd -m $1
+    sudo chsh -s /bin/zsh $1
+}
+
+deploy_mongodb() {
+    mkdir_if_not_exist /data/db
+    mkdir_if_not_exist /opt/mongo
+    rmdir_if_exist /opt/mongo/$VERSION
+    sudo mv $VERSION /opt/mongo/
+    create_user mongo
+    set_permission mongo
+}
+
+install_mongodb() {
+    mkdir install_mongodb
+    cd install_mongodb
+    case "$SYSTEM_ID" in
+      linux-i386 | linux-i686)
+        curl http://downloads.mongodb.org/linux/mongodb-linux-i686-$VERSION.tgz > mongodb.tgz
+        tar xzvf mongodb.tgz
+        mv mongodb-linux-i686-$VERSION $VERSION
+        deploy_mongodb
+        ;;
+      linux-amd64 | linux-x86_64)
+        curl http://downloads.mongodb.org/linux/mongodb-linux-x86_64-$VERSION.tgz > mongodb.tgz
+        tar xzvf mongodb.tgz
+        mv mongodb-linux-x86_64-$VERSION $VERSION
+        deploy_mongodb
+        ;;
+    esac
+    cd ..
+    rm -rf install_mongodb
+}
+
+install_main() {
+    setup_environment $*
+    install_mongodb $*
+}
+
+ping -c 1 id774.net > /dev/null 2>&1 || exit 1
+install_main $*
