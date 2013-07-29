5acfc015b89bb10fdf7662cf6c724bcd5fcdbba7
id774 <idnanashi@gmail.com>
2013-07-29 17:38:11 +0900

    1185 Install rails both 3.2 and 4.0

--- installer/install_gems.sh
+++ installer/install_gems.sh
@@ -26,7 +26,7 @@ install_gem() {
     sudo $GEM install -v "~> 1.3.5" bundler --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install -v "~> 3.1.0" builder --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install minitest --no-ri --no-rdoc $2 $3 $4
-    sudo $GEM install -v "~> 3.2.0" rails --no-ri --no-rdoc $2 $3 $4
+    sudo $GEM install -v "~> 3" rails --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install -v "~> 4.0.0" rails --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install sqlite3 --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install sass --no-ri --no-rdoc $2 $3 $4
