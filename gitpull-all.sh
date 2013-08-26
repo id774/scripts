1e9d6de59c09a264e283c54b8313bfddc3801fc9
id774 <idnanashi@gmail.com>
2013-08-26 15:27:24 +0900

    1209 Fix typo

--- gitpull-all.sh
+++ gitpull-all.sh
@@ -115,7 +115,7 @@ gitpull_all() {
     gitpull github id774 rails4-bootstrap $*
     gitpull github id774 sinatra-bootstrap $*
     gitpull github id774 sinatra-api-provider $*
-    gitpull github id774 fluentd-json-reciever $*
+    gitpull github id774 fluentd-json-receiver $*
     gitpull github jugyo rubytter $*
     gitpull github fluent fluentd $*
     gitpull github sandal rbp $*
