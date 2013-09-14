a770b32e809bd14679f6a00b91729a002e0d0f6c
id774 <idnanashi@gmail.com>
2013-09-14 19:20:39 +0900

    1220 Add gem redis-server

--- installer/install_gems.sh
+++ installer/install_gems.sh
@@ -105,6 +105,7 @@ install_gem() {
     sudo $GEM install log4r --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install sinatra --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install -v "~> 3.0.0" redis --no-ri --no-rdoc $2 $3 $4
+    sudo $GEM install redis-server --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install rubytree --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install shoulda --no-ri --no-rdoc $2 $3 $4
     sudo $GEM install tzinfo --no-ri --no-rdoc $2 $3 $4
