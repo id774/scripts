832accbdd52fe306103c864e8a0768fff8619eca
id774 <idnanashi@gmail.com>
2013-11-29 22:29:12 +0900

    1269 Safety for kernel purge script

--- purge_old_kernel_of_ubuntu.sh
+++ purge_old_kernel_of_ubuntu.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
-LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
-METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
-OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
+if [ -f /etc/lsb-release ]; then
+  CURKERNEL=$(uname -r|sed 's/-*[a-z]//g'|sed 's/-386//g')
+  LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
+  METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
+  OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
 
-sudo aptitude purge $OLDKERNELS
+  sudo aptitude purge $OLDKERNELS
+fi
