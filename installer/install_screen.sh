ddb27ba726e96dc04b61cceaef2fa04a93e2cf6c
id774 <idnanashi@gmail.com>
2014-01-20 10:27:11 +0900

    1294 Renew screen build process

--- installer/install_screen.sh
+++ installer/install_screen.sh
@@ -2,12 +2,13 @@
 #
 ########################################################################
 # Install screen
-#  $1 = version
-#  $2 = prefix
-#  $3 = not save to src
+#  $1 = prefix
+#  $2 = not save to src
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.3 1/20,2014
+#       Renew screen build process.
 #  v0.2 9/16,2010
 #       Refactoring.
 #  v0.1 9/14,2010
@@ -15,8 +16,6 @@
 ########################################################################
 
 setup_environment() {
-    test -n "$3" || SUDO=sudo
-    test -n "$3" && SUDO=
     case $OSTYPE in
       *darwin*)
         OPTIONS=-pR
@@ -30,37 +29,35 @@ setup_environment() {
 }
 
 save_sources() {
-    test -d /usr/local/src/screen || sudo mkdir -p /usr/local/src/screen
-    sudo cp $OPTIONS screen-$SCREEN_VERSION /usr/local/src/screen
+    test -d /usr/local/src/screen && sudo rm -rf /usr/local/src/screen
+    sudo cp $OPTIONS screen /usr/local/src
     sudo chown $OWNER /usr/local/src/screen
-    sudo chown -R $OWNER /usr/local/src/screen/screen-$SCREEN_VERSION
+    sudo chown -R $OWNER /usr/local/src/screen
 }
 
 make_and_install() {
-    cd screen-$SCREEN_VERSION
-    test -n "$2" || ./configure --prefix=$HOME/local/screen/$SCREEN_VERSION
-    test -n "$2" && ./configure --prefix=$2
-    make
-    $SUDO make install
-    cd ..
+    test -n "$1" || ./configure --prefix=/opt/screen --enable-pam --enable-colors256 --enable-rxvt_osc --enable-use-locale --enable-telnet
+    test -n "$1" && ./configure --prefix=$2 --enable-pam --enable-colors256 --enable-rxvt_osc --enable-use-locale --enable-telnet
+    sudo make
+    sudo make install
 }
 
 get_screen() {
     mkdir install_screen
     cd install_screen
-    wget wget ftp://www.dekaino.net/pub/screen/screen-$SCREEN_VERSION.tar.gz
-    test -f screen-$SCREEN_VERSION.tar.gz || exit 1
-    tar xzvf screen-$SCREEN_VERSION.tar.gz
-    test "$2" = "sourceonly" || make_and_install $1 $2
-    test -n "$3" || save_sources
+    git clone git://git.savannah.gnu.org/screen.git
+    cd screen/src
+    autoconf
+    ./autogen.sh
+    test "$1" = "sourceonly" || make_and_install $1 $2
+    cd ../..
+    test -n "$2" || save_sources
     cd ..
     rm -rf install_screen
 }
 
 install_screen() {
     setup_environment $*
-    test -n "$1" && SCREEN_VERSION=$1
-    test -n "$1" || SCREEN_VERSION=4.0.3
     get_screen $*
 }
 
