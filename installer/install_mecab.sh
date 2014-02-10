6058182e42e3c98a3e31ba9a35adff70ddf7e175
id774 <idnanashi@gmail.com>
2014-02-10 21:10:30 +0900

    1333 Refactoring MeCab installer

--- installer/install_mecab.sh
+++ installer/install_mecab.sh
@@ -10,6 +10,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.3 2/10,2014
+#       Refactoring.
 #  v0.2 1/28,2013
 #       Add NAIST dic.
 #  v0.1 8/7,2012
@@ -17,6 +19,8 @@
 ########################################################################
 
 setup_environment() {
+    test -n "$SCRIPTS" || export SCRIPTS=$HOME/scripts
+    test -n "$PRIVATE" || export PRIVATE=$HOME/private/scripts
     test -n "$1" || MECAB_VERSION=0.994
     test -n "$1" && MECAB_VERSION=$1
     test -n "$2" || IPADIC_VERSION=2.7.0-20070801
@@ -47,12 +51,7 @@ save_sources() {
     sudo chown -R $OWNER /usr/local/src/mecab
 }
 
-install_mecab() {
-    setup_environment $*
-
-    mkdir install_mecab
-    cd install_mecab
-
+source_compile() {
     wget http://mecab.googlecode.com/files/mecab-$MECAB_VERSION.tar.gz
     tar xzvf mecab-$MECAB_VERSION.tar.gz
     cd mecab-$MECAB_VERSION
@@ -60,19 +59,9 @@ install_mecab() {
     make
     sudo make install
     cd ..
-    wget http://mecab.googlecode.com/files/mecab-python-$MECAB_VERSION.tar.gz
-    tar xzvf mecab-python-$MECAB_VERSION.tar.gz
-    cd mecab-python-$MECAB_VERSION
-    python setup.py build
-    sudo `which python` setup.py install
-    cd ..
-    wget http://mecab.googlecode.com/files/mecab-ruby-$MECAB_VERSION.tar.gz
-    tar xzvf mecab-ruby-$MECAB_VERSION.tar.gz
-    cd mecab-ruby-$MECAB_VERSION
-    `which ruby` extconf.rb
-    make
-    sudo make install
-    cd ..
+}
+
+ipadic_compile() {
     wget http://mecab.googlecode.com/files/mecab-ipadic-$IPADIC_VERSION.tar.gz
     tar xzvf mecab-ipadic-$IPADIC_VERSION.tar.gz
     cd mecab-ipadic-$IPADIC_VERSION
@@ -80,6 +69,9 @@ install_mecab() {
     make
     sudo make install
     cd ..
+}
+
+naistdic_compile() {
     wget "http://sourceforge.jp/frs/redir.php?m=iij&f=%2Fnaist-jdic%2F$NAISTDIC_NUM%2Fmecab-naist-jdic-$NAISTDIC_VERSION.tar.gz" -O naistdic.tar.gz
     tar xzvf naistdic.tar.gz
     cd mecab-naist-jdic-$NAISTDIC_VERSION
@@ -87,11 +79,40 @@ install_mecab() {
     make
     sudo make install
     cd ..
+}
+
+get_binding() {
+    wget http://mecab.googlecode.com/files/mecab-python-$MECAB_VERSION.tar.gz
+    tar xzvf mecab-python-$MECAB_VERSION.tar.gz
+    wget http://mecab.googlecode.com/files/mecab-ruby-$MECAB_VERSION.tar.gz
+    tar xzvf mecab-ruby-$MECAB_VERSION.tar.gz
+}
+
+install_mecab() {
+    mkdir install_mecab
+    cd install_mecab
+
+    install_mecab
+    source_compile
+    ipadic_compile
+    naistdic_compile
+    get_binding
 
     test -n "$5" || save_sources
     cd ..
     rm -rf install_mecab
 }
 
+install_binding() {
+    $SCRIPTS/installer/install_mecab_ruby.sh /opt/ruby/current /usr/local/src/mecab/mecab-ruby-$MECAB_VERSION
+    $SCRIPTS/installer/install_mecab_python.sh /opt/python/current /usr/local/src/mecab/mecab-python-$MECAB_VERSION
+}
+
+main() {
+    setup_environment $*
+    install_mecab $*
+    install_binding $*
+}
+
 ping -c 1 id774.net > /dev/null 2>&1 || exit 1
-install_mecab $*
+main $*
