7c063b58470e0447a879feed28d9c44744ba91bc
id774 <idnanashi@gmail.com>
2014-04-12 22:10:15 +0900

    1371 Add python binding for protobuf

--- installer/install_python3-protobuf.sh
+++ installer/install_python3-protobuf.sh
@@ -3,17 +3,24 @@
 ########################################################################
 # Install Python 3 Protocol Buffers
 #  $1 = prefix
-#  $2 = nosudo
+#  $2 = python path
+#  $3 = nosudo
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.2 4/12,2014
+#       Add python binding.
 #  v0.1 4/11,2014
 #       First.
 ########################################################################
 
 setup_environment() {
-    test -n "$2" || SUDO=sudo
-    test -n "$2" && SUDO=
+    test -n "$1" && PREFIX=$1
+    test -n "$1" || PREFIX=/opt/protobuf
+    test -n "$2" && PYTHON_BIN=$2/bin/python
+    test -n "$2" || PYTHON_BIN=python
+    test -n "$3" || SUDO=sudo
+    test -n "$3" && SUDO=
     case $OSTYPE in
       *darwin*)
         OPTIONS=-pR
@@ -46,11 +53,17 @@ gitpull_all() {
 
 make_and_install() {
     ./autogen.sh
-    test -n "$2" || ./configure --prefix=/opt/protobuf
-    test -n "$2" && ./configure --prefix=$1
+    ./configure --prefix=$PREFIX
     make
     make check
     $SUDO make install
+}
+
+install_python_binding() {
+    cd python
+    $PYTHON_BIN setup.py build
+    $PYTHON_BIN setup.py test
+    $SUDO $PYTHON_BIN setup.py install
 }
 
 install_trunk() {
@@ -58,17 +71,12 @@ install_trunk() {
     test -d $HOME/local/github/python3-protobuf || exit 1
     cd $HOME/local/github/python3-protobuf
     make_and_install $*
+    install_python_binding $*
 }
 
 main() {
     setup_environment $*
-    case "$1" in
-      trunk)
-        install_trunk $*
-        ;;
-      *)
-        ;;
-    esac
+    install_trunk $*
 }
 
 ping -c 1 id774.net > /dev/null 2>&1 || exit 1
