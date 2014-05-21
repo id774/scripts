a139a376025bbb37680d2ad33895b903788af946
id774 <idnanashi@gmail.com>
2014-05-21 11:10:09 +0900

    1398 Add TeX Live installer

---
+++ installer/install_texlive.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+########################################################################
+# Install texlive (for Debian 7.0/Ubuntu 14.04 or later)
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v0.1 5/21,2014
+#       First.
+########################################################################
+
+sudo apt-get -y install texlive
+sudo apt-get -y install texlive-lang-cjk
+sudo apt-get -y install xdvik-ja
+sudo apt-get -y install dvipsk-ja
+sudo apt-get -y install gv
+sudo apt-get -y install texlive-fonts-recommended texlive-fonts-extra
+
