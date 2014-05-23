a3d97c62b139abe802f620661e4814fe4bea1416
id774 <idnanashi@gmail.com>
2014-05-23 14:18:39 +0900

    1400 Evaluate GNU/Linux Distribution

--- linux_distribution.sh
+++ linux_distribution.sh
@@ -4,13 +4,13 @@ ISSUE=`cat /etc/issue | head -n 1`
 DIST=`echo $ISSUE | awk '{print $1}'`
 
 if [[ $DIST =~ "CentOS" ]]; then
-	VER=`echo $ISSUE | awk '{print $3}'`
+    VER=`echo $ISSUE | awk '{print $3}'`
 elif [[ $DIST =~ "Scientific" ]]; then
-	VER=`echo $ISSUE | awk '{print $4}'`
+    VER=`echo $ISSUE | awk '{print $4}'`
 elif [[ $DIST =~ "Ubuntu" ]]; then
-	VER=`echo $ISSUE | awk '{print $2}'`
+    VER=`echo $ISSUE | awk '{print $2}'`
 elif [[ $DIST =~ "Debian" ]]; then
-	VER=`echo $ISSUE | awk '{print $3}'`
+    VER=`echo $ISSUE | awk '{print $3}'`
 fi
 
 echo "Distribution: $DIST"
