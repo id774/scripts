b15b1151bd6fb90f4fad6fd7aa8ad0961ca1601e
id774 <idnanashi@gmail.com>
2014-05-28 14:54:27 +0900

    1407 Evaluate other linux distribution

--- linux_distribution.sh
+++ linux_distribution.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-ISSUE=`cat /etc/issue | head -n 1`
-DIST=`echo $ISSUE | awk '{print $1}'`
+if [ -f /etc/issue ]; then
+    ISSUE=`cat /etc/issue | head -n 1`
+    DIST=`echo $ISSUE | awk '{print $1}'`
 
-if [[ $DIST =~ "CentOS" ]]; then
-    VER=`echo $ISSUE | awk '{print $3}'`
-elif [[ $DIST =~ "Scientific" ]]; then
-    VER=`echo $ISSUE | awk '{print $4}'`
-elif [[ $DIST =~ "Ubuntu" ]]; then
-    VER=`echo $ISSUE | awk '{print $2}'`
-elif [[ $DIST =~ "Debian" ]]; then
-    VER=`echo $ISSUE | awk '{print $3}'`
+    if [[ $DIST =~ "CentOS" ]]; then
+        VER=`echo $ISSUE | awk '{print $3}'`
+    elif [[ $DIST =~ "Scientific" ]]; then
+        VER=`echo $ISSUE | awk '{print $4}'`
+    elif [[ $DIST =~ "Ubuntu" ]]; then
+        VER=`echo $ISSUE | awk '{print $2}'`
+    elif [[ $DIST =~ "Debian" ]]; then
+        VER=`echo $ISSUE | awk '{print $3}'`
+    fi
+
+    echo "Distribution: $DIST"
+    echo "Version: $VER"
+else
+    echo "This is NOT Linux Distribution."
 fi
-
-echo "Distribution: $DIST"
-echo "Version: $VER"
