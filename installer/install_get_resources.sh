c00bf5494d735a47d25b46a2be504170631e119e
id774 <idnanashi@gmail.com>
2014-06-06 23:12:22 +0900

    1418 Remove obsolete archive

--- installer/install_get_resources.sh
+++ installer/install_get_resources.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v1.5 6/6,2014
+#       Remove obsolete archive.
 #  v1.4 11/5,2011
 #       Purge obsolete jobs.
 #  v1.3 9/11,2008
@@ -37,16 +39,6 @@ sudo chown root:adm /var/log/sysadmin/resources.log
 sudo cp $SCRIPTS/cron/etc/logrotate.d/resources /etc/logrotate.d/resources
 sudo chmod 644 /etc/logrotate.d/resources
 sudo chown root:root /etc/logrotate.d/resources
-
-# Archive Old Logs
-if [ -f /var/log/auto-upgrade.log ]; then
-    test -d /var/log/sysadmin/archive || sudo mkdir /var/log/sysadmin/archive
-    sudo chmod 750 /var/log/sysadmin/archive
-    sudo chown root:adm /var/log/sysadmin/archive
-    test -f /var/log/auto-upgrade.log && sudo mv /var/log/auto-upgrade.log* /var/log/sysadmin/archive/
-    test -f /var/log/backup && sudo mv /var/log/backup* /var/log/sysadmin/archive/
-    test -f /var/log/resources.log && sudo mv /var/log/resources.log* /var/log/sysadmin/archive/
-fi
 
 # Edit crontab
 # 50 23 * * 1-5 root cd / && run-parts --report /etc/cron.weekday
