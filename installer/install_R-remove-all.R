3efba25ec9b152f000059a778437d790a0ba0c05
id774 <idnanashi@gmail.com>
2014-07-21 01:44:47 +0900

    1472 Refactoring

--- installer/install_R-remove-all.R
+++ installer/install_R-remove-all.R
@@ -1,8 +1,9 @@
 
 # sudo R --no-save --no-restore < ~/scripts/installer/install_R-remove-all.R
 
-options(repos="http://cran.ism.ac.jp")
-packs <- available.packages(contriburl=contrib.url("http://cran.ism.ac.jp/"))
+mirror <- "http://cran.ism.ac.jp"
+options(repos=mirror)
+packs <- available.packages(contriburl=contrib.url(mirror))
 remove.packages(packs[,1],lib="/usr/local/lib/R/site-library")
 library()
 
