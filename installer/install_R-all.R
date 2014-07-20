5ff75ce215acd0ce45a461aebc123eac106978ff
id774 <idnanashi@gmail.com>
2014-07-20 23:09:59 +0900

    1472 Refactoring

--- installer/install_R-all.R
+++ installer/install_R-all.R
@@ -1,8 +1,9 @@
 
 # sudo R --no-save --no-restore < ~/scripts/installer/install_R-all.R
 
-options(repos="http://cran.ism.ac.jp")
-packs <- available.packages(contriburl=contrib.url("http://cran.ism.ac.jp/"))
-install.packages(packs[,1],contriburl=contrib.url("http://cran.ism.ac.jp/"))
+mirror <- "http://cran.ism.ac.jp"
+options(repos=mirror)
+packs <- available.packages(contriburl=contrib.url(mirror))
+install.packages(packs[,1],contriburl=contrib.url(mirror))
 library()
 
