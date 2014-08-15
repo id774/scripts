081d4af0c480309861ce9ba109b888c4bcf83b36
id774 <idnanashi@gmail.com>
2014-08-16 06:48:07 +0900

    1501 Define -i option for autopep8

--- pyck.sh
+++ pyck.sh
@@ -5,10 +5,19 @@ setup_environment() {
     IGNORE_ERRORS=E302
 }
 
+run_check() {
+    $PYTHON_PATH/bin/flake8 --ignore=$IGNORE_ERRORS $*
+}
+
+autofix() {
+    shift && $PYTHON_PATH/bin/autopep8 --ignore=$IGNORE_ERRORS -v -i $*
+}
+
 main() {
     setup_environment
     which $PYTHON_PATH/bin/flake8 > /dev/null || exit 254
-    $PYTHON_PATH/bin/flake8 --ignore=$IGNORE_ERRORS $*
+    test "$1" = "-i" || run_check $*
+    test "$1" = "-i" && autofix $*
     exit 0
 }
 
