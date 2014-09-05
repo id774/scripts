e43eb34d4c08356141deafdf1425cc33f9d92aff
id774 <idnanashi@gmail.com>
2014-09-05 12:28:30 +0900

    1512 Create ipython nbserver profile

--- installer/setup_dot_ipython.sh
+++ installer/setup_dot_ipython.sh
@@ -13,6 +13,12 @@ setup_environment() {
     esac
 }
 
+init_nbserver() {
+    cd
+    ipython profile create nbserver
+    test -d $HOME/.ipython/profile_nbserver/startup && cp $OPTIONS $SCRIPTS/dot_files/dot_ipython/profile_default/startup/00-init.py $HOME/.ipython/profile_nbserver/startup/
+}
+
 copy_dotipython() {
     test -d $HOME/.ipython/profile_default/startup || mkdir -p $HOME/.ipython/profile_default/startup
     cp $OPTIONS $SCRIPTS/dot_files/dot_ipython/profile_default/startup/00-init.py $HOME/.ipython/profile_default/startup/
@@ -21,6 +27,7 @@ copy_dotipython() {
 main() {
     setup_environment $*
     copy_dotipython $*
+    init_nbserver $*
 }
 
 main $*
