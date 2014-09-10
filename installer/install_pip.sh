1701d8a8c3c48fe9b628796081b005d857af4a97
id774 <idnanashi@gmail.com>
2014-09-10 12:49:42 +0900

    1514 Add pip package husl

--- installer/install_pip.sh
+++ installer/install_pip.sh
@@ -49,9 +49,9 @@ install_libs() {
     $SUDO $PIP install $PROXY -U matplotlib
     $SUDO $PIP install $PROXY -U pandas
     $SUDO $PIP install $PROXY -U patsy
-    $SUDO $PIP install $PROXY -U statsmodels
+    $SUDO $PIP install $PROXY statsmodels
     $SUDO $PIP install $PROXY -U sympy
-    $SUDO $PIP install $PROXY -U seaborn
+    $SUDO $PIP install $PROXY seaborn
     $SUDO $PIP install $PROXY bokeh
     $SUDO $PIP install $PROXY -U twisted
     $SUDO $PIP install $PROXY -U Flask
@@ -67,6 +67,7 @@ install_libs() {
     $SUDO $PIP install $PROXY -U beautifulsoup4
     $SUDO $PIP install $PROXY -U lxml
     $SUDO $PIP install $PROXY -U requests
+    $SUDO $PIP install $PROXY husl
     $SUDO $PIP install $PROXY -U ggplot
     $SUDO $PIP install $PROXY -U pyper
     $SUDO $PIP install $PROXY -U jinja2 tornado pyzmq
