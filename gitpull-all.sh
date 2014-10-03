7fff05828e5844f09b9ed8dd3bf1fe268bf49ac0
id774 <idnanashi@gmail.com>
2014-10-03 13:56:26 +0900

    1520 Add repo fastladder

--- gitpull-all.sh
+++ gitpull-all.sh
@@ -128,6 +128,7 @@ gitpull_all() {
     gitpull github id774 house_api_web $*
     gitpull github id774 akazunomabot $*
     gitpull github id774 scipy-lecture-notes $*
+    gitpull github fastladder fastladder $*
     gitpull github jugyo rubytter $*
     gitpull github fluent fluentd $*
     gitpull github sandal rbp $*
