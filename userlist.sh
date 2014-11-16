8f278ed44fd3c69754874a7482decdb63796a633
id774 <idnanashi@gmail.com>
2014-11-16 23:18:11 +0900

    1539 Userlist working on GNU/Linux only

--- userlist.sh
+++ userlist.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-while read LINE
-do
-  if [ `echo $LINE | cut -f3 -d:` -ge 500 ]
-  then
-    echo $LINE | cut -f1 -d:
-  fi
-done < /etc/passwd
+THRESHOLD=0
+test -f /etc/debian_version && THRESHOLD=1000
+test -f /etc/redhat-release && THRESHOLD=500
+
+show_userlist() {
+  while read LINE
+  do
+    if [ `echo $LINE | cut -f3 -d:` -ge $THRESHOLD ]
+    then
+      echo $LINE | cut -f1 -d:
+    fi
+  done < /etc/passwd
+}
+
+test $THRESHOLD = 0 || show_userlist
