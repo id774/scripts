d656fb1e98940432cc7a53c0692b4e4d165aaf32
id774 <idnanashi@gmail.com>
2014-12-03 23:59:02 +0900

    1541 Specify keyserver

--- gpg-import.sh
+++ gpg-import.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test -n "$1" && gpg --keyserver pgp.nic.ad.jp --recv-keys $1
-test -n "$1" && ( sudo gpg --armor --export $1 | sudo apt-key add - )
-test -n "$1" || echo "usage: $0 PUBKEY"
+test -n "$2" && gpg --keyserver $1 --recv-keys $2
+test -n "$2" && ( sudo gpg --armor --export $2 | sudo apt-key add - )
+test -n "$2" || echo "usage: $0 KEYSERVER PUBKEY"
 
