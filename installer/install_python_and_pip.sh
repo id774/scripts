5de0ed98ba03679bf19d46e6e3b7f4df62cd391a
id774 <idnanashi@gmail.com>
2015-01-13 14:23:04 +0900

    1561 Update to Pthon 3.4.2

--- installer/install_python_and_pip.sh
+++ installer/install_python_and_pip.sh
@@ -18,9 +18,9 @@ install_python() {
 }
 
 operation() {
-    test -n "$1" || VERSION=3.3.6
+    test -n "$1" || VERSION=3.4.2
     test -n "$1" && VERSION=$1
-    test -n "$2" || PATH_VERSION=3.3
+    test -n "$2" || PATH_VERSION=3.4
     test -n "$2" && PATH_VERSION=$1
     test -n "$SCRIPTS" || export SCRIPTS=$HOME/scripts
     test -n "$PRIVATE" || export PRIVATE=$HOME/private/scripts
