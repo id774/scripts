c4c7ac1e2dfcdb94aa0b537993e21a0531643499
id774 <idnanashi@gmail.com>
2015-01-20 15:14:03 +0900

    1564 Add function set_trace() and debug()

--- dot_files/dot_ipython/profile_default/startup/00-init.py
+++ dot_files/dot_ipython/profile_default/startup/00-init.py
@@ -1,4 +1,3 @@
-
 import sys
 import os
 import readline
@@ -45,3 +44,12 @@ try:
     print("import pylab")
 except ImportError:
     print("pylab has not been loaded")
+
+def set_trace():
+    from IPython.core.debugger import Pdb
+    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+
+def debug(f, *args, **kwargs):
+    from IPython.core.debugger import Pdb
+    pdb = Pdb(color_scheme='Linux')
+    return pdb.runcall(f, *args, **kwargs)
