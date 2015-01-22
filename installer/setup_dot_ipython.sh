4e5d63038ec6a413863e201feb6c511e7833634a
id774 <idnanashi@gmail.com>
2015-01-22 15:02:48 +0900

    1565 Add IPython config file

--- installer/setup_dot_ipython.sh
+++ installer/setup_dot_ipython.sh
@@ -20,8 +20,11 @@ init_nbserver() {
 }
 
 copy_dotipython() {
+    cd
+    ipython profile create default
     test -d $HOME/.ipython/profile_default/startup || mkdir -p $HOME/.ipython/profile_default/startup
     cp $OPTIONS $SCRIPTS/dot_files/dot_ipython/profile_default/startup/00-init.py $HOME/.ipython/profile_default/startup/
+    cp $OPTIONS $SCRIPTS/dot_files/dot_ipython/profile_default/ipython_config.py $HOME/.ipython/profile_default/
 }
 
 main() {
