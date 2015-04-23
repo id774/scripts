078eb5137740ddece15eccafa192d5e47c0e897e
id774 <idnanashi@gmail.com>
2015-04-23 09:14:56 +0900

    1594 Ignore control chars

--- dev/diskcopy.sh
+++ dev/diskcopy.sh
@@ -15,7 +15,7 @@ echo -n "Diskcopy job started at `/bin/hostname` on ">>$JOBLOG
 date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    nkf -w $JOBLOG | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat -v $JOBLOG | nkf -w | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
@@ -25,7 +25,7 @@ echo -n "Diskcopy user1 completed at `/bin/hostname` on ">>$JOBLOG
 date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    nkf -w $JOBLOG | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat -v $JOBLOG | nkf -w | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
@@ -35,7 +35,7 @@ echo -n "Diskcopy user2 completed at `/bin/hostname` on ">>$JOBLOG
 date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    nkf -w $JOBLOG | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat -v $JOBLOG | nkf -w | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
@@ -45,7 +45,7 @@ echo -n "Diskcopy user3 completed at `/bin/hostname` on ">>$JOBLOG
 date "+%Y/%m/%d %T">>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    nkf -w $JOBLOG | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat -v $JOBLOG | nkf -w | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
@@ -56,7 +56,7 @@ date "+%Y/%m/%d %T">>$JOBLOG
 df -T>>$JOBLOG
 case "$ADMIN_MAIL_ADDRESS" in
   *@*)
-    nkf -w $JOBLOG | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
+    cat -v $JOBLOG | nkf -w | mail -s "[cron][`/bin/hostname`] Diskcopy Log" $ADMIN_MAIL_ADDRESS
     ;;
 esac
 
