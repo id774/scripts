faa808fdaaff767a443c72a9774755cf812c25fa
id774 <idnanashi@gmail.com>
2015-08-06 12:36:42 +0900

    1621 Install mecab and cabocha binding when installing Ruby

--- installer/install_ruby_and_rails.sh
+++ installer/install_ruby_and_rails.sh
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v1.1 8/6,2015
+#       Install mecab and cabocha binding.
 #  v1.0 7/8,2013
 #       Cut out purge process.
 #  v0.1 7/11,2011
@@ -12,15 +14,17 @@
 ########################################################################
 
 install_ruby() {
-    $SCRIPTS/installer/install_ruby.sh 222 /opt/ruby/2.2
-    $SCRIPTS/installer/install_gems.sh /opt/ruby/2.2
+    $SCRIPTS/installer/install_ruby.sh $1 $2
+    which mecab > /dev/null && $SCRIPTS/installer/install_mecab_ruby.sh $2
+    which cabocha > /dev/null && $SCRIPTS/installer/install_cabocha_ruby.sh $2
+    $SCRIPTS/installer/install_gems.sh $2
 }
 
 operation() {
     test -n "$SCRIPTS" || export SCRIPTS=$HOME/scripts
     test -n "$PRIVATE" || export PRIVATE=$HOME/private/scripts
     $SCRIPTS/installer/purge_obsolete_sources.sh
-    install_ruby
+    install_ruby 222 /opt/ruby/2.2
 }
 
 operation $*
