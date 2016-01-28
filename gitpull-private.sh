6f14d223e021059a92a088f6f040b81de22c149c
id774 <idnanashi@gmail.com>
2016-01-28 11:05:45 +0900

    1643 Add ifexist option

--- gitpull-private.sh
+++ gitpull-private.sh
@@ -31,25 +31,41 @@ remove_github_repo() {
     fi
 }
 
-pull_from_gitbare() {
-    remove_github_repo $2
-    echo "Pulling $1 $2 $5"
-    if [ -e $HOME/local/$1/$2 ]; then
-        cd $HOME/local/$1/$2
-        test -n "$5" && git reset --hard
-        git pull
-    else
-        cd $HOME/local/$1
-        git clone ssh://$GITBARE_USER@$GITBARE_HOST/var/lib/git/$2.git
-    fi
+set_permission() {
     chmod 700 $HOME/local/$1/$2
     test -L $HOME/$2 || ln -fs $HOME/local/$1/$2 $HOME/$2
+}
+
+pull_repo() {
+    echo "Pulling $1 $2 $5"
+    cd $HOME/local/$1/$2
+    test "$5" = "--hard" && git reset --hard
+    git pull
+    set_permission $*
+}
+
+clone_repo() {
+    echo "Cloning $1 $2 $5"
+    cd $HOME/local/$1
+    git clone ssh://$GITBARE_USER@$GITBARE_HOST/var/lib/git/$2.git
+    set_permission $*
+}
+
+pull_from_gitbare() {
+    remove_github_repo $2
+    if [ -e $HOME/local/$1/$2 ]; then
+        pull_repo $*
+    else
+        if [ ! "$5" = "--ifexist" ]; then
+            clone_repo $*
+        fi
+    fi
 }
 
 gitpull_all() {
     pull_from_gitbare git private $*
     pull_from_gitbare git development $*
-    # pull_from_gitbare git data $*
+    pull_from_gitbare git data $*
     pull_from_gitbare git id774-net $*
     # pull_from_gitbare git kabbala $*
     # pull_from_gitbare git goodstory $*
