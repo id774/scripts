1ccc7884a310b8de24a7467a26204066490c1e6c
id774 <idnanashi@gmail.com>
2016-03-23 16:00:02 +0900

    1658 Delete pins after display

--- get_pins_from_fastladder.sh
+++ get_pins_from_fastladder.sh
@@ -4,4 +4,8 @@ SQL="select 'fav ' || link from pins;"
 COMMAND="sqlite3 -separator , $HOME/fastladder/db/fastladder.db"
 echo $SQL | $COMMAND
 
+SQL="delete from pins;"
+COMMAND="sqlite3 -separator , $HOME/fastladder/db/fastladder.db"
+echo $SQL | $COMMAND
+
 exit 0
