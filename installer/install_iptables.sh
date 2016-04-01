e3d77539a28138aef252e82dbb32fefbb786d5f0
id774 <idnanashi@gmail.com>
2016-04-01 15:36:31 +0900

    1668 Using iptables-persistent

--- installer/install_iptables.sh
+++ installer/install_iptables.sh
@@ -5,11 +5,23 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v1.2 4/1,2016
+#       Using iptables-persistent.
 #  v1.1 9/27,2011
 #       for RHEL.
 #  v1.0 8/15,2008
 #       Stable.
 ########################################################################
+
+smart_apt() {
+    while [ $# -gt 0 ]
+    do
+        if [ `aptitude search $1 | awk '/^i/' | wc -l` = 0 ]; then
+            sudo apt-get -y install $1
+        fi
+        shift
+    done
+}
 
 install_iptables() {
     case "$1" in
@@ -25,6 +37,7 @@ install_iptables() {
             ;;
         *)
             test -f /etc/network/if-pre-up.d/iptables && sudo rm /etc/network/if-pre-up.d/iptables
+            smart_apt iptables-persistent
             test -d /etc/iptables || sudo mkdir /etc/iptables
             sudo cp $SCRIPTS/etc/iptables/rules.v4 /etc/iptables/rules.v4
             sudo chmod 600 /etc/iptables/rules.v4
