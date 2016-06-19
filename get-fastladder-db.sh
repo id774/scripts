fa8d0b9034dacf6d0107ac008d77113b6b655894
id774 <idnanashi@gmail.com>
2016-06-19 12:18:19 +0900

    1718 Define fastladder host by args

--- get-fastladder-db.sh
+++ get-fastladder-db.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-USER=debian
-HOST=harpuia
+test -n "$1" && USER=$1
+test -n "$1" || USER=debian
+test -n "$2" && HOST=$2
+test -n "$2" || HOST=harpuia
+
 DB_PATH=fastladder/db/fastladder.db
 
 rsync -auvz $USER@$HOST:~/$DB_PATH ~/$DB_PATH
--- put-fastladder-db.sh
+++ put-fastladder-db.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-USER=debian
-HOST=harpuia
+test -n "$1" && USER=$1
+test -n "$1" || USER=debian
+test -n "$2" && HOST=$2
+test -n "$2" || HOST=harpuia
+
 DB_PATH=fastladder/db/fastladder.db
 
 sqlite3 ~/$DB_PATH vacuum
