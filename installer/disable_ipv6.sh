78483605b7856d72494d639daebaa1c27faafb21
id774 <idnanashi@gmail.com>
2016-07-08 10:29:20 +0900

    1723 Disable ipv6

--- installer/disable_ipv6.sh
+++ installer/disable_ipv6.sh
@@ -5,7 +5,10 @@ sudo sh -c 'echo "net.ipv6.conf.default.disable_ipv6 = 1">>/etc/sysctl.conf'
 sudo sh -c 'echo "net.ipv6.conf.lo.disable_ipv6 = 1">>/etc/sysctl.conf'
 
 sudo vim /etc/sysctl.conf
+sudo vim /etc/hosts
 
 sudo sysctl -p
 
 ip a | grep inet6
+
+cat /proc/sys/net/ipv6/conf/all/disable_ipv6
