531010ef484d214b7d5b349fd72d64b4bf92beec
id774 <idnanashi@gmail.com>
2016-07-27 17:26:26 +0900

    1730 PostgreSQL Backup

--- installer/install_postgres-backup.sh
+++ installer/install_postgres-backup.sh
@@ -10,7 +10,8 @@
 ########################################################################
 
 setup_environment() {
-    TARGET=/var/lib/postgresql
+    test -d /var/lib/postgresql && TARGET=/var/lib/postgresql
+    test -d /var/lib/pgsql && TARGET=/var/lib/pgsql
 
     case $OSTYPE in
       *darwin*)
@@ -26,10 +27,9 @@ install_script() {
     setup_environment
 
     sudo cp $OPTIONS $SCRIPTS/cron/etc/cron.d/postgres-backup /etc/cron.d/
-    test -d $TARGET && test -d $TARGET/backup || sudo mkdir -p $TARGET/backup
+    test -d $TARGET && test -d $TARGET/backup || sudo mkdir -vp $TARGET/backup
     test -d $TARGET/backup && sudo chown postgres:postgres $TARGET/backup
     test -d $TARGET/backup && sudo chmod 750 $TARGET/backup
 }
 
-test -d $TARGET || exit 1
 install_script
