de43386418d35a56e1a96399be6b7c37f4a53958
id774 <idnanashi@gmail.com>
2016-09-26 06:18:57 +0900

    1740 Correspond to HDD sdz

--- tcmount.py
+++ tcmount.py
@@ -5,6 +5,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v2.1 9/26,2016
+#       Correspond to sdz.
 #  v2.0 5/26,2014
 #       Show version on console.
 #       Mount with crypt files on home dir.
@@ -69,6 +71,26 @@ def mount_all(options, args, mount_options):
     partition(options, args, mount_options, 'sdd')
     partition(options, args, mount_options, 'sde')
     partition(options, args, mount_options, 'sdf')
+    partition(options, args, mount_options, 'sdg')
+    partition(options, args, mount_options, 'sdh')
+    partition(options, args, mount_options, 'sdi')
+    partition(options, args, mount_options, 'sdj')
+    partition(options, args, mount_options, 'sdk')
+    partition(options, args, mount_options, 'sdl')
+    partition(options, args, mount_options, 'sdm')
+    partition(options, args, mount_options, 'sdn')
+    partition(options, args, mount_options, 'sdo')
+    partition(options, args, mount_options, 'sdp')
+    partition(options, args, mount_options, 'sdq')
+    partition(options, args, mount_options, 'sdr')
+    partition(options, args, mount_options, 'sds')
+    partition(options, args, mount_options, 'sdt')
+    partition(options, args, mount_options, 'sdu')
+    partition(options, args, mount_options, 'sdv')
+    partition(options, args, mount_options, 'sdw')
+    partition(options, args, mount_options, 'sdx')
+    partition(options, args, mount_options, 'sdy')
+    partition(options, args, mount_options, 'sdz')
 
 def mount_legacy(options, args):
     mount_local('pc98a', options)
@@ -101,7 +123,7 @@ def tcmount(options, args):
         mount_device(options, args)
 
 def main():
-    version = "2.0"
+    version = "2.1"
     from optparse import OptionParser
     usage = "usage: %prog [options]"
     parser = OptionParser(usage)
