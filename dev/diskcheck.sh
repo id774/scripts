bddd26de5a99bc786459f3b3f88974581c0d2b7b
id774 <idnanashi@gmail.com>
2016-09-26 13:04:19 +0900

    1743 Add disk check script

---
+++ dev/diskcheck.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+test -n "$1" && SOURCE_DRV=$1
+test -n "$1" || SOURCE_DRV=$HOME/mnt/sdf
+test -n "$2" && TARGET_DRV=$2
+test -n "$2" || TARGET_DRV=$HOME/mnt/sdg
+test -n "$3" && ADMIN_MAIL_ADDRESS=$3
+test -n "$3" || ADMIN_MAIL_ADDRESS=sysadmin@id774.net
+
+test -d $SOURCE_DRV || exit 1
+test -d $TARGET_DRV || exit 1
+
+JOBLOG=$TMP/diskcheck.log
+
+echo "diskcheck start.">$JOBLOG
+
+echo -n "Diskcheck job started at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+
+rsync --dry-run -avz --no-o --no-g --delete $SOURCE_DRV/user1 $HOME/mnt/$T_MOUNT/>>$JOBLOG
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcheck user1 completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+rsync --dry-run -avz --no-o --no-g --delete $SOURCE_DRV/user2 $TARGET_DRV/>>$JOBLOG
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcheck user2 completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+rsync --dry-run -avz --no-o --no-g --delete $SOURCE_DRV/user3 $TARGET_DRV/>>$JOBLOG
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcheck user3 completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+rsync --dry-run -avz --no-o --no-g --delete $SOURCE_DRV/largefiles $TARGET_DRV/>>$JOBLOG
+echo "Return code is $?">>$JOBLOG
+echo -n "Diskcheck largefiles completed at `/bin/hostname` on ">>$JOBLOG
+date "+%Y/%m/%d %T">>$JOBLOG
+
+case "$ADMIN_MAIL_ADDRESS" in
+  *@*)
+    cat -v $JOBLOG | nkf -w | mail -s "[admin][`/bin/hostname`] Diskcheck Log" $ADMIN_MAIL_ADDRESS
+    ;;
+esac
