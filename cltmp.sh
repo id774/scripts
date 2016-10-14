fa9789dc72942429b50c4367df719c407a770587
id774 <idnanashi@gmail.com>
2016-10-14 15:08:49 +0900

    1750 Clear package cache

--- cltmp.sh
+++ cltmp.sh
@@ -6,7 +6,6 @@ rm -vf ~/hardcopy.*
 rm -vf ~/wget-log*
 rm -vf ~/.emacs.d/%backup%\~
 rm -vf ~/%backup%\~
-rm -vf ~/.zcompdump
 rm -vrf ~/.emacs.d/tmp/*
 rm -vrf ~/.emacs.d/backups/*
 rm -vrf ~/.emacs.d/auto-save-list/.saves*
@@ -15,5 +14,10 @@ rm -vf ~/Pictures/*.*g
 rm -vf ~/Pictures/*.*G
 rm -vf ~/Downloads/*
 rm -vf ~/Desktop/*
-echo "cltmp (20160604) done."
+test -d ~/.gem && rm -vrf ~/.gem
+test -d ~/.pip && rm -vrf ~/.pip
+test -d ~/.npm && rm -vrf ~/.npm
+test -d /root/.cache && rm -vrf /root/.cache
+test -d ~/fastladder/log/development.log && rm -vf ~/fastladder/log/development.log
+echo "cltmp (20161014) done."
 
