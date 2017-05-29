29fcd666845378d42be97a5e838add95b1ce4ea6
id774 <idnanashi@gmail.com>
2017-05-29 10:03:08 +0900

    1775 Add stocks backup script installer

---
+++ installer/install_stocks-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+########################################################################
+# Install Stocks backup
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v0.1 5/29,2017
+#       First.
+########################################################################
+
+setup_environment() {
+    case $OSTYPE in
+      *darwin*)
+        OPTIONS=-Rv
+        ;;
+      *)
+        OPTIONS=-Rvd
+        ;;
+    esac
+}
+
+install_script() {
+    setup_environment
+    sudo cp $OPTIONS $SCRIPTS/cron/etc/cron.d/stocks-backup /etc/cron.d/
+    sudo chown root:adm /etc/cron.d/stocks-backup
+    sudo chmod 640 /etc/cron.d/stocks-backup
+}
+
+install_script
