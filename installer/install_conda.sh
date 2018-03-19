3ff9f924d70af342b14bee23269f5bc91480c6e4
id774 <idnanashi@gmail.com>
2018-03-19 13:50:28 +0900

    1798 Remove unnecessary version designation

--- installer/install_conda.sh
+++ installer/install_conda.sh
@@ -3,7 +3,6 @@
 ########################################################################
 # Install Anaconda Libraries.
 #  $1 = python path (ex. /usr/local)
-#  $2 = no sudo
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
@@ -20,7 +19,7 @@ setup_environment() {
 
 install_libs() {
     $PIP install -y pip
-    $PIP install -y 'IPython>=3.1.0'
+    $PIP install -y IPython
     $PIP install -y jupyter
     $PIP install -y notebook
     $PIP install -y pyflakes
@@ -35,16 +34,16 @@ install_libs() {
     $PIP install -y docopt
     $PIP install -y simplejson
     $PIP install -y msgpack-python
-    $PIP install -y 'numpy>=1.11.0'
-    $PIP install -y 'scipy>=0.17.1'
-    $PIP install -y 'scikit-learn>=0.17.1'
+    $PIP install -y numpy
+    $PIP install -y scipy
+    $PIP install -y scikit-learn
     $PIP install -y japandas
-    $PIP install -y 'pandas-datareader>=0.2.1'
+    $PIP install -y pandas-datareader
     $PIP install -y chainer
     $PIP install -y joblib
     $PIP install -y dask
     $PIP install -y patsy
-    $PIP install -y 'statsmodels>=0.6.1'
+    $PIP install -y statsmodels
     $PIP install -y sympy
     #$PIP install -y pystan
     $PIP install -y seaborn
@@ -55,8 +54,8 @@ install_libs() {
     $PIP install -y Flask-Bootstrap
     $PIP install -y Hamlish-Jinja
     $PIP install -y gunicorn
-    $PIP install -y 'django>=1.8'
-    $PIP install -y 'SQLAlchemy>=1.0.2'
+    $PIP install -y django
+    $PIP install -y SQLAlchemy
     $PIP install -y lmdb
     $PIP install -y migrate
     $PIP install -y readline
@@ -67,8 +66,8 @@ install_libs() {
     $PIP install -y cherrypy
     $PIP install -y beautifulsoup4
     $PIP install -y lxml
-    $PIP install -y 'requests<3.0'
-    $PIP install -y 'pysolr<4.0'
+    $PIP install -y requests
+    $PIP install -y pysolr
     $PIP install -y watson-developer-cloud
     $PIP install -y html5lib
     $PIP install -y husl
@@ -92,9 +91,9 @@ install_libs() {
     $PIP install -y python-tr
     $PIP install -y -c conda-forge keras
     $PIP install -y tensorflow
-    $PIP install -y 'matplotlib<1.5'
-    $PIP install -y 'pandas<0.17'
-    $PIP install -y 'pep8<1.6'
+    $PIP install -y matplotlib
+    $PIP install -y pandas
+    $PIP install -y pep8
     $EASY_INSTALL TA-Lib
     $EASY_INSTALL nltk
 }
