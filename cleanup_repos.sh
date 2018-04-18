6df7094d40f219f6f67ea96a349c3878b2d7988d
id774 <idnanashi@gmail.com>
2018-04-18 20:51:58 +0900

    1813 sort

--- cleanup_repos.sh
+++ cleanup_repos.sh
@@ -14,4 +14,76 @@ clean_repo() {
     done
 }
 
-clean_repo automaticruby termtter termtter-plugins stdout sysadmin instant-deployer twitter_viewer blog_viewer repo_manager abuse naivebayes kmeans recommendation vocabulary depression numerology hotnews list_shift rurima batch_framework hadoop-streaming heartbeat-id774net ctoD okura rails4-bootstrap rails5-bootstrap sinatra-api-provider sinatra-bootstrap fluentd-json-receiver house_api_web scipy-lecture-notes rubytter fluentd rbp helm rhtml rinari popup-el fuzzy-el html5-el haml-mode sass-mode scss-mode coffee-mode js2-mode shadow.el shadow.vim html5.vim vim-coffee-script vim-ham anything-config munin-plugins deferred-sync intraweb-template d3js-charts  d3js-data-clips d3js-stacked-chart flask-hello flask-bootstrap auto-complete emacs-deferred emacs-highlight-unique-symbol zencoding vim-haml weekly-report service_portal news_cloud wordpress
+clean_repo \
+  abuse \
+  anything-config \
+  auto-complete \
+  automaticruby \
+  batch_framework \
+  blog_viewer \
+  classify \
+  coffee-mode \
+  ctoD \
+  d3js-charts \
+  d3js-data-clips \
+  d3js-stacked-chart \
+  deferred-sync \
+  depression \
+  emacs-deferred \
+  emacs-highlight-unique-symbol \
+  flask-bootstrap \
+  flask-hello \
+  fluentd \
+  fluentd-json-receiver \
+  fuzzy-el \
+  goodstory \
+  hadoop-streaming \
+  haml-mode \
+  heartbeat-id774net \
+  helm \
+  hotnews \
+  house_api_web \
+  html5-el \
+  html5.vim \
+  instant-deployer \
+  intraweb-template \
+  js2-mode \
+  kmeans \
+  list_shift \
+  munin-plugins \
+  naivebayes \
+  news_cloud \
+  newscloud-sinatra \
+  numerology \
+  okura \
+  popup-el \
+  rails4-bootstrap \
+  rails5-bootstrap \
+  rbp \
+  recommendation \
+  redmine \
+  repo_manager \
+  rhtml \
+  rinari \
+  rubytter \
+  rurima \
+  sass-mode \
+  scipy-lecture-notes \
+  scss-mode \
+  service_portal \
+  shadow.el \
+  shadow.vim \
+  sinatra-api-provider \
+  sinatra-bootstrap \
+  stdout \
+  sysadmin \
+  termtter \
+  termtter-plugins \
+  twitter_viewer \
+  vim-coffee-script \
+  vim-ham \
+  vim-haml \
+  vocabulary \
+  weekly-report \
+  wordpress \
+  zencoding
