6eebc16d9945bc4765168b8ab25e650f75106b41
id774 <idnanashi@gmail.com>
2018-05-02 09:48:11 +0900

    1820 Append channels conda-forge

--- installer/install_conda.sh
+++ installer/install_conda.sh
@@ -6,6 +6,8 @@
 #
 #  Maintainer: id774 <idnanashi@gmail.com>
 #
+#  v0.2 5/2,2018
+#       Append channels conda-forge.
 #  v0.1 2/28,2018
 #       First.
 ########################################################################
@@ -13,90 +15,91 @@
 setup_environment() {
     test -n "$1" && export EASY_INSTALL=$1/bin/easy_install
     test -n "$1" || export EASY_INSTALL=$HOME/local/anaconda3/bin/easy_install
-    test -n "$1" && export PIP=$1/bin/conda
-    test -n "$1" || export PIP=$HOME/local/anaconda3/bin/conda
+    test -n "$1" && export CONDA=$1/bin/conda
+    test -n "$1" || export CONDA=$HOME/local/anaconda3/bin/conda
+    $CONDA config --append channels conda-forge
 }
 
 install_libs() {
-    $PIP install -y pip
-    $PIP install -y IPython
-    $PIP install -y jupyter
-    $PIP install -y notebook
-    $PIP install -y pyflakes
-    $PIP install -y flake8
-    $PIP install -y pytest
-    $PIP install -y pytest-pep8
-    $PIP install -y autopep8
-    $PIP install -y autoflake
-    $PIP install -y Cython
-    $PIP install -y docutils
-    $PIP install -y nose
-    $PIP install -y docopt
-    $PIP install -y simplejson
-    $PIP install -y msgpack-python
-    $PIP install -y numpy
-    $PIP install -y scipy
-    $PIP install -y scikit-learn
-    $PIP install -y japandas
-    $PIP install -y pandas-datareader
-    $PIP install -y chainer
-    $PIP install -y joblib
-    $PIP install -y dask
-    $PIP install -y patsy
-    $PIP install -y statsmodels
-    $PIP install -y sympy
-    #$PIP install -y pystan
-    $PIP install -y seaborn
-    $PIP install -y bokeh
-    $PIP install -y twisted
-    $PIP install -y Flask
-    $PIP install -y Flask-Assets
-    $PIP install -y Flask-Bootstrap
-    $PIP install -y Hamlish-Jinja
-    $PIP install -y gunicorn
-    $PIP install -y django
-    $PIP install -y SQLAlchemy
-    $PIP install -y lmdb
-    $PIP install -y migrate
-    $PIP install -y readline
-    $PIP install -y Pygments
-    $PIP install -y Babel
-    $PIP install -y Genshi
-    $PIP install -y bottle
-    $PIP install -y cherrypy
-    $PIP install -y beautifulsoup4
-    $PIP install -y lxml
-    $PIP install -y requests
-    $PIP install -y pysolr
-    $PIP install -y watson-developer-cloud
-    $PIP install -y html5lib
-    $PIP install -y husl
-    $PIP install -y pillow
-    $PIP install -y ggplot
-    $PIP install -y pyper
-    $PIP install -y jinja2 tornado pyzmq
-    $PIP install -y awscli
-    $PIP install -y cchardet
-    $PIP install -y openpyxl
-    $PIP install -y xlrd
-    $PIP install -y simpy
-    $PIP install -y networkx
-    $PIP install -y pdfminer3k
-    $PIP install -y pybrain
-    $PIP install -y uwsgi
-    $PIP install -y pypandoc
-    $PIP install -y zipline
-    $PIP install -y DocumentFeatureSelection
-    $PIP install -y python-tr
-    $PIP install -y mod_wsgi
-    $PIP install -y beaker
-    $PIP install -y python-memcached
-    $PIP install -y psycopg2-binary
-    $PIP install -y -c conda-forge keras
-    $PIP install -y tensorflow
-    $PIP install -y matplotlib
-    $PIP install -y pandas
-    $PIP install -y pep8
+    $CONDA install -y pip
+    $CONDA install -y IPython
+    $CONDA install -y jupyter
+    $CONDA install -y notebook
+    $CONDA install -y pyflakes
+    $CONDA install -y flake8
+    $CONDA install -y pytest
+    $CONDA install -y pytest-pep8
+    $CONDA install -y autopep8
+    $CONDA install -y autoflake
+    $CONDA install -y Cython
+    $CONDA install -y docutils
+    $CONDA install -y nose
+    $CONDA install -y docopt
+    $CONDA install -y simplejson
+    $CONDA install -y msgpack-python
+    $CONDA install -y numpy
+    $CONDA install -y scipy
+    $CONDA install -y scikit-learn
+    $CONDA install -y japandas
+    $CONDA install -y pandas-datareader
+    $CONDA install -y chainer
+    $CONDA install -y joblib
+    $CONDA install -y dask
+    $CONDA install -y patsy
+    $CONDA install -y statsmodels
+    $CONDA install -y sympy
+    #$CONDA install -y pystan
+    $CONDA install -y seaborn
+    $CONDA install -y bokeh
+    $CONDA install -y twisted
+    $CONDA install -y Flask
+    $CONDA install -y Flask-Assets
+    $CONDA install -y Flask-Bootstrap
+    $CONDA install -y Hamlish-Jinja
+    $CONDA install -y gunicorn
+    $CONDA install -y django
+    $CONDA install -y SQLAlchemy
+    $CONDA install -y lmdb
+    $CONDA install -y migrate
+    $CONDA install -y readline
+    $CONDA install -y Pygments
+    $CONDA install -y Babel
+    $CONDA install -y Genshi
+    $CONDA install -y bottle
+    $CONDA install -y cherrypy
+    $CONDA install -y beautifulsoup4
+    $CONDA install -y lxml
+    $CONDA install -y requests
+    $CONDA install -y pysolr
+    $CONDA install -y watson-developer-cloud
+    $CONDA install -y html5lib
+    $CONDA install -y husl
+    $CONDA install -y pillow
+    $CONDA install -y ggplot
+    $CONDA install -y pyper
+    $CONDA install -y jinja2 tornado pyzmq
+    $CONDA install -y awscli
+    $CONDA install -y cchardet
+    $CONDA install -y openpyxl
+    $CONDA install -y xlrd
+    $CONDA install -y simpy
+    $CONDA install -y networkx
+    $CONDA install -y pdfminer3k
+    $CONDA install -y pybrain
+    $CONDA install -y uwsgi
+    $CONDA install -y pypandoc
+    $CONDA install -y zipline
+    $CONDA install -y DocumentFeatureSelection
+    $CONDA install -y python-tr
+    $CONDA install -y mod_wsgi
+    $CONDA install -y beaker
+    $CONDA install -y python-memcached
+    $CONDA install -y psycopg2-binary
+    $CONDA install -y keras
+    $CONDA install -y tensorflow
+    $CONDA install -y matplotlib
+    $CONDA install -y pandas
+    $CONDA install -y pep8
     $EASY_INSTALL TA-Lib
     $EASY_INSTALL nltk
 }
