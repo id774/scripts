0c43d05cc8e723962eb665381ffff5c34d233a07
id774 <idnanashi@gmail.com>
2018-08-20 14:16:37 +0900

    1838 Remove obsolete scripts

--- dev/mail_sender_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-#
-ifconfig | mail -s "[admin][`/bin/hostname`] This is test mail" idnanashi@gmail.com
--- dev/remove_python3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo aptitude purge python3.2 python3-minimal python3.2-minimal
-sudo update-alternatives --remove-all python
-
