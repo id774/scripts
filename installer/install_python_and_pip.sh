4bb25e29a8ecbcea85f59260f4c3c1f1eab95ee1
id774 <idnanashi@gmail.com>
2019-04-19 01:11:05 +0900

    1856 Update to Python 3.7.3

--- installer/install_python_and_pip.sh
+++ installer/install_python_and_pip.sh
@@ -18,9 +18,9 @@ install_python() {
 }
 
 operation() {
-    test -n "$1" || VERSION=3.6.5
+    test -n "$1" || VERSION=3.7.3
     test -n "$1" && VERSION=$1
-    test -n "$2" || PATH_VERSION=3.6
+    test -n "$2" || PATH_VERSION=3.7
     test -n "$2" && PATH_VERSION=$1
     test -n "$SCRIPTS" || export SCRIPTS=$HOME/scripts
     test -n "$PRIVATE" || export PRIVATE=$HOME/private/scripts
