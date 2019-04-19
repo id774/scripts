bb22f3dbbf070cfc0efdc808c1fbe53385e2414a
id774 <idnanashi@gmail.com>
2019-04-20 02:17:36 +0900

    1857 Improvement fastladder script

--- vacuum-fastladder-db.sh
+++ vacuum-fastladder-db.sh
@@ -3,7 +3,7 @@
 cd $HOME/fastladder/db
 DB_PATH=fastladder/db/fastladder.db
 
-rm new.db
+test -f new.db && rm -vf new.db
 sqlite3 $HOME/$DB_PATH vacuum
 sqlite3 fastladder.db .dump | sqlite3 new.db
 
