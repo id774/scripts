0d1996da7f5c1eeaac07a72e76dc8e91ecb2987f
id774 <idnanashi@gmail.com>
2019-10-18 11:41:59 +0900

    1874 Update to Python 3.7.5

--- installer/install_python_and_pip.sh
+++ installer/install_python_and_pip.sh
@@ -18,7 +18,7 @@ install_python() {
 }
 
 operation() {
-    test -n "$1" || VERSION=3.7.3
+    test -n "$1" || VERSION=3.7.5
     test -n "$1" && VERSION=$1
     test -n "$2" || PATH_VERSION=3.7
     test -n "$2" && PATH_VERSION=$1
