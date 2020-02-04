e4fb52b127dd9506bc4f892dbec122b6f6df1f68
id774 <idnanashi@gmail.com>
2020-02-04 13:02:17 +0900

    1883 Add dconf setup script

---
+++ installer/setup_keys_for_workspaces_on_dconf.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+########################################################################
+# Batch Setup Script for dconf-editor
+#
+#  Maintainer: id774 <idnanashi@gmail.com>
+#
+#  v0.1 2/4,2020
+#       First.
+########################################################################
+
+setup_switch_keys() {
+    while [ $# -gt 0 ]
+    do
+        dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-$1 "['<Primary>$1']"
+        dconf read /org/gnome/desktop/wm/keybindings/switch-to-workspace-$1
+        shift
+    done
+}
+
+setup_switch_keys 1 2 3 4 5 6 7 8 9
+
+setup_move_keys() {
+    while [ $# -gt 0 ]
+    do
+        dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-$1 "['<Primary><Alt>$1']"
+        dconf read /org/gnome/desktop/wm/keybindings/move-to-workspace-$1
+        shift
+    done
+}
+
+setup_move_keys 1 2 3 4 5 6 7 8 9
