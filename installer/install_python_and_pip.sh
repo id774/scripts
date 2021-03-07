63c6bfff82749a2ac19ae24be1cc7f7c76a1239d
id774 <idnanashi@gmail.com>
2021-03-08 03:21:46 +0900

    1905 Update to Python 3.9.2

--- installer/install_python_and_pip.sh
+++ installer/install_python_and_pip.sh
@@ -18,7 +18,7 @@ install_python() {
 }
 
 operation() {
-    test -n "$1" || VERSION=3.9.0
+    test -n "$1" || VERSION=3.9.2
     test -n "$1" && VERSION=$1
     test -n "$2" || PATH_VERSION=3.9
     test -n "$2" && PATH_VERSION=$1
