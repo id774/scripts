3bec8aa8adb981702cc307e894fc1d17510455cd
id774 <idnanashi@gmail.com>
2022-08-05 18:53:13 +0900

    1917 Remove blank line padding

--- wakeonlan.py
+++ wakeonlan.py
@@ -2,9 +2,9 @@ import sys
 import socket
 import struct
 from traceback import print_exc
- 
+
 DEFAULT_PORT = 9
- 
+
 def send_magic_packet(addr):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -17,11 +17,11 @@ def send_magic_packet(addr):
         magicp = b''
         for i in range(0, len(buf_), 2):
             magicp += struct.pack('B', int(buf_[i:i + 2], 16))
- 
+
         print('Sending magic packet to 255.255.255.255:{} with {}'.format(DEFAULT_PORT, addr))
         s.sendto(magicp, ('<broadcast>', DEFAULT_PORT))
- 
- 
+
+
 if __name__ == '__main__':
     argsmin = 1
     version = (3, 0)
