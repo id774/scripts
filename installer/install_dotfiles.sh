05bea5758e0a67528bf96576174c58a3af0e19b6
id774 <idnanashi@gmail.com>
2023-03-11 12:53:12 +0900

    1952 Append .condarc to dotfiles installer

--- installer/install_dotfiles.sh
+++ installer/install_dotfiles.sh
@@ -76,7 +76,7 @@ emacs_batch_byte_compile() {
 }
 
 deploy_dotfile() {
-    for DOT_FILES in zshrc screenrc vimrc gvimrc gitconfig gitignore gemrc Rprofile emacs
+    for DOT_FILES in zshrc screenrc vimrc gvimrc gitconfig gitignore condarc gemrc Rprofile emacs
     do
         test -d $1 && sudo cp $OPTIONS $SCRIPTS/dot_files/dot_$DOT_FILES $1/.$DOT_FILES
     done
