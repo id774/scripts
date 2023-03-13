eb91c14e5c7b921ddd4052129439ea135a9340a7
id774 <idnanashi@gmail.com>
2023-03-14 02:07:31 +0900

    1955 Unlock System Folder Localizations

---
+++ dev/macos_system_folder_unlocalizations.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+test -f ~/Applications/.localized && rm -vf ~/Applications/.localized
+test -f ~/Documents/.localized && rm -vf ~/Documents/.localized
+test -f ~/Downloads/.localized && rm -vf ~/Downloads/.localized
+test -f ~/Desktop/.localized && rm -vf ~/Desktop/.localized
+test -f ~/Public/.localized && rm -vf ~/Public/.localized
+test -f ~/Pictures/.localized && rm -vf ~/Pictures/.localized
+test -f ~/Music/.localized && rm -vf ~/Music/.localized
+test -f ~/Movies/.localized && rm -vf ~/Movies/.localized
+test -f ~/Library/.localized && rm -vf ~/Library/.localized
+test -f /Applications/.localized && sudo rm -vf /Applications/.localized
