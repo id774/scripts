3b44b44d58b5d7ee5407f1a863623f7a7abb1eca
id774 <idnanashi@gmail.com>
2023-03-16 10:55:37 +0900

    1956 System Folder Localizations

---
+++ dev/macos_system_folder_localizations.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+test -f ~/Applications/.localized || touch ~/Applications/.localized
+test -f ~/Documents/.localized || touch ~/Documents/.localized
+test -f ~/Downloads/.localized || touch ~/Downloads/.localized
+test -f ~/Desktop/.localized || touch ~/Desktop/.localized
+test -f ~/Public/.localized || touch ~/Public/.localized
+test -f ~/Pictures/.localized || touch ~/Pictures/.localized
+test -f ~/Music/.localized || touch ~/Music/.localized
+test -f ~/Movies/.localized || touch ~/Movies/.localized
+test -f ~/Library/.localized || touch ~/Library/.localized
+test -f /Applications/.localized || sudo touch /Applications/.localized
+test -f /Applications/Utilities/.localized || sudo touch /Applications/Utilities/.localized
