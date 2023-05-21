6ccd447fe8775e4497102bdf49f26b1e20719e05
id774 <idnanashi@gmail.com>
2023-05-21 19:17:20 +0900

    1966 Set sshd configuration for mac

--- check_sshd_config.sh
+++ check_sshd_config.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-grep Port /etc/ssh/sshd_config | grep -v "#"
-grep PermitRootLogin /etc/ssh/sshd_config | grep -v "#"
-grep PasswordAuthentication /etc/ssh/sshd_config | grep -v "#"
-grep ChallengeResponseAuthentication /etc/ssh/sshd_config | grep -v "#"
+os=$(uname)
 
+if [ "$os" = "Darwin" ]; then
+  test -f /etc/ssh/sshd_config.d/000-sshdconfig.conf || sudo cp -v $SCRIPTS/etc/sshd_config.d/000-sshdconfig.conf /etc/ssh/sshd_config.d/000-sshdconfig.conf
+  cat /etc/ssh/sshd_config.d/000-sshdconfig.conf
+else
+  grep Port /etc/ssh/sshd_config | grep -v "#"
+  grep PermitRootLogin /etc/ssh/sshd_config | grep -v "#"
+  grep PasswordAuthentication /etc/ssh/sshd_config | grep -v "#"
+  grep ChallengeResponseAuthentication /etc/ssh/sshd_config | grep -v "#"
+fi
+
+exit 0
