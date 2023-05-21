567d143a4cdbd0811ae3fbd6d1f9d7cf24181514
id774 <idnanashi@gmail.com>
2023-05-21 19:10:56 +0900

    1965 Make scripts Mac-Only

--- dev/fix_compinit.sh
+++ dev/fix_compinit.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-sudo chown -R root:wheel /usr/local/Homebrew/completions/zsh/
-sudo chown -R root:wheel /usr/local/share/zsh/
+os=$(uname)
+
+if [ "$os" = "Darwin" ]; then
+  sudo chown -R root:wheel /usr/local/Homebrew/completions/zsh/
+  sudo chown -R root:wheel /usr/local/share/zsh/
+else
+  exit 1
+fi
+
+exit 0
