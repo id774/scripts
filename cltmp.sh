920cc8ff782dd3978dd41aa69edac3b096b268da
id774 <idnanashi@gmail.com>
2023-06-21 20:49:36 +0900

    1978 Improved temporary file cleaner

--- cltmp.sh
+++ cltmp.sh
@@ -8,6 +8,7 @@ if [ "$os" = "Darwin" ]; then
   touch $HOME/Documents/.localized
   touch $HOME/Downloads/.localized
   touch $HOME/Desktop/.localized
+  test -d $HOME/largefiles/dashcam && find $HOME/largefiles/dashcam -type f -mtime +30 -exec rm -vf {} \;
 elif [ "$os" = "Linux" ]; then
   test -d /root/.cache && rm -vrf /root/.cache
   rm -vf $HOME/hardcopy.*
@@ -19,16 +20,16 @@ rm -vf "$HOME/%backup%$HOME"
 test -d $HOME/.gem && rm -vrf $HOME/.gem
 test -d $HOME/.pip && rm -vrf $HOME/.pip
 test -d $HOME/.npm && rm -vrf $HOME/.npm
-test -d $HOME/tmp && rm -vrf $HOME/tmp/*
+test -d $HOME/tmp && find $HOME/tmp -type f -mtime +3 -exec rm -vf {} \;
 test -d $HOME/.tmp && find $HOME/.tmp -type f -mtime +7 -exec rm -vf {} \;
 test -d $HOME/.emacs.d/tmp && find $HOME/.emacs.d/tmp -type f -mtime +30 -exec rm -vf {} \;
 test -d $HOME/.emacs.d/backups && find $HOME/.emacs.d/backups -type f -mtime +30 -exec rm -vf {} \;
 test -d $HOME/.emacs.d/auto-save-list && find $HOME/.emacs.d/auto-save-list -type f -mtime +30 -exec rm -vf {} \;
 test -d $HOME/.emacs.d/tramp-auto-save && find $HOME/.emacs.d/tramp-auto-save -type f -mtime +30 -exec rm -vf {} \;
 test -d $HOME/Pictures && find $HOME/Pictures -type f -mtime +30 -exec rm -vf {} \;
-test -d $HOME/Downloads && find $HOME/Downloads -type f -mtime +30 -exec rm -vf {} \;
-test -d $HOME/Desktop && find $HOME/Desktop -type f -mtime +30 -exec rm -vf {} \;
+test -d $HOME/Downloads && find $HOME/Downloads -type f -mtime +7 -exec rm -vf {} \;
+test -d $HOME/Desktop && find $HOME/Desktop -type f -mtime +7 -exec rm -vf {} \;
 test -d $HOME/twitter_viewer/log && find $HOME/twitter_viewer/log -type f -mtime +7 -exec rm -vf {} \;
 test -d $HOME/fastladder/log && find $HOME/fastladder/log -type f -mtime +7 -exec rm -vf {} \;
-echo "cltmp (20230609) done."
+echo "cltmp (20230621) done."
 
