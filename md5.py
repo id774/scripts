388f7010af32c001b956ab26312fc9cbc714f317
id774 <idnanashi@gmail.com>
2023-06-24 21:27:09 +0900

    1987 Implement given string option

--- md5.py
+++ md5.py
@@ -16,31 +16,33 @@ class Md5Checksum:
                 m.update(line)
         return m.hexdigest()
 
+    @staticmethod
+    def calculate_checksum_for_string(input_string):
+        m = hashlib.md5()
+        m.update(input_string.encode('utf-8'))
+        return m.hexdigest()
+
 def print_checksum(include_subdir, reversed_format, quiet_mode, paths):
-    script_name = os.path.basename(sys.argv[0])
     for path in paths:
-        if not os.path.exists(path):
-            print("{0}: {1}: No such file or directory".format(script_name, path))
-            continue
         if include_subdir and S_ISDIR(os.stat(path)[ST_MODE]):
             for root, dirs, files in os.walk(path):
                 for file in files:
                     full_path = os.path.join(root, file)
                     checksum = Md5Checksum.calculate_checksum(full_path)
-                    print_formatted_checksum(checksum, full_path, reversed_format, quiet_mode)
+                    print_formatted_checksum(checksum, full_path, reversed_format, quiet_mode, is_file=True)
         elif S_ISDIR(os.stat(path)[ST_MODE]):
-            print("{0}: {1}: Is a directory".format(script_name, path))
+            print(f"{os.path.basename(sys.argv[0])}: {path}: Is a directory")
         else:
             checksum = Md5Checksum.calculate_checksum(path)
-            print_formatted_checksum(checksum, path, reversed_format, quiet_mode)
+            print_formatted_checksum(checksum, path, reversed_format, quiet_mode, is_file=True)
 
-def print_formatted_checksum(checksum, path, reversed_format, quiet_mode):
+def print_formatted_checksum(checksum, path, reversed_format, quiet_mode, is_file):
     if quiet_mode:
         print(checksum)
     elif reversed_format:
-        print("{0} {1}".format(checksum, path))
+        print("{0} {1}".format(checksum, path if is_file else f'"{path}"'))
     else:
-        print("MD5 ({0}) = {1}".format(path, checksum))
+        print("MD5 ({0}) = {1}".format(path if is_file else f'"{path}"', checksum))
 
 def main():
     usage = "usage: %prog [options] file"
@@ -53,9 +55,14 @@ def main():
                       action="store_true", dest="reversed_format")
     parser.add_option("-q", "--quiet", help="quiet mode - only the checksum is printed out",
                       action="store_true", dest="quiet_mode")
+    parser.add_option("-s", "--string", help="print a checksum of the given string",
+                      action="store", type="string", dest="input_string")
     (options, args) = parser.parse_args()
     if options.version:
         print("{0} Version 1.0".format(os.path.basename(sys.argv[0])))
+    elif options.input_string:
+        checksum = Md5Checksum.calculate_checksum_for_string(options.input_string)
+        print_formatted_checksum(checksum, options.input_string, options.reversed_format, options.quiet_mode, is_file=False)
     elif len(args) < 1:
         parser.print_help()
     else:
