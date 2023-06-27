847ab28d41b63a3ea265e4bdcb8bd416b0b2be69
id774 <idnanashi@gmail.com>
2023-06-27 09:11:36 +0900

    1991 Improvement temp file cleaner

--- cltmp.sh
+++ cltmp.sh
@@ -9,9 +9,11 @@ if [ "$os" = "Darwin" ]; then
   touch $HOME/Downloads/.localized
   touch $HOME/Desktop/.localized
   test -d $HOME/largefiles/dashcam && find $HOME/largefiles/dashcam -type f -mtime +30 -exec rm -vf {} \;
+  test -d $HOME/tmp && find $HOME/tmp -type f -mtime +3 -exec rm -vf {} \;
 elif [ "$os" = "Linux" ]; then
   test -d /root/.cache && rm -vrf /root/.cache
   rm -vf $HOME/hardcopy.*
+  test -d $HOME/tmp && find $HOME/tmp -type f -mtime +1 -exec rm -vf {} \;
 fi
 
 rm -vf "$HOME/wget-log*"
@@ -20,7 +22,6 @@ rm -vf "$HOME/%backup%$HOME"
 test -d $HOME/.gem && rm -vrf $HOME/.gem
 test -d $HOME/.pip && rm -vrf $HOME/.pip
 test -d $HOME/.npm && rm -vrf $HOME/.npm
-test -d $HOME/tmp && find $HOME/tmp -type f -mtime +3 -exec rm -vf {} \;
 test -d $HOME/.tmp && find $HOME/.tmp -type f -mtime +7 -exec rm -vf {} \;
 test -d $HOME/.emacs.d/tmp && find $HOME/.emacs.d/tmp -type f -mtime +30 -exec rm -vf {} \;
 test -d $HOME/.emacs.d/backups && find $HOME/.emacs.d/backups -type f -mtime +30 -exec rm -vf {} \;
@@ -31,5 +32,5 @@ test -d $HOME/Downloads && find $HOME/Downloads -type f -mtime +7 -exec rm -vf {
 test -d $HOME/Desktop && find $HOME/Desktop -type f -mtime +7 -exec rm -vf {} \;
 test -d $HOME/twitter_viewer/log && find $HOME/twitter_viewer/log -type f -mtime +7 -exec rm -vf {} \;
 test -d $HOME/fastladder/log && find $HOME/fastladder/log -type f -mtime +7 -exec rm -vf {} \;
-echo "cltmp (20230621) done."
+echo "cltmp (20230627) done."
 
