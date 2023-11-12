060855ee452658c6af6e664c4fef33e056eed865
id774 <idnanashi@gmail.com>
2023-11-12 14:25:40 +0900

    2012 Refactoring for veracrypt script

--- veramount.sh
+++ veramount.sh
@@ -1,5 +1,41 @@
 #!/bin/zsh
 
-test -n "$1" || exit 1
+# Check arguments
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [unmount] device"
+    exit 1
+fi
 
-sudo veracrypt -tc -t -k "" --protect-hidden no --fs-options utf8 /dev/$1 ~/mnt/$1
+ACTION="mount"
+if [ "$1" = "unmount" ]; then
+    ACTION="unmount"
+    DEVICE=$2
+else
+    DEVICE=$1
+fi
+MOUNT_POINT=$HOME/mnt/$DEVICE
+
+# Function to mount the device
+mount_device() {
+    sudo veracrypt -tc -t -k "" --protect-hidden no --fs-options utf8 /dev/$DEVICE $MOUNT_POINT
+}
+
+# Function to unmount the device
+unmount_device() {
+    sudo veracrypt -d /dev/$DEVICE
+}
+
+# Main logic
+case $ACTION in
+    mount)
+        mount_device
+        ;;
+    unmount)
+        unmount_device
+        ;;
+    *)
+        echo "Invalid action. Use 'mount' or 'unmount'."
+        exit 2
+        ;;
+esac
+
