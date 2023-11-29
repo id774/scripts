c9f6cb93ed0e93d9efcff95a196cb2c6222f0197
id774 <idnanashi@gmail.com>
2023-11-29 18:16:02 +0900

    2030 Enhance userlist.sh for macOS compatibility using dscacheutil, retaining Unix/Linux support

--- userlist.sh
+++ userlist.sh
@@ -6,12 +6,15 @@
 #  Description:
 #  This script lists user accounts with UIDs greater than a specified threshold.
 #  The threshold is set based on the existence of system-specific files,
-#  distinguishing between Debian-based and RedHat-based systems.
+#  distinguishing between Debian-based, RedHat-based, and macOS systems.
 #
 #  Author: id774
 #  Contact: idnanashi@gmail.com
 #
 #  Version History:
+#  v1.1 11/29,2023
+#       Improved script to support macOS by using 'dscacheutil' for user information retrieval.
+#       Maintained compatibility with Linux and other Unix-like systems.
 #  v1.0 11/16,2014
 #       Initial release. Detects system type and displays user accounts with
 #       UIDs above the system-specific threshold.
@@ -28,15 +31,31 @@
 THRESHOLD=0
 test -f /etc/debian_version && THRESHOLD=1000
 test -f /etc/redhat-release && THRESHOLD=500
+test "$(uname)" = "Darwin" && THRESHOLD=500
 
 show_userlist() {
-  while read LINE
-  do
-    if [ `echo $LINE | cut -f3 -d:` -ge $THRESHOLD ]
-    then
-      echo $LINE | cut -f1 -d:
-    fi
-  done < /etc/passwd
+  if [ "$(uname)" = "Darwin" ]; then
+    # macOS
+    dscacheutil -q user | awk -v thresh=$THRESHOLD '
+      /uid:/ { uid=$2 }
+      /name:/ { name=$2 }
+      /uid:/ && uid >= thresh { print name }
+    '
+  else
+    # Linux/Unix
+    while read LINE
+    do
+      USER_ID=$(echo $LINE | cut -d: -f3)
+      if [ -z "$USER_ID" ] || ! [ "$USER_ID" -eq "$USER_ID" ] 2> /dev/null; then
+        continue
+      fi
+      if [ $USER_ID -ge $THRESHOLD ]
+      then
+        echo $LINE | cut -d: -f1
+      fi
+    done < /etc/passwd
+  fi
 }
 
 test $THRESHOLD = 0 || show_userlist
+
