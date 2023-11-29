d0179d1350ac130535ce1e414227822a24059f64
id774 <idnanashi@gmail.com>
2023-11-29 18:03:25 +0900

    2029 Add headers with descriptions, usage, and version history to multiple scripts

--- userlist.sh
+++ userlist.sh
@@ -1,4 +1,29 @@
 #!/bin/sh
+#
+########################################################################
+# User List Display Script (userlist.sh)
+#
+#  Description:
+#  This script lists user accounts with UIDs greater than a specified threshold.
+#  The threshold is set based on the existence of system-specific files,
+#  distinguishing between Debian-based and RedHat-based systems.
+#
+#  Author: id774
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.0 11/16,2014
+#       Initial release. Detects system type and displays user accounts with
+#       UIDs above the system-specific threshold.
+#
+# Usage:
+#  Run the script without any arguments:
+#      userlist.sh
+#
+#  The script automatically detects the system type and sets the UID threshold
+#  accordingly. It then displays a list of user accounts with UIDs above this threshold.
+#
+########################################################################
 
 THRESHOLD=0
 test -f /etc/debian_version && THRESHOLD=1000
--- usershells.py
+++ usershells.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
+#
+########################################################################
+# User Shell Listing Script (usershells.py)
+#
+#  Description:
+#  This script lists user accounts and their corresponding shells from the /etc/passwd file,
+#  excluding accounts with non-interactive shells like 'false', 'nologin', 'sync', 'shutdown',
+#  and 'halt'.
+#
+#  Author: id774
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 8/30,2018
+#       Improved shell filtering to exclude non-interactive system accounts such as 'sync',
+#       'shutdown', and 'halt'.
+#  v1.0 2/14,2017
+#       Initial release. Lists user accounts and their shells, excluding 'false' and 'nologin'.
+#
+# Usage:
+#  Run the script without any arguments:
+#      python usershells.py
+#
+#  The script parses /etc/passwd and outputs a list of user accounts with interactive shells.
+#
+########################################################################
 
 passwd = "/etc/passwd"
 fo = open(passwd, 'r')
