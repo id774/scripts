0c2863d672d1f90e84d599df6508f4ff08dc8cbc
id774 <idnanashi@gmail.com>
2023-11-30 10:22:03 +0900

    2044 Add file type check for /etc/securetty in securetty.sh script

--- securetty.sh
+++ securetty.sh
@@ -1,3 +1,38 @@
 #!/bin/sh
+#
+########################################################################
+# securetty.sh: Script to Clear /etc/securetty for Unrestricted Root Access
+#
+#  Description:
+#  This script checks if /etc/securetty is a regular file and clears its content
+#  to enable root login from any TTY device. This might be necessary for certain
+#  system administration tasks or policy changes.
+#
+#  Warning:
+#  Using this script can decrease system security by allowing root access from any
+#  terminal. It should only be used when absolutely necessary and in secure environments.
+#  This script does nothing if /etc/securetty is a directory, which might be the case
+#  in some systems like macOS.
+#
+#  Author: id774
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 11/30,2023
+#       Added check to ensure /etc/securetty is a file before clearing it.
+#  v1.0 5/21,2012
+#       Initial release. Script to clear /etc/securetty for unrestricted root access.
+#
+#  Usage:
+#  Run the script without any arguments:
+#      ./securetty.sh
+#
+########################################################################
 
-sudo sh -c ": > /etc/securetty"
+if [ -f /etc/securetty ]; then
+    sudo sh -c ": > /etc/securetty"
+elif [ -d /etc/securetty ]; then
+    echo "/etc/securetty is a directory, no changes were made."
+else
+    echo "/etc/securetty does not exist as a file or directory."
+fi
