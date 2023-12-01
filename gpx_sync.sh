66630617568251238cf14aafc080f50e2cd93ef4
id774 <idnanashi@gmail.com>
2023-12-01 19:18:46 +0900

    2045 Improve handling of multiple GPX files with revised file copying and deletion logic

--- gpx_sync.sh
+++ gpx_sync.sh
@@ -1,28 +1,32 @@
+
 #!/bin/bash
 #
 ########################################################################
 # GPX File Management Script
 #
-#  Description:
-#  This script manages GPX files by performing operations like copying to
-#  specific directories, syncing with a remote server, and cleaning up
-#  temporary files. It's tailored for cycling or running data management.
+# Description:
+# This script manages GPX files by performing operations like copying to
+# specific directories, syncing with a remote server, and cleaning up
+# temporary files. It's tailored for cycling or running data management.
 #
-#  Author: id774
-#  Contact: idnanashi@gmail.com
+# Author: id774
+# Contact: idnanashi@gmail.com
 #
-#  Version History:
-#  v1.0 11/24,2023
-#       Initial release. Features include checking for GPX files, copying them
-#       to specific user and mounted directories, syncing with a remote server,
-#       and removing files from the temporary directory.
+# Version History:
+# v1.1 12/01,2023
+#     Improved handling of multiple GPX files with enhanced file copying
+#     and deletion functionality.
+# v1.0 11/24,2023
+#     Initial release. Features include checking for GPX files, copying them
+#     to specific user and mounted directories, syncing with a remote server,
+#     and removing files from the temporary directory.
 #
 # Usage:
-#  Run the script without any arguments. Ensure that the TMP_DIR and USER_GPX_DIR
-#  variables are set to the correct paths before running:
-#      bash gpx_sync.sh
+# Run the script without any arguments. Ensure that the TMP_DIR and USER_GPX_DIR
+# variables are set to the correct paths before running:
+#     ./gpx_sync.sh
 #
-#  The script will automatically handle the copying, syncing, and removal of GPX files.
+# The script will automatically handle the copying, syncing, and removal of GPX files.
 #
 ########################################################################
 
@@ -40,18 +44,18 @@ check_gpx_files() {
         echo "No GPX files found in $dir. Exiting."
         return 1
     fi
+    return 0
 }
 
 # Function to copy files to a directory, creating the directory if it does not exist
 copy_files() {
-    local source=$1
+    local source_dir=$1
     local destination=$2
     if [ ! -d "$destination" ]; then
-        echo "Destination directory $destination does not exist. Exiting."
-        return 1
+        mkdir -p "$destination"
     fi
-    echo cp "$source" "$destination"
-    cp "$source" "$destination"
+    echo "Copying files from $source_dir to $destination"
+    cp "$source_dir"/*.gpx "$destination"
 }
 
 # Function to perform rsync
@@ -65,19 +69,21 @@ sync_files() {
 
 # Function to remove files
 remove_files() {
-    local file_pattern=$1
-    rm -v $file_pattern
+    for file in "$@"; do
+        echo "Removing file: $file"
+        rm -v "$file"
+    done
 }
 
 # Main logic
 # Check if there are GPX files in the TMP_DIR
 check_gpx_files "$TMP_DIR" || exit 1
 
-# Copy .gpx files to user3 directory
-copy_files "$TMP_DIR"/*.gpx "$HOME/$USER_GPX_DIR/$CURRENT_YEAR/" || exit 1
+# Copy .gpx files to GPX directory
+copy_files "$TMP_DIR" "$HOME/$USER_GPX_DIR/$CURRENT_YEAR/" || exit 1
 
 # Copy .gpx files to mounted directory, skip if not mounted
-copy_files "$TMP_DIR"/*.gpx "$HOME/mnt/sdb/$USER_GPX_DIR/$CURRENT_YEAR/" || exit 1
+copy_files "$TMP_DIR" "$HOME/mnt/sdb/$USER_GPX_DIR/$CURRENT_YEAR/" || exit 1
 
 # Rsync files to the server
 sync_files "$HOME/$USER_GPX_DIR" "$RSYNC_USER" "$RSYNC_HOST"
