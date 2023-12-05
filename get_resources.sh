7338bbdbb35038b025cf1e11d61e9d67fc91f4fc
id774 <idnanashi@gmail.com>
2023-12-05 23:35:46 +0900

    2057 Refactor get_resources.sh for macOS compatibility and dynamic command checking

--- get_resources.sh
+++ get_resources.sh
@@ -1,103 +1,95 @@
 #!/bin/sh
 
-echo "[GNU/Linux version]"
-dmesg | grep "Linux version"
-which lsb_release > /dev/null && lsb_release -a
-echo
+########################################################################
+# get_resources.sh
+# System Resources Information Gathering Script
+#
+#  Description:
+#  This script gathers and displays various system resources and logs.
+#  It adapts to the running environment (Linux or macOS) and uses
+#  alternative commands or options when necessary.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-05
+#       Refactored for macOS compatibility and command availability checks.
+#  v1.0 2008-08-22
+#       Initial release. Gathers system resources and log information.
+#
+#  Usage:
+#  ./get_resources.sh
+#
+########################################################################
 
-echo "[uname -a]"
-uname -a
-echo
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
 
-echo "[uptime]"
-uptime
-echo
+# Function to display a command's output if the command exists
+execute_command() {
+    if command_exists "$1"; then
+        echo "[$1]"
+        "$@"
+        echo
+    fi
+}
 
-echo "[cpuinfo]"
-cat /proc/cpuinfo
-echo
+# Function to display log file contents if the file exists
+display_log() {
+    if [ -f "$1" ]; then
+        echo "[Contents of $1]"
+        cat "$1"
+        echo
+    fi
+}
 
-echo "[meminfo]"
-cat /proc/meminfo
-echo
+# System Information
+execute_command dmesg | grep "Linux version"
+execute_command lsb_release -a
 
-echo "[free -t]"
-free -t
-echo
+execute_command uname -a
+execute_command uptime
 
-echo "[vmstat -n]"
-vmstat -n
-echo
+# Conditional commands for macOS and Linux
+if [ "$(uname)" = "Darwin" ]; then
+    # macOS specific commands
+    execute_command sysctl -n machdep.cpu.brand_string
+    execute_command sysctl vm.swapusage
+    execute_command vm_stat
+    execute_command df -H
+    execute_command top -l 1
+    execute_command ps aux
+else
+    # Linux specific commands
+    execute_command cat /proc/cpuinfo
+    execute_command cat /proc/meminfo
+    execute_command free -t
+    execute_command vmstat -n
+    execute_command df -P -T
+    execute_command top -b -n 1
+    execute_command ps -H auxZwww
+    execute_command ps axl --sort -vsize | head -20
+fi
 
-echo "[df -P -T]"
-df -P -T
-echo
+# Network and process information
+execute_command ip addr show
+execute_command lsof -i
+execute_command ss -t
+execute_command netstat -t
+execute_command netstat -s
+execute_command netstat -an
+execute_command w
+execute_command lsmod
+execute_command netstat -tan | grep ':80 ' | awk '{print $6}' | sort | uniq -c
+execute_command ntpq -pn
 
-echo "[ip addr show]"
-ip addr show
-echo
-
-echo "[lsof -i]"
-lsof -i
-echo
-
-echo "[ss -t]"
-ss -t
-echo
-
-echo "[netstat -t]"
-netstat -t
-echo
-
-echo "[netstat -s]"
-netstat -s
-echo
-
-echo "[netstat -an]"
-netstat -an
-echo
-
-echo "[w]"
-w
-echo
-
-echo "[top -b -n 1]"
-top -b -n 1
-echo
-
-echo "[lsmod]"
-lsmod
-echo
-
-echo "[ps -H auxZwww]"
-ps -H auxZwww
-echo
-
-echo "[ps axl --sort -vsize | head -20]"
-ps axl --sort -vsize | head -20
-echo
-
-echo "[netstat -tan | grep ':80 ' | awk '{print $6}' | sort | uniq -c]"
-netstat -tan | grep ':80 ' | awk '{print $6}' | sort | uniq -c
-echo
-
-echo "[ntpq -pn]"
-ntpq -pn
-echo
-
-echo "[grep Accepted /var/log/auth.log]"
-grep "Accepted" /var/log/auth.log
-echo
-
-echo "[grep attack /var/log/messages]"
-grep " attack" /var/log/messages
-echo
-
-echo "[egrep '(Fail|refuse)' /var/log/auth.log]"
-egrep '(Fail|refuse)' /var/log/auth.log
-echo
-
-echo "[grep WARNING /var/log/fail2ban.log]"
-grep "WARNING" /var/log/fail2ban.log
-echo
+# Log files
+display_log "/var/log/auth.log"
+display_log "/var/log/messages"
+display_log "/var/log/fail2ban.log"
 
