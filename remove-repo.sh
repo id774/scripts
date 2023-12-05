e0c56637d670f026b5dff6b78d7a6fceda7f240b
id774 <idnanashi@gmail.com>
2023-12-05 22:59:19 +0900

    2053 Refactor remove-repo.sh for improved readability and error handling

--- remove-repo.sh
+++ remove-repo.sh
@@ -1,17 +1,57 @@
 #!/bin/sh
 
+########################################################################
+# remove-repo.sh: Git Repository Removal Script
+#
+#  Description:
+#  This script removes specified Git repositories from the user's local
+#  directories ($HOME/local/github and $HOME/local/git) and any associated
+#  symbolic links in the home directory.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-05
+#       Refactored script with additional comments and error checking.
+#  v1.0 2018-04-20
+#       Initial release. Removes Git repositories and their symbolic links.
+#
+#  Usage:
+#  ./remove-repo.sh [repository_name...]
+#
+########################################################################
+
 remove_repo() {
-    test -d $HOME/local/github/$1 && rm -rf $HOME/local/github/$1
-    test -d $HOME/local/git/$1 && rm -rf $HOME/local/git/$1
-    test -L $HOME/$1 && rm -vf $HOME/$1
+    if [ -d "$HOME/local/github/$1" ]; then
+        echo "Removing repository: $HOME/local/github/$1"
+        rm -rf "$HOME/local/github/$1"
+    fi
+
+    if [ -d "$HOME/local/git/$1" ]; then
+        echo "Removing repository: $HOME/local/git/$1"
+        rm -rf "$HOME/local/git/$1"
+    fi
+
+    if [ -L "$HOME/$1" ]; then
+        echo "Removing symlink: $HOME/$1"
+        rm -vf "$HOME/$1"
+    fi
 }
 
 remove_repos() {
     while [ $# -gt 0 ]
     do
-        remove_repo $1
+        remove_repo "$1"
         shift
     done
 }
 
-test -n "$1" && remove_repos $*
+if [ -n "$1" ]; then
+    remove_repos "$@"
+else
+    echo "Usage: $0 [repository_name...]"
+fi
+
