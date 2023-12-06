ce1201cc3cfd22346fd702a10d2c4d7bb690e794
id774 <idnanashi@gmail.com>
2023-12-06 15:46:27 +0900

    2078 Update Instagram photo download scripts for enhanced functionality and documentation

--- download_instagram_batch.py
+++ download_instagram_batch.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+########################################################################
+# download_instagram_batch.py: Batch Download Photos from Instagram
+#
+#  Description:
+#  This script batch downloads photos from a specified Instagram account.
+#  It uses the instaloader library to fetch all photo URLs from the account
+#  and downloads them locally.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-06
+#       Refactored for clarity, added English comments, and updated documentation.
+#  v1.0 2023-02-26
+#       Initial release.
+#
+#  Usage:
+#  python download_instagram_batch.py <Instagram username>
+#  Example: python download_instagram_batch.py username
+#
+########################################################################
+
 import instaloader
 import time
 import urllib.request
@@ -9,17 +34,20 @@ import re
 
 class InstagramPhotoDownloader:
     def __init__(self, username):
+        # Initialize the downloader with the given username
         self.username = username
         self.L = instaloader.Instaloader()
         self.profile = instaloader.Profile.from_username(self.L.context, self.username)
 
     def download(self):
+        # Retrieve the URLs of all Instagram photos
         urls = self._get_instagram_photo_urls()
         post_count = len(urls)
         print(f'This account has {post_count} image posts.')
         minutes = int(post_count/60)+1
-        print(f'Estimate processing time is {minutes} minutes.')
+        print(f'Estimate processing time is about {minutes} minutes.')
 
+        # Determine the starting index based on existing files
         max_number = self._get_max_number('.')
 
         for i, url in enumerate(reversed(urls)):
@@ -34,6 +62,7 @@ class InstagramPhotoDownloader:
                 time.sleep(1)
 
     def _get_max_number(self, directory_path):
+        # Determine the highest numbered file in the directory
         max_number = 0
         for filename in os.listdir(directory_path):
             basename = os.path.splitext(filename)[0]
@@ -45,9 +74,11 @@ class InstagramPhotoDownloader:
         return max_number
 
     def _download_image(self, url, filename):
+        # Download an image from the given URL
         urllib.request.urlretrieve(url, filename)
 
     def _get_instagram_photo_urls(self):
+        # Fetch URLs of all photos from the Instagram profile
         urls = []
         for post in self.profile.get_posts():
             for image_url in post.get_sidecar_nodes():
@@ -58,7 +89,7 @@ class InstagramPhotoDownloader:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('username', help='Instagram アカウントのユーザー名')
+    parser.add_argument('username', help='Instagram username')
     args = parser.parse_args()
 
     downloader = InstagramPhotoDownloader(args.username)
--- download_instagram_single.py
+++ download_instagram_single.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python
 
+########################################################################
+# download_instagram_single.py: Download Single Image from Instagram
+#
+#  Description:
+#  This script downloads a single image from a given Instagram URL.
+#  It uses BeautifulSoup to parse the page and extract the image URL.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-06
+#       Refactored for clarity, added English comments, and updated documentation.
+#  v1.0 2023-02-26
+#       Initial release.
+#
+#  Usage:
+#  python download_instagram_single.py <Instagram URL> <filename>
+#  Example: python download_instagram_single.py https://www.instagram.com/p/xxxx image.jpg
+#
+########################################################################
+
 import requests
 from bs4 import BeautifulSoup
 import argparse
 
 def download_image(url, filename):
-    # Instagramのページを取得
+    # Fetch the Instagram page
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 画像のURLを取得
+    # Retrieve the image URL
     image_url = soup.find('meta', {'property': 'og:image'})['content']
 
-    # 画像をダウンロード
+    # Download the image
     response = requests.get(image_url)
     with open(filename, 'wb') as f:
         f.write(response.content)
 
-    print(f'{filename} をダウンロードしました。')
+    print(f'Downloaded {filename}.')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='Instagram の URL')
-    parser.add_argument('filename', help='保存するファイル名')
+    parser.add_argument('url', help='Instagram URL')
+    parser.add_argument('filename', help='Name of the file to save')
     args = parser.parse_args()
 
-    url = args.url
-    filename = args.filename
-    download_image(url, filename)
+    download_image(args.url, args.filename)
 
