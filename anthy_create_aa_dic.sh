99d6b9ac16e3a3fc88b1aa965e8d518e1ba4f9fc
id774 <idnanashi@gmail.com>
2023-12-06 15:53:04 +0900

    2079 Enhance dictionary scripts for Anthy and MS-IME conversion with improved checks and documentation

--- anthy_create_aa_dic.sh
+++ anthy_create_aa_dic.sh
@@ -1,8 +1,35 @@
 #!/bin/sh
 
-test -f $SCRIPTS/convert_msime2cannna.rb || exit 1
-test -f $SCRIPTS/etc/aa.txt || exit 1
+########################################################################
+# anthy_create_aa_dic.sh: Create Anthy Dictionary for ASCII Art
+#
+#  Description:
+#  This script creates a custom dictionary for Anthy, a Japanese input
+#  method, using ASCII art (AA) entries. It converts an MS-IME format
+#  dictionary to the Canna format using the convert_msime2cannna.rb script.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-06
+#       Refactored for improved readability and added detailed comments.
+#  v1.0 2009-11-17
+#       Initial release.
+#
+#  Usage:
+#  ./anthy_create_aa_dic.sh
+#
+########################################################################
+
+# Ensure necessary files and directories exist
+test -f $SCRIPTS/convert_msime2cannna.rb || { echo "Missing convert_msime2cannna.rb"; exit 1; }
+test -f $SCRIPTS/etc/aa.txt || { echo "Missing aa.txt"; exit 1; }
 test -d $HOME/.anthy || mkdir $HOME/.anthy
 
+# Create Anthy dictionary for ASCII Art
 ruby -Ku $SCRIPTS/convert_msime2cannna.rb < $SCRIPTS/etc/aa.txt > $HOME/.anthy/private_words_default
 echo "Dictionary created."
+
--- convert_msime2cannna.rb
+++ convert_msime2cannna.rb
@@ -1,18 +1,42 @@
 #!/usr/bin/env ruby
-$:.unshift File.join(ENV['SCRIPTS'], 'lib') unless ENV['SCRIPTS'] == nil
-$:.unshift File.join(File.dirname(__FILE__), 'lib')
+
+########################################################################
+# convert_msime2canna.rb: Convert MS-IME Dictionary to Canna Format
+#
+#  Description:
+#  This script converts a Microsoft IME dictionary file to a format
+#  compatible with the Canna Japanese input method system. It specifically
+#  targets entries labeled as "顔文字" (emoji) and formats them accordingly.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-06
+#       Refactored for improved readability and added detailed comments.
+#  v1.0 2009-11-16
+#       Initial release.
+#
+#  Usage:
+#  ruby convert_msime2canna.rb < MS-IME_dictionary_file
+#
+########################################################################
 
 require 'kconv'
 
 while line = gets
-
+  # Split the line into components
   str = line.split(/\t/)
 
+  # Skip lines without a third column or not labeled as "顔文字"
   next unless str[2]
   next unless str[2].toutf8.chop == "顔文字"
 
-  print str[0].toutf8
-  print " #KJ "
-  puts str[1].toutf8.gsub(/ /, "\\ ")
-
+  # Output the converted line in Canna format
+  print str[0].toutf8  # Original word
+  print " #KJ "       # Canna keyword
+  puts str[1].toutf8.gsub(/ /, "\\ ")  # Converted reading with escaped spaces
 end
+
