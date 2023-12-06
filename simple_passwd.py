4a12edca757779c7e38a3bd36b8ef4a87227ff41
id774 <idnanashi@gmail.com>
2023-12-06 22:27:37 +0900

    2089 Refactor and document simple_password.py for password generation

--- simple_passwd.py
+++ simple_passwd.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 
+########################################################################
+# simple_password.py: Simple Password Generator
+#
+#  Description:
+#  This script generates a simple random password. It includes letters,
+#  digits, and some special characters.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.0 2013-01-07
+#       Initial release.
+#
+#  Usage:
+#  simple_password.py [length]
+#  Default length is 8 if not specified.
+#
+########################################################################
+
 import sys
 import string
 from random import choice
 
-def generate_passwd(length):
-    print("".join([choice(string.ascii_letters + string.digits + '_' + '-' + '!' + '#' + '&')
-                   for i in range(int(length))]))
+def generate_passwd(length=8):
+    """Generate a random password of specified length."""
+    chars = string.ascii_letters + string.digits + '_-!#&'
+    print("".join([choice(chars) for i in range(int(length))]))
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        generate_passwd(8)
-    else:
-        generate_passwd(sys.argv[1])
+    length = sys.argv[1] if len(sys.argv) == 2 else 8
+    generate_passwd(length)
+
