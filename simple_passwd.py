b1ce2fb9cf9bac7b96c7b9d423bf0bf151390451
id774 <idnanashi@gmail.com>
2023-12-06 22:51:26 +0900

    2091 Update simple_password.py and simple_password.rb to include argument validation and consistent behavior

--- simple_passwd.py
+++ simple_passwd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 ########################################################################
-# simple_password.py: Simple Password Generator
+# simple_password.py: Simple Password Generator in Python
 #
 #  Description:
-#  This script generates a simple random password. It includes letters,
-#  digits, and some special characters.
+#  This script generates a random password. It can optionally exclude
+#  special symbols from the password.
 #
 #  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
@@ -13,25 +13,40 @@
 #  Contact: idnanashi@gmail.com
 #
 #  Version History:
+#  v1.1 2023-12-06
+#       Added option to exclude special symbols and improved argument handling.
 #  v1.0 2013-01-07
 #       Initial release.
 #
 #  Usage:
-#  simple_password.py [length]
-#  Default length is 8 if not specified.
+#  python simple_password.py [options] length
+#  Options:
+#    -s, --no-symbols: Do not include symbols in the password
 #
 ########################################################################
 
 import sys
 import string
 from random import choice
+from optparse import OptionParser
 
-def generate_passwd(length=8):
+def generate_passwd(length, use_symbols=True):
     """Generate a random password of specified length."""
-    chars = string.ascii_letters + string.digits + '_-!#&'
-    print("".join([choice(chars) for i in range(int(length))]))
+    chars = string.ascii_letters + string.digits
+    if use_symbols:
+        chars += '_-!#&'
+    print("".join([choice(chars) for i in range(length)]))
 
 if __name__ == '__main__':
-    length = sys.argv[1] if len(sys.argv) == 2 else 8
-    generate_passwd(length)
+    parser = OptionParser(usage="usage: %prog [options] length")
+    parser.add_option("-s", "--no-symbols", action="store_false", dest="use_symbols", default=True,
+                      help="Do not include symbols in the password")
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1 or not args[0].isdigit():
+        parser.print_help()
+        sys.exit("Error: Length must be a number.")
+
+    length = int(args[0])
+    generate_passwd(length, options.use_symbols)
 
--- simple_passwd.rb
+++ simple_passwd.rb
@@ -4,9 +4,8 @@
 # simple_password.rb: Simple Password Generator in Ruby
 #
 #  Description:
-#  This script generates a random password of a specified length.
-#  It includes a mix of numbers, lowercase and uppercase letters,
-#  and special characters (_, -, !, #, &).
+#  This script generates a random password. It can optionally exclude
+#  special symbols from the password.
 #
 #  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
@@ -14,23 +13,42 @@
 #  Contact: idnanashi@gmail.com
 #
 #  Version History:
+#  v1.1 2023-12-06
+#       Added option to exclude special symbols and improved argument handling.
 #  v1.0 2013-01-07
 #       Initial release.
 #
 #  Usage:
-#  ruby simple_password.rb [length]
-#  Default length is 8 if not specified.
+#  ruby simple_password.rb [options] length
+#  Options:
+#    -s, --no-symbols: Do not include symbols in the password
 #
 ########################################################################
 
-def generate_passwd(length)
-  # Generate a random password
-  chars = [*0..9, *'a'..'z', *'A'..'Z', '_', '-', '!', '#', '&']
+require 'optparse'
+
+def generate_passwd(length, use_symbols=true)
+  chars = [*'0'..'9', *'a'..'z', *'A'..'Z']
+  chars += ['_', '-', '!', '#', '&'] if use_symbols
   puts chars.sample(length).join
 end
 
-if __FILE__ == $0
-  length = ARGV.shift || 8
-  generate_passwd(length.to_i)
+options = { use_symbols: true }
+option_parser = OptionParser.new do |opts|
+  opts.banner = "Usage: simple_password.rb [options] length"
+  opts.on("-s", "--no-symbols", "Do not include symbols in the password") do
+    options[:use_symbols] = false
+  end
 end
+
+option_parser.parse!
+
+if ARGV.length != 1 || !/\A\d+\z/.match?(ARGV[0])
+  puts "Error: Length must be a number."
+  puts option_parser
+  exit
+end
+
+length = ARGV.shift.to_i
+generate_passwd(length, options[:use_symbols])
 
