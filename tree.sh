e6bbafb9da964803de7bdc02cc6c22d7ee701145
id774 <idnanashi@gmail.com>
2023-12-06 22:58:27 +0900

    2093 Add documentation header to tree.sh for displaying directory tree

--- tree.sh
+++ tree.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-pwd;find .|sort|sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
+########################################################################
+# tree.sh: Display Directory Tree
+#
+#  Description:
+#  This script displays the directory tree of the current directory.
+#  It uses 'find', 'sort', and 'sed' commands to format the tree structure.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.0 2014-10-22
+#       Initial release.
+#
+#  Usage:
+#  ./tree.sh
+#  Run the script in the directory you want to display the tree for.
+#
+########################################################################
+
+# Display the current directory
+pwd
+
+# Generate and display the directory tree
+find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
+
