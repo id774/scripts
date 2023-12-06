6eaf03defb55fe99d9d286f6590d4299e882e80e
id774 <idnanashi@gmail.com>
2023-12-06 16:04:08 +0900

    2081 Refactor exif_drop.sh for enhanced clarity and safety checks; remove copydir.py

--- exif_drop.sh
+++ exif_drop.sh
@@ -1,16 +1,43 @@
 #!/bin/sh
 
+########################################################################
+# exif_drop.sh: Remove EXIF Data from Images with GPS Information
+#
+#  Description:
+#  This script searches for image files with GPS information in their
+#  EXIF data and removes this data using jhead for privacy reasons.
+#
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.1 2023-12-06
+#       Refactored for improved readability and added detailed comments.
+#  v1.0 2009-10-05
+#       Initial release.
+#
+#  Usage:
+#  ./exif_drop.sh <directory_path>
+#
+########################################################################
+
 FIND=/usr/bin/find
 EXIFTOOL=/usr/bin/exiftool
 JHEAD=/usr/bin/jhead
 PATH=$1
 
+# Ensure the required tools are installed
+[ -x "$EXIFTOOL" ] || { echo "exiftool not found"; exit 1; }
+[ -x "$JHEAD" ] || { echo "jhead not found"; exit 1; }
+
+# Process image files with GPS information
 $FIND $PATH -type f | while read FILE
 do
-  RESULT=`$EXIFTOOL -gps:GPSLatitude $FILE`
-  if test -n "$RESULT"
-  then
-    $JHEAD -purejpg $FILE
-    echo $FILE
+  if [ -n "$($EXIFTOOL -gps:GPSLatitude "$FILE")" ]; then
+    $JHEAD -purejpg "$FILE"
+    echo "Processed $FILE"
   fi
 done
+
