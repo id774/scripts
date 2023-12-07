fb49d8fbe6a02aedf8a69a5f215c5a0af3a23b8a
id774 <idnanashi@gmail.com>
2023-12-07 21:50:33 +0900

    2110 Add verification to ensure directories are Git repositories before removal in remove-repo.sh

--- remove-repo.sh
+++ remove-repo.sh
@@ -6,7 +6,8 @@
 #  Description:
 #  This script removes specified Git repositories from the user's local
 #  directories ($HOME/local/github and $HOME/local/git) and any associated
-#  symbolic links in the home directory.
+#  symbolic links in the home directory. It checks if the directory is a Git
+#  repository before removing it.
 #
 #  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
@@ -14,6 +15,8 @@
 #  Contact: idnanashi@gmail.com
 #
 #  Version History:
+#  v1.2 2023-12-07
+#       Added check to verify if directory is a Git repository.
 #  v1.1 2023-12-05
 #       Refactored script with additional comments and error checking.
 #  v1.0 2018-04-20
@@ -24,20 +27,36 @@
 #
 ########################################################################
 
+is_git_repo() {
+    if [ -d "$1/.git" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 remove_repo() {
-    if [ -d "$HOME/local/github/$1" ]; then
-        echo "Removing repository: $HOME/local/github/$1"
-        rm -rf "$HOME/local/github/$1"
+    local repo_path_github="$HOME/local/github/$1"
+    local repo_path_git="$HOME/local/git/$1"
+    local symlink_path="$HOME/$1"
+
+    if is_git_repo "$repo_path_github"; then
+        echo "Removing Git repository: $repo_path_github"
+        rm -rf "$repo_path_github"
+    else
+        echo "Skipping: $repo_path_github is not a Git repository."
     fi
 
-    if [ -d "$HOME/local/git/$1" ]; then
-        echo "Removing repository: $HOME/local/git/$1"
-        rm -rf "$HOME/local/git/$1"
+    if is_git_repo "$repo_path_git"; then
+        echo "Removing Git repository: $repo_path_git"
+        rm -rf "$repo_path_git"
+    else
+        echo "Skipping: $repo_path_git is not a Git repository."
     fi
 
-    if [ -L "$HOME/$1" ]; then
-        echo "Removing symlink: $HOME/$1"
-        rm -vf "$HOME/$1"
+    if [ -L "$symlink_path" ]; then
+        echo "Removing symlink: $symlink_path"
+        rm -vf "$symlink_path"
     fi
 }
 
