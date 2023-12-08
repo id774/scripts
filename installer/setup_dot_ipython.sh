75a267c65694c37ffb74a3a1e243023ed1197257
id774 <idnanashi@gmail.com>
2023-12-08 18:53:30 +0900

    2132 Fixed permissions of 00-init.py in startup of dot ipython

--- installer/setup_dot_ipython.sh
+++ installer/setup_dot_ipython.sh
@@ -17,6 +17,7 @@ init_nbserver() {
     cd
     ipython profile create nbserver
     test -d $HOME/.ipython/profile_nbserver/startup && cp $OPTIONS $SCRIPTS/dot_files/dot_ipython/profile_default/startup/00-init.py $HOME/.ipython/profile_nbserver/startup/
+    test -f $HOME/.ipython/profile_nbserver/startup/00-init.py && chmod -x $HOME/.ipython/profile_nbserver/startup/00-init.py
 }
 
 copy_dotipython() {
@@ -25,6 +26,7 @@ copy_dotipython() {
     ipython profile create default
     test -d $HOME/.ipython/profile_default/startup || mkdir -p $HOME/.ipython/profile_default/startup
     cp $OPTIONS $SCRIPTS/dot_files/dot_ipython/profile_default/startup/00-init.py $HOME/.ipython/profile_default/startup/
+    test -f $HOME/.ipython/profile_default/startup/00-init.py && chmod -x $HOME/.ipython/profile_default/startup/00-init.py
     cp $OPTIONS $SCRIPTS/dot_files/dot_zshrc_local $HOME/.zshrc_local
 }
 
