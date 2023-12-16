fd4828b6232a4cdd8eee1e1af00649ba267c8a35
id774 <idnanashi@gmail.com>
2023-12-17 02:14:20 +0900

    2192 Added compatibility checks for subprocess.DEVNULL and async/await in find_fstrings.sh

--- find_fstrings.sh
+++ find_fstrings.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ########################################################################
-# find_fstrings.sh: Find f-strings in Python files
+# find_python_compat_issues.sh: Find Python compatibility issues in Python files
 #
 # Description:
-# This script searches for f-strings in Python files within a specified directory.
+# This script searches for Python compatibility issues such as f-strings,
+# subprocess.DEVNULL usage, and usage of async/await keywords in Python files
+# within a specified directory.
 #
 # Author: id774 (More info: http://id774.net)
 # Source Code: https://github.com/id774/scripts
@@ -12,16 +14,20 @@
 # Contact: idnanashi@gmail.com
 #
 # Version History:
+#  v1.1 2023-12-17
+#       Added search for async/await keyword usage for Python compatibility issues.
+#       Extended the script to search for subprocess.DEVNULL usage for Python compatibility issues.
 #  v1.0 2023-12-08
 #       Initial release. Search for f-strings in Python files.
 #
 # Usage:
-# ./find_fstrings.sh [directory]
+# ./find_python_compat_issues.sh [directory]
 # If no directory is specified, it searches in the current directory.
 #
 # Note:
 # - This script requires grep to be installed on the system.
-# - The script uses regular expression to find lines that might contain f-strings.
+# - The script uses regular expressions to find lines that might contain
+#   compatibility issues.
 #
 ########################################################################
 
@@ -29,5 +35,14 @@
 TARGET_DIR=${1:-.}
 
 # Search for f-strings in Python files
+echo "Searching for f-strings in Python files..."
 grep -n -r --include="*.py" "f['\"]" "$TARGET_DIR"
+
+# Search for subprocess.DEVNULL usage in Python files
+echo "Searching for subprocess.DEVNULL usage in Python files..."
+grep -n -r --include="*.py" "subprocess.DEVNULL" "$TARGET_DIR"
+
+# Search for async and await keyword usage in Python files
+echo "Searching for async and await keyword usage in Python files..."
+grep -n -r --include="*.py" -E "\basync\b|\bawait\b" "$TARGET_DIR"
 
