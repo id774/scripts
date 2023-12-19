fcad6a14d66ab0be1e5718459570e38462dcd7ad
id774 <idnanashi@gmail.com>
2023-12-19 23:20:02 +0900

    2203 Extend dirsize.sh to support up to QB and refactor for code efficiency

--- dirsize.sh
+++ dirsize.sh
@@ -6,11 +6,12 @@
 #  Description:
 #  This script displays a detailed list of files and directories in the
 #  current directory, including hidden files, and calculates the total
-#  size of these files in human-readable format (KB, MB, GB, TB, PB).
-#  It's useful for quickly estimating the size of contents in a directory
-#  without including subdirectories. The script does not recursively
-#  account for sizes within subdirectories and is designed for simplicity,
-#  providing a clear overview of the immediate directory contents.
+#  size of these files in human-readable format (KB, MB, GB, TB, PB, EB,
+#  ZB, YB, RB, QB). It's useful for quickly estimating the size of contents
+#  in a directory without including subdirectories. The script does not
+#  recursively account for sizes within subdirectories and is designed
+#  for simplicity, providing a clear overview of the immediate directory
+#  contents. Now updated to support file size display up to Quettabytes (QB).
 #
 #  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
@@ -19,8 +20,9 @@
 #
 #  Version History:
 #  v1.1 2023-12-19
-#       Updated to display file sizes in human-readable format (KB, MB, GB,
-#       TB, PB). Enhanced readability for larger data sets.
+#       Updated to display file sizes in human-readable format up to QB,
+#       including EB, ZB, YB, RB. Refactored code for improved readability
+#       and maintainability.
 #  v1.0 2009-07-07
 #       Initial release. Provides a summary of file sizes in the current
 #       directory, including hidden files, but does not include sizes
@@ -33,27 +35,13 @@ ls -all | awk '{
     x += $5
 }
 END {
-    suffix = "B";
-    if (x >= 1024) {
+    suffixes = "KB MB GB TB PB EB ZB YB RB QB";
+    split(suffixes, units, " ");
+    for (i = 1; x >= 1024 && i <= length(units); i++) {
         x /= 1024;
-        suffix = "KB";
+        suffix = units[i];
     }
-    if (x >= 1024) {
-        x /= 1024;
-        suffix = "MB";
-    }
-    if (x >= 1024) {
-        x /= 1024;
-        suffix = "GB";
-    }
-    if (x >= 1024) {
-        x /= 1024;
-        suffix = "TB";
-    }
-    if (x >= 1024) {
-        x /= 1024;
-        suffix = "PB";
-    }
+    if (!suffix) suffix = "B";
     printf "Total: %.2f %s\n", x, suffix
 }'
 
