282eb4c9e07546a9631eff351f3fc98954dff717
id774 <idnanashi@gmail.com>
2023-12-19 15:04:46 +0900

    2201 Add human-readable file size display (KB, MB, GB, TB, PB) to dirsize.sh

--- dirsize.sh
+++ dirsize.sh
@@ -6,11 +6,11 @@
 #  Description:
 #  This script displays a detailed list of files and directories in the
 #  current directory, including hidden files, and calculates the total
-#  size of these files. It's useful for quickly estimating the size of
-#  contents in a directory without including subdirectories. However,
-#  it does not recursively account for the sizes within subdirectories
-#  and may be seen as simplistic or limited for more complex directory
-#  structures.
+#  size of these files in human-readable format (KB, MB, GB, TB, PB).
+#  It's useful for quickly estimating the size of contents in a directory
+#  without including subdirectories. The script does not recursively
+#  account for sizes within subdirectories and is designed for simplicity,
+#  providing a clear overview of the immediate directory contents.
 #
 #  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
@@ -18,6 +18,9 @@
 #  Contact: idnanashi@gmail.com
 #
 #  Version History:
+#  v1.1 2023-12-19
+#       Updated to display file sizes in human-readable format (KB, MB, GB,
+#       TB, PB). Enhanced readability for larger data sets.
 #  v1.0 2009-07-07
 #       Initial release. Provides a summary of file sizes in the current
 #       directory, including hidden files, but does not include sizes
@@ -25,4 +28,32 @@
 #
 ########################################################################
 
-ls -all | awk '{ print $0; x += $5 } END{ print "Total: " x }'
+ls -all | awk '{
+    print $0;
+    x += $5
+}
+END {
+    suffix = "B";
+    if (x >= 1024) {
+        x /= 1024;
+        suffix = "KB";
+    }
+    if (x >= 1024) {
+        x /= 1024;
+        suffix = "MB";
+    }
+    if (x >= 1024) {
+        x /= 1024;
+        suffix = "GB";
+    }
+    if (x >= 1024) {
+        x /= 1024;
+        suffix = "TB";
+    }
+    if (x >= 1024) {
+        x /= 1024;
+        suffix = "PB";
+    }
+    printf "Total: %.2f %s\n", x, suffix
+}'
+
