91f803b4a8540dce199dd4db82b9713af17edd9f
id774 <idnanashi@gmail.com>
2023-12-20 14:13:10 +0900

    2212 Added comprehensive documentation to the IPython setup script

--- installer/setup_dot_ipython.sh
+++ installer/setup_dot_ipython.sh
@@ -1,5 +1,46 @@
 #!/bin/sh
 
+########################################################################
+# setup_ipython.sh: IPython Setup Script
+#
+# Description:
+# This script automates the setup and configuration of the IPython environment.
+# It creates IPython profiles, copies necessary startup files, and sets appropriate
+# permissions.
+#
+# Author: id774 (More info: http://id774.net)
+# Source Code: https://github.com/id774/scripts
+# License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+# Contact: idnanashi@gmail.com
+#
+# Version History:
+#  v1.1 2023-12-20
+#       Refactored script for readability and added documentation.
+#  v1.0 2014-08-16
+#       Initial release for automating IPython setup.
+#
+# Usage:
+# Run this script to initialize and configure your IPython environment.
+# This script sets up a default IPython profile and an additional 'nbserver' profile.
+# It also copies necessary startup files from a predefined 'SCRIPTS' directory.
+# Before running this script, ensure that the 'SCRIPTS' environment variable points
+# to your directory containing the IPython startup files.
+#
+# Note:
+# - This script is intended to be used with Zsh.
+# - Make sure to back up any existing IPython configuration before running this script.
+# - Ensure that the IPython is installed on your system.
+# - 'SCRIPTS' environment variable must be correctly set.
+#
+########################################################################
+
+# Check if SCRIPTS variable is set
+if [ -z "$SCRIPTS" ]; then
+    echo "Error: SCRIPTS environment variable is not set."
+    echo "Please set the SCRIPTS variable to the path of your IPython startup files."
+    exit 1
+fi
+
 setup_environment() {
     SCRIPTS=$HOME/scripts
 
