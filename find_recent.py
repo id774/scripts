b501006d6ac18aec7f3d279716a6d411ba2319aa
id774 <idnanashi@gmail.com>
2024-03-03 14:56:07 +0900

    2418 Add option to list filenames only in find_recent script

--- find_recent.py
+++ find_recent.py
@@ -8,6 +8,7 @@
 #  directory by default) and its subdirectories that have been modified after
 #  a given date and time. It displays the modification time of each file.
 #  Hidden directories are ignored by default unless the '-a' option is used.
+#  The '-f' option can be used to list filenames only, without path or modification time.
 #
 #  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
@@ -15,6 +16,8 @@
 #  Contact: idnanashi@gmail.com
 #
 #  Version History:
+#  v1.1 2024-03-03
+#       Added '-f' option to list filenames only.
 #  v1.0 2024-02-25
 #       Initial release. Added functionality to list files based on modification date,
 #       displaying their modification time, and ignoring hidden directories by default.
@@ -22,17 +25,19 @@
 #  Usage:
 #  Run this script with the '-d' option followed by the date in YYYY-MM-DD format
 #  and optionally time in HH:MM format. You can also specify a directory path with '-p'.
-#  Use the '-a' option to include hidden directories.
+#  Use the '-a' option to include hidden directories. The '-f' option lists filenames only.
 #  Examples:
 #     ./find_recent.py -d "2024-01-01"
 #     ./find_recent.py -d "2024-01-01 13:00"
 #     ./find_recent.py -d "2024-01-01" -p "/path/to/directory"
 #     ./find_recent.py -d "2024-01-01 13:00" -a -p "/path/to/directory"
+#     ./find_recent.py -d "2024-03-02" -f
 #
 #  Notes:
 #  - This script is compatible with Python 3.2 and later versions.
 #  - If the time is not specified, it defaults to 00:00 (midnight) of the given date.
 #  - Hidden directories are ignored by default. Use the '-a' option to include them.
+#  - The '-f' option lists filenames only, without path or modification time.
 #
 #  Error Conditions and Return Codes:
 #  0: Success
@@ -57,6 +62,7 @@ def parse_arguments():
         datetime_str: A string representing the date and optional time in ISO format.
         path: The directory path to search. Defaults to current directory if not specified.
         include_hidden: Boolean flag indicating whether to include hidden directories in the search.
+        filenames_only: Boolean flag indicating whether to list filenames only, without path or modification time.
     """
     parser = argparse.ArgumentParser(description='List files updated after a specified datetime.')
 
@@ -69,6 +75,9 @@ def parse_arguments():
     # Add argument to include hidden directories in the search
     parser.add_argument('-a', '--all', action='store_true', help='Include hidden directories in the search.')
 
+    # Add argument to list filenames only
+    parser.add_argument('-f', '--filenames', action='store_true', help='List filenames only, without path or modification time.')
+
     args = parser.parse_args()
 
     # date_arg and time_arg are combined in args.datetime, so they need to be split
@@ -76,8 +85,7 @@ def parse_arguments():
     date_arg = datetime_parts[0]
     time_arg = datetime_parts[1] if len(datetime_parts) > 1 else None
 
-    return date_arg, time_arg, args.path, args.all
-
+    return date_arg, time_arg, args.path, args.all, args.filenames
 
 # Function to convert string arguments to datetime object
 def parse_datetime(date_str, time_str=None):
@@ -114,7 +122,7 @@ def check_directory_exists(path):
         sys.exit(1)
 
 # Function to list recent files
-def list_recent_files(root_dir, datetime_obj, include_hidden):
+def list_recent_files(root_dir, datetime_obj, include_hidden, filenames_only):
     """
     List files that have been modified after the specified datetime.
 
@@ -122,6 +130,7 @@ def list_recent_files(root_dir, datetime_obj, include_hidden):
         root_dir: The root directory to search for files.
         datetime_obj: A datetime object to compare file modification times against.
         include_hidden: A boolean indicating whether to include hidden directories and files.
+        filenames_only: A boolean indicating whether to list filenames only, without path or modification time.
     """
     for dirpath, dirnames, filenames in os.walk(root_dir):
         if not include_hidden:
@@ -131,25 +140,28 @@ def list_recent_files(root_dir, datetime_obj, include_hidden):
             filenames = [f for f in filenames if not f.startswith('.')]
 
         for file in filenames:
-            file_path = os.path.join(dirpath, file)  # Changed from Path(dirpath) / file
-            mtime = datetime.fromtimestamp(os.path.getmtime(file_path), tz=timezone.utc)  # Changed from file_path.stat().st_mtime
+            file_path = os.path.join(dirpath, file)
+            mtime = datetime.fromtimestamp(os.path.getmtime(file_path), tz=timezone.utc)
             if mtime > datetime_obj:
-                print("{} - {}".format(mtime.strftime('%Y-%m-%d %H:%M:%S'), file_path))  # No change in print statement
+                if filenames_only:
+                    print(file)
+                else:
+                    print("{} - {}".format(mtime.strftime('%Y-%m-%d %H:%M:%S'), file_path))
 
 
 def main():
     # Check Python version
     if sys.version_info < (3, 2):
         print("Error: This script requires Python 3.2 or later.")
-        sys.exit(4)
+        sys.exit(3)
 
-    date_arg, time_arg, path_arg, include_hidden = parse_arguments()
+    date_arg, time_arg, path_arg, include_hidden, filenames_only = parse_arguments()
     datetime_obj = parse_datetime(date_arg, time_arg)
 
     # Call the new function to check directory existence
     check_directory_exists(path_arg)
 
-    list_recent_files(path_arg, datetime_obj, include_hidden)
+    list_recent_files(path_arg, datetime_obj, include_hidden, filenames_only)
 
 
 if __name__ == "__main__":
