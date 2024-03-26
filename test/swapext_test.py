fd854f1ca0372aa84bf27d2b44c9b48a77a48e77
id774 <idnanashi@gmail.com>
2024-03-27 01:25:26 +0900

    2479 Updated author info for consistency across scripts

--- test/swapext_test.py
+++ test/swapext_test.py
@@ -8,7 +8,7 @@
 #  the functionality of the swap_extensions function to ensure that it
 #  correctly changes the file extensions within a specified directory.
 #
-#  Author: id774
+#  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
 #  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
 #  Contact: idnanashi@gmail.com
--- test/zero_padding_test.py
+++ test/zero_padding_test.py
@@ -8,7 +8,7 @@
 #  the functionality of the rename_files function to ensure that file names
 #  are correctly zero-padded to the specified number of digits.
 #
-#  Author: id774
+#  Author: id774 (More info: http://id774.net)
 #  Source Code: https://github.com/id774/scripts
 #  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
 #  Contact: idnanashi@gmail.com
