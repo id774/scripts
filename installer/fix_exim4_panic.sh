749d9a886790a9ad653e087176c7875160587495
id774 <idnanashi@gmail.com>
2025-03-16 12:18:14 +0900

    2870 Removed obsolete scripts related to Exim4, RHEL setup, and Vim uninstallation

--- installer/fix_exim4_panic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo sed -i "s/dc_local_interfaces='127.0.0.1 ; ::1'/dc_local_interfaces='127.0.0.1'/" /etc/exim4/update-exim4.conf.conf
-
-test -f /var/log/exim4/paniclog && sudo rm /var/log/exim4/paniclog
