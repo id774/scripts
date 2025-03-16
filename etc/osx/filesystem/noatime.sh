483615246aca9056ddc44ed03a4535d399d762b0
id774 <idnanashi@gmail.com>
2025-03-16 15:38:05 +0900

    2885 Remove obsolete cron scripts and macOS system configurations

--- etc/osx/filesystem/noatime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo mount -vuwo noatime /
-sudo cp -v $SCRIPTS/etc/osx/filesystem/com.my.noatime.plist /Library/LaunchDaemons
-sudo chown root:wheel /Library/LaunchDaemons/com.my.noatime.plist
--- etc/osx/ramdisk/ramdisk.sh
+++
@@ -1,8 +0,0 @@
-RAMDISK="$(hdid -nomount ram://1048576)"
-diskutil eraseDisk HFS+ ramdisk $RAMDISK
-mkdir /Volumes/ramdisk/tmp
-chmod 700 /Volumes/ramdisk/tmp
-mkdir /Volumes/ramdisk/Caches
-chmod 700 /Volumes/ramdisk/Caches
-
-mount -u -o noatime /
