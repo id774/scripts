c03384543dd6dd21b29974ac2ca2ec8bb8908bcc
id774 <idnanashi@gmail.com>
2025-03-16 15:43:07 +0900

    2886 Refactored ClamAV autoscan script for POSIX compliance and improved error handling

--- cron/bin/clamscan.sh
+++ cron/bin/clamscan.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
-#
+
 ########################################################################
-# ClamAV AutoScan Script
+# clamav_autoscan.sh: Automated ClamAV Scan Script
 #
-#  Maintainer: id774 <idnanashi@gmail.com>
+#  Description:
+#  This script performs an automated ClamAV scan on specified directories.
+#  - Runs `freshclam` to update virus definitions.
+#  - Reads an exclusion file for directories and files to exclude.
+#  - Runs `clamscan` recursively on the target directories.
+#  - Logs scan results to a specified log file.
 #
+#  Author: id774 (More info: http://id774.net)
+#  Source Code: https://github.com/id774/scripts
+#  License: LGPLv3 (Details: https://www.gnu.org/licenses/lgpl-3.0.html)
+#  Contact: idnanashi@gmail.com
+#
+#  Version History:
+#  v1.0 2025-03-16
+#       Refactored script for POSIX compliance and improved maintainability.
+#       Added safety checks and validation for exclusion file handling.
 #  v0.5 2011-06-15
 #       Forked from clamav_upgrade.sh.
 #  v0.4 2009-10-22
@@ -12,30 +26,49 @@
 #  v0.3 2009-06-16
 #       Refactoring.
 #  v0.2 2007-11-10
-#       Add clamscan. Not delete source and object code.
+#       Added clamscan, preserved source and object code.
 #  v0.1 2007-10-16
-#       New.
+#       Initial version.
+#
+#  Usage:
+#  Run this script to perform an automated ClamAV scan:
+#      ./clamav_autoscan.sh
+#
+#  Requirements:
+#  - Must be executed on a system with `clamav` installed.
+#  - Requires `freshclam` for virus database updates.
+#  - Expects an exclusion file for directories and files to be ignored.
+#
 ########################################################################
-TARGETDIRS="/"
-LOGFILE=/var/log/clamav/clamav.log
-EXECDIR=${0%/*}
-EXCLUDEFILE=$EXECDIR/clamscan_exclude
 
-freshclam
-if [ -s $EXCLUDEFILE ]; then
-    for i in `cat $EXCLUDEFILE`
-    do
-        if [ $(echo "$i"|grep \/$) ]; then
-            i=`echo $i|sed -e 's/^\([^ ]*\)\/$/\1/p' -e d`
-            OPTS="${OPTS} --exclude-dir=$i"
-        else
-            OPTS="${OPTS} --exclude=$i"
-        fi
-    done
+# Define scan targets and log file
+TARGETDIRS="/"
+LOGFILE="/var/log/clamav/clamav.log"
+EXECDIR="$(dirname "$0")"
+EXCLUDEFILE="$EXECDIR/clamscan_exclude"
+
+# Update virus definitions
+freshclam || {
+    echo "Error: Failed to update ClamAV virus definitions." >&2
+    exit 1
+}
+
+# Construct exclusion options
+OPTS=""
+if [ -s "$EXCLUDEFILE" ]; then
+    while IFS= read -r line; do
+        case "$line" in
+            */) OPTS="$OPTS --exclude-dir=${line%/}" ;;
+            *)  OPTS="$OPTS --exclude=$line" ;;
+        esac
+    done < "$EXCLUDEFILE"
 fi
 
-for dir in $TARGETDIRS
-do
-    echo "clamscan ${dir} ${OPTS} -r -i -l ${LOGFILE}"
-    clamscan ${dir} ${OPTS} -r -i -l ${LOGFILE}
+# Run ClamAV scan
+for dir in $TARGETDIRS; do
+    echo "Scanning: $dir"
+    clamscan "$dir" $OPTS -r -i -l "$LOGFILE"
 done
+
+echo "ClamAV scan completed. Logs available at: $LOGFILE"
+exit 0
