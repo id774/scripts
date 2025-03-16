975205fcc99b5dce0eb6095d541fb06df5df26b5
id774 <idnanashi@gmail.com>
2025-03-16 11:52:53 +0900

    2866 Removed install_R-all.R and install_R-remove-all.R as they are no longer needed

--- installer/install_R-all.R
+++
@@ -1,9 +0,0 @@
-
-# sudo R --no-save --no-restore < ~/scripts/installer/install_R-all.R
-
-mirror <- "http://cran.ism.ac.jp"
-options(repos=mirror)
-packs <- available.packages(contriburl=contrib.url(mirror))
-install.packages(packs[,1],contriburl=contrib.url(mirror))
-library()
-
--- installer/install_R-remove-all.R
+++
@@ -1,9 +0,0 @@
-
-# sudo R --no-save --no-restore < ~/scripts/installer/install_R-remove-all.R
-
-mirror <- "http://cran.ism.ac.jp"
-options(repos=mirror)
-packs <- available.packages(contriburl=contrib.url(mirror))
-remove.packages(packs[,1],lib="/usr/local/lib/R/site-library")
-library()
-
