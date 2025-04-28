92943860b5264b1e529306fd33bb1fef91b5fd44
id774 <idnanashi@gmail.com>
2025-04-28 17:49:57 +0900

    3280 Change error messages to warning level in md5.py

--- md5.py
+++ md5.py
@@ -77,7 +77,7 @@ def print_checksum(include_subdir, reversed_format, quiet_mode, paths):
     script_name = os.path.basename(sys.argv[0])
     for path in paths:
         if not os.path.exists(path):
-            print("[ERROR] {0}: {1}: No such file or directory".format(script_name, path), file=sys.stderr)
+            print("[WARN] {0}: {1}: No such file or directory".format(script_name, path), file=sys.stderr)
             continue
         if include_subdir and S_ISDIR(os.stat(path)[ST_MODE]):
             for root, dirs, files in os.walk(path):
@@ -87,7 +87,7 @@ def print_checksum(include_subdir, reversed_format, quiet_mode, paths):
                     print_formatted_checksum(
                         checksum, full_path, reversed_format, quiet_mode, is_file=True)
         elif S_ISDIR(os.stat(path)[ST_MODE]):
-            print("{0}: {1}: Is a directory".format(script_name, path))
+            print("[WARN] {0}: {1}: Is a directory".format(script_name, path), file=sys.stderr)
         else:
             checksum = Md5Checksum.calculate_checksum(path)
             print_formatted_checksum(
