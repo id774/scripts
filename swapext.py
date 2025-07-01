f5cee597fafc41a2aa7b98ef2fb6d1e70836f6e8
id774 <idnanashi@gmail.com>
2025-07-01 22:05:49 +0900

    3478 Show usage when required arguments are missing in swapext.py

--- swapext.py
+++ swapext.py
@@ -157,6 +157,6 @@ def main():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 or sys.argv[1] in ('-h', '--help', '-v', '--version'):
+    if len(sys.argv) < 4 or sys.argv[1] in ('-h', '--help', '-v', '--version'):
         usage()
     main()
