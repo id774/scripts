8880f073a6129ea768cb1ad4cce190a0b53c3f50
id774 <idnanashi@gmail.com>
2026-08-23 01:40:26 +0900

    3522 Fix import formatting in wget test

--- test/wget_test.py
+++ test/wget_test.py
@@ -43,6 +43,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 try:
     import requests
+
     from wget import download_file
     HAS_REQUESTS = True
 except ImportError:
